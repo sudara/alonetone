ba24cff3cb1d008199225dd162963081a7006f89
Sudara <sudara@alonetone.com>
2008-03-13 17:18:31 +0100

    yuk, fixed a bad quote that took down the about page

---
+++ spec/controllers/pages_controller_spec.rb
@@ -0,0 +1,11 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe PagesController do
+  
+  it "should have an about page that renders without errors" do 
+    get :index
+    response.should be_success
+  end 
+  
+  
+end
