d860c0b1bed0eca893f1f8b071b1542dc202212d
Sudara <sudara@alonetone.com>
2008-05-16 21:15:21 +0200

    remove facebooker plugin

--- vendor/plugins/facebooker/lib/facebooker/photo.rb
+++
@@ -1,9 +0,0 @@
-require 'facebooker/model'
-module Facebooker
-  class Photo
-    include Model
-    attr_accessor :pid, :aid, :owner, :title,
-                  :link, :caption, :created,
-                  :src, :src_big, :src_small
-  end
-end
--- vendor/plugins/facebooker/lib/facebooker/version.rb
+++
@@ -1,9 +0,0 @@
-module Facebooker #:nodoc:
-  module VERSION #:nodoc:
-    MAJOR = 0
-    MINOR = 5
-    TINY  = 0
-
-    STRING = [MAJOR, MINOR, TINY].join('.')
-  end
-end
