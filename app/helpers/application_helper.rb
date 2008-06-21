21992240998789bd36b2ee7b3ccc8a39fb808191
Sudara <sudara@alonetone.com>
2008-06-21 17:29:01 +0200

    forum css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -101,7 +101,7 @@ module ApplicationHelper
   
   def feed_icon_tag(title, url)
     (@feed_icons ||= []) << { :url => url, :title => title }
-    link_to image_tag('feed-icon.png', :size => '14x14', :alt => "Subscribe to #{title}"), url
+    link_to image_tag('feed-icon.png', :size => '14x14', :alt => "Subscribe to #{title}"), url, :class => 'rss'
   end  
   protected 
 end
