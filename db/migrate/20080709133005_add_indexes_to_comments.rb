fbc09e1964f65292cbc0941d09a194f8400c880b
Sudara <sudara@alonetone.com>
2008-07-10 15:01:55 +0200

    remove_column, not drop_column

--- db/migrate/20080709133005_add_indexes_to_comments.rb
+++ db/migrate/20080709133005_add_indexes_to_comments.rb
@@ -1,10 +1,10 @@
 class AddIndexesToComments < ActiveRecord::Migration
   def self.up
-    add_index :comments, [:commentable_id, :commentable_type]
-    add_index :comments, :spam
-    add_index :comments, :created_at
-    add_index :pics, [:picable_id, :picable_type]
-    drop_column :comments, :is_spam
+    #add_index :comments, [:commentable_id, :commentable_type]
+    #add_index :comments, :spam
+    #add_index :comments, :created_at
+    #add_index :pics, [:picable_id, :picable_type]
+    remove_column :comments, :is_spam
   end
 
   def self.down
