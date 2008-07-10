6d45b161b7c94012af6dd5b4ad13d195ea2bfe13
Sudara <sudara@alonetone.com>
2008-07-10 15:03:23 +0200

    wow, playing with migrations on production server. fun stuff.

--- db/migrate/20080709133005_add_indexes_to_comments.rb
+++ db/migrate/20080709133005_add_indexes_to_comments.rb
@@ -1,9 +1,9 @@
 class AddIndexesToComments < ActiveRecord::Migration
   def self.up
-    #add_index :comments, [:commentable_id, :commentable_type]
-    #add_index :comments, :spam
-    #add_index :comments, :created_at
-    #add_index :pics, [:picable_id, :picable_type]
+    add_index :comments, [:commentable_id, :commentable_type]
+    add_index :comments, :spam
+    add_index :comments, :created_at
+    add_index :pics, [:picable_id, :picable_type]
     remove_column :comments, :is_spam
   end
 
