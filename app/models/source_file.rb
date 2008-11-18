154be4f619c6f4568031a0521dc33624c129f300
Sudara <sudara@alonetone.com>
2008-11-18 17:50:22 +0100

    Remove new relic for now

--- app/models/source_file.rb
+++ app/models/source_file.rb
@@ -3,7 +3,7 @@ class SourceFile < ActiveRecord::Base
   belongs_to      :user
   has_attachment  :storage      => ALONETONE.storage, 
                   :content_type => ['audio/x-aiff','audio/x-wav','application/zip'],
-                  :max_size     => 150.megabytes,
+                  :max_size     => 400.megabytes,
                   :path_prefix  => File.join(ALONETONE.path_prefix, "source_files"),
                   :s3_access    => :authenticated_read,
                   :processor    => ALONETONE.image_processor 
