ab25157ce49535a40a1f7b528a44aed1b17eaf31
Sudara <sudara@alonetone.com>
2008-11-20 22:55:26 +0100

    Email comments by default to alonetoners

---
+++ app/models/comment_observer.rb
@@ -0,0 +1,17 @@
+class CommentObserver < ActiveRecord::Observer
+  def after_save(comment)
+    if !comment.spam? and 
+        user_wants_email?(comment.user) and 
+        comment.commentable.class == Asset and
+        comment.user != comment.commenter
+      CommentMailer.deliver_new_comment(comment, comment.commentable) 
+    end
+  end
+  
+  protected
+  
+  def user_wants_email?(user)
+    # anyone who doesn't have it set to false, aka, opt-out
+    user.settings && (user.settings[:email_comments] != "false")
+  end
+end
--- config/environment.rb
+++ config/environment.rb
@@ -27,5 +27,5 @@ Rails::Initializer.run do |config|
 
   config.action_controller.session_store = :active_record_store
 
-  config.active_record.observers = :user_observer
+  config.active_record.observers = :user_observer, :comment_observer
 end
