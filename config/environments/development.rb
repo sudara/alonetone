14a76896ed6582e9de200bfef43812ae45febb11
Sudara <sudara@alonetone.com>
2008-11-20 22:09:06 +0100

    Woah, don't want to deliver emails in dev mode!!!

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,4 +17,4 @@ config.action_controller.perform_caching             = false
 # Don't care if the mailer can't send
 config.action_mailer.raise_delivery_errors = false
 
-config.action_mailer.delivery_method = :sendmail
+config.action_mailer.delivery_method = :test
