9f5ab386b5d13fdab74f44a36b3afa867191153a
Sudara <sudara@alonetone.com>
2009-08-07 00:14:49 +0200

    Typos fixed.

--- public/javascripts/soundmanager.js
+++ public/javascripts/soundmanager.js
@@ -15,8 +15,8 @@ var soundManager = null;
 function SoundManager(smURL,smID) {
  
   this.flashVersion = 9;           // version of flash to require, either 8 or 9. Some API features require Flash 9.
-  this.debugMode = true;           // enable debugging output (div#soundmanager-debug, OR console if available + configured)
-  this.useConsole = true;          // use firebug/safari console.log()-type debug console if available
+  this.debugMode = false;           // enable debugging output (div#soundmanager-debug, OR console if available + configured)
+  this.useConsole = false;          // use firebug/safari console.log()-type debug console if available
   this.consoleOnly = true;        // if console is being used, do not create/write to #soundmanager-debug
   this.waitForWindowLoad = false;  // force SM2 to wait for window.onload() before trying to call soundManager.onload()
   this.nullURL = 'null.mp3';       // path to "null" (empty) MP3 file, used to unload sounds (Flash 8 only)
