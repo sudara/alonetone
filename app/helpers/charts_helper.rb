af5d7d51300c6753b7da1793b30247005d54e4f1
Sudara <sudara@alonetone.com>
2009-08-09 20:14:00 +0200

    Re-format blog css, update charts_helper

--- app/helpers/charts_helper.rb
+++ app/helpers/charts_helper.rb
@@ -1,47 +1,40 @@
 # Wow, ugliest 3rd party lib ever.
-# Can't complain though, yay for free charts!
+# Can't complain too much though, yay for free charts!
+# cleaned up a bit...
 
-module ChartsHelper
+module ChartsHelper 
 
-#Contains an array of colors to be used as default set of colors for FusionCharts
-#arr_FCColors is the array that would contain the hex code of colors 
-#ALL COLORS HEX CODES TO BE USED WITHOUT #
+  # Contains an array of colors to be used as default set of colors for FusionCharts
+  # arr_FCColors is the array that would contain the hex code of colors 
+  # ALL COLORS HEX CODES TO BE USED WITHOUT #
 
 
-#We also initiate a counter variable to help us cyclically rotate through
-#the array of colors.
-@@FC_ColorCounter=0;
-@@arr_FCColors=[]
-@@arr_FCColors[0] = "1941A5" #Dark Blue
-@@arr_FCColors[1] = "AFD8F8"
-@@arr_FCColors[2] = "F6BD0F"
-@@arr_FCColors[3] = "8BBA00"
-@@arr_FCColors[4] = "A66EDD"
-@@arr_FCColors[5] = "F984A1" 
-@@arr_FCColors[6] = "CCCC00" #Chrome Yellow+Green
-@@arr_FCColors[7] = "999999" #Grey
-@@arr_FCColors[8] = "0099CC" #Blue Shade
-@@arr_FCColors[9] = "FF0000" #Bright Red 
-@@arr_FCColors[10] = "006F00" #Dark Green
-@@arr_FCColors[11] = "0099FF" #Blue (Light)
-@@arr_FCColors[12] = "FF66CC" #Dark Pink
-@@arr_FCColors[13] = "669966" #Dirty green
-@@arr_FCColors[14] = "7C7CB4" #Violet shade of blue
-@@arr_FCColors[15] = "FF9933" #Orange
-@@arr_FCColors[16] = "9900FF" #Violet
-@@arr_FCColors[17] = "99FFCC" #Blue+Green Light
-@@arr_FCColors[18] = "CCCCFF" #Light violet
-@@arr_FCColors[19] = "669900" #Shade of green
-
-  #get_FC_color function helps return a color from arr_FCColors array. It uses
-  #cyclic iteration to return a color from a given index. The index value is
-  #maintained in FC_ColorCounter
+  # We also initiate a counter variable to help us cyclically rotate through
+  # the array of colors.
+  
+  @@FC_ColorCounter = 0;
+  @@arr_FCColors = ["1941A5" , "AFD8F8" , "F6BD0F" , "8BBA00" , "A66EDD" , "F984A1" , "CCCC00" , "999999" , "0099CC" , "FF0000" ,  "006F00",  "0099FF",  "FF66CC",  "669966",  "7C7CB4",  "FF9933",  "9900FF",  "99FFCC",  "CCCCFF",  "669900"]
+  
+  
+  
+  # get_chart_color function helps return a color from arr_FCColors array. It uses
+  # cyclic iteration to return a color from a given index. The index value is
+  # maintained in FC_ColorCounter
   def get_chart_color 
     #Update index
     @@FC_ColorCounter=@@FC_ColorCounter+1
     counter = @@FC_ColorCounter % (@@arr_FCColors.size)
     #Return color
     return @@arr_FCColors[counter]
+  end
+  
+  def chart_options(options)
+    options.merge({ :bgColor => 'E1E2E1',
+                    :canvasBgColor => 'f6fceb',
+                    :baseFontColor => "37392B",
+                    :baseFontSize => 10,
+                    :outCnvBaseFontSze => 14,
+                    :baseFont => "Helvetica,Arial,Geneva"})
   end
   
   # Renders a chart from the swf file passed as parameter either making use of setDataURL method or
@@ -57,45 +50,40 @@ module ChartsHelper
   # - parameter debug_mode : (Not used in Free version) If value is true, chart is shown in debug mode.
   # - parameter register_with_js : (Not used in Free version) If value is true, the chart is registered with javascript
   # Can be called from html block int he view where the chart needs to be embedded.
-  def fusion_chart(chart_swf,url_or_xml,chart_id,chart_width,chart_height,debug_mode,register_with_js,&block)
+  def fusion_chart(chart_swf,url_or_xml,chart_id,chart_width,chart_height,debug_mode,register_with_js)
     chart_width=chart_width.to_s
     chart_height=chart_height.to_s
     
-    debug_mode_num="0";
     register_with_js_num="0";
     
-    if debug_mode==true
-      debug_mode_num="1"
-    end
+    debug_mode_num = debug_mode ? "1" : "0"
+    register_with_js_num = register_with_js ? "1" : "0"
+
     
-    if register_with_js==true
-      register_with_js_num="1"
-    end
+    result = ""
+    result += "\t\t<!-- START Script Block for Chart "+chart_id+" -->\n\t\t"
+    result += content_tag("div","\n\t\t\t\tChart.\n\t\t",{:id=>chart_id+"Div",:align=>"center"})
+    result += "\n\t\t<script type='text/javascript'>\n"
     
-    concat("\t\t<!-- START Script Block for Chart "+chart_id+" -->\n\t\t",block.binding) 
-    concat(content_tag("div","\n\t\t\t\tChart.\n\t\t",{:id=>chart_id+"Div",:align=>"center"}),block.binding)
-    concat("\n\t\t<script type='text/javascript'>\n",block.binding)
-    
-    concat("\t\t\t\tvar chart_"+chart_id+"=new FusionCharts('"+chart_swf+"','"+chart_id+"',"+chart_width+","+chart_height+","+debug_mode_num+","+register_with_js_num+");\n",block.binding)
+    result += "\t\t\t\tvar chart_"+chart_id+"=new FusionCharts('"+chart_swf+"','"+chart_id+"',"+chart_width+","+chart_height+","+debug_mode_num+","+register_with_js_num+");\n"
     
     if url_or_xml.match('/')
-      concat("\t\t\t\t<!-- Set the dataURL of the chart -->\n",block.binding)
-      concat("\t\t\t\tchart_"+chart_id+".setDataURL(\""+CGI::escape(url_or_xml)+"\");\n",block.binding)
+      result += "\t\t\t\t<!-- Set the dataURL of the chart -->\n"
+      result += "\t\t\t\tchart_"+chart_id+".setDataURL(\""+CGI::escape(url_or_xml)+"\");\n"
       logger.info("The method used is setDataURL.The URL is " + url_or_xml)
     else
-      concat("\t\t\t\t<!-- Provide entire XML data using DataXML method -->\n",block.binding)
-      #concat("\t\t\t\tchart_"+chart_id+".setDataXML(\""+str_xml+"\");\n",block.binding)
-      concat("\t\t\t\t",block.binding)
-      concat('chart_'+chart_id+'.setDataXML(\''+url_or_xml+'\');',block.binding)
-      concat("\n",block.binding)
+      result += "\t\t\t\t<!-- Provide entire XML data using DataXML method -->\n"
+      result += "\t\t\t\t"
+      result += 'chart_'+chart_id+'.setDataXML(\''+url_or_xml+'\');'
+      result += "\n"
       logger.info("The method used is setDataXML.The XML is " + url_or_xml)
     end
     
-    concat("\t\t\t\t<!-- Finally render the chart. -->\n",block.binding)
-    concat("\t\t\t\tchart_"+chart_id+".render('"+chart_id+"Div');\n",block.binding)
-    concat("\t\t</script>\n",block.binding)
-    concat("\t\t<!-- END Script Block for Chart "+chart_id+" -->\n",block.binding)
-    
+    result += "\t\t\t\t<!-- Finally render the chart. -->\n"
+    result += "\t\t\t\tchart_"+chart_id+".render('"+chart_id+"Div');\n"
+    result += "\t\t</script>\n"
+    result += "\t\t<!-- END Script Block for Chart "+chart_id+" -->\n"
+    result
   end
   # Renders a chart from the swf file passed as parameter either making use of setDataURL method or 
   # setDataXML method. The width and height of chart are passed as parameters to this function. If the chart is not rendered,
@@ -109,6 +97,7 @@ module ChartsHelper
   # - parameter debug_mode :  (Not used in Free version)True ( a boolean ) for debugging errors, if any, while rendering the chart.
   # Can be called from html block in the view where the chart needs to be embedded.
   def render_chart_html(chart_swf,str_url,str_xml,chart_id,chart_width,chart_height,debug_mode,&block)
+    result = ""
     chart_width=chart_width.to_s
     chart_height=chart_height.to_s
     
@@ -125,7 +114,7 @@ module ChartsHelper
       str_flash_vars="chartWidth="+chart_width+"&chartHeight="+chart_height+"&debugmode="+debug_mode_num+"&dataXML="+str_xml
       logger.info("The method used is setDataXML.The XML is " + str_xml)
     end
-    concat("\t\t<!-- START Code Block for Chart "+chart_id+" -->\n\t\t",block.binding)
+    result += "\t\t<!-- START Code Block for Chart "+chart_id+" -->\n\t\t"
     
     object_attributes={:classid=>"clsid:D27CDB6E-AE6D-11cf-96B8-444553540000"}
     object_attributes=object_attributes.merge(:codebase=>"http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0")
@@ -156,8 +145,9 @@ module ChartsHelper
     
     embed_tag=content_tag("embed","",embed_attributes)
     
-    concat(content_tag("object","\n\t\t\t\t"+param_tag1+"\n\t\t\t\t"+param_tag2+"\n\t\t\t\t"+param_tag3+"\n\t\t\t\t"+param_tag4+"\n\t\t\t\t"+embed_tag+"\n\t\t",object_attributes),block.binding)
-    concat("\n\t\t<!-- END Code Block for Chart "+chart_id+" -->\n",block.binding)
+    result += content_tag("object","\n\t\t\t\t"+param_tag1+"\n\t\t\t\t"+param_tag2+"\n\t\t\t\t"+param_tag3+"\n\t\t\t\t"+param_tag4+"\n\t\t\t\t"+embed_tag+"\n\t\t",object_attributes)
+    result += "\n\t\t<!-- END Code Block for Chart "+chart_id+" -->\n"
+    result
   end
   
   # Uses render_component.  
