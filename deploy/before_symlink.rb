40562f1ea9ecc85e54f0aa42e7a91fe542b79567
Sudara <sudara@alonetone.com>
2010-07-08 14:14:40 +0200

    no more old ey cloud stuff

--- deploy/before_symlink.rb
+++
@@ -1,5 +0,0 @@
-# A hook to get all our config in the right place
-
-['alonetone.yml','database.yml','amazon_s3.yml','defensio.yml', 'newrelic.yml'].each do |config_file|
-  run "ln -nfs #{shared_path}/config/#{config_file} #{release_path}/config/#{config_file}"
-end
