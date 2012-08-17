db03ad933e38ff83856bf548d25f2065bc10f92a
Sudara <sudara@alonetone.com>
2012-08-17 10:58:38 -0700

    update all config/environment stuff

--- config/environment.rb
+++ config/environment.rb
@@ -1,38 +1,40 @@
-RAILS_GEM_VERSION = '2.3.14' unless defined? RAILS_GEM_VERSION
+# Load the rails application
+require File.expand_path('../application', __FILE__)
 
-# Bootstrap the Rails environment, frameworks, and default configuration
-require File.join(File.dirname(__FILE__), 'boot')
+# Initialize the rails application
+Alonetone::Application.initialize!
 
-ALONETONE = YAML.load_file("#{RAILS_ROOT}/config/alonetone.yml")[RAILS_ENV]
+# FIGURE OUT WHAT TO DO WITH ALL THIS STUFF BELOWE
 
-class << ALONETONE
-  ALONETONE.keys.each do |key|
-    class_eval "def #{key}; ALONETONE['#{key}']; end"
-  end  
-end
+# ALONETONE = YAML.load_file("#{RAILS_ROOT}/config/alonetone.yml")[RAILS_ENV]
 
-
-Rails::Initializer.run do |config|
-
-
-  # load gems from vendor
-  config.load_paths += Dir["#{RAILS_ROOT}/vendor/gems/**"].map do |dir| 
-    File.directory?(lib = "#{dir}/lib") ? lib : dir
-  end
-
-  config.action_controller.session = {
-    :session_key => 'alonetone_cookie',
-    :secret => ALONETONE.secret
-  }
+# class << ALONETONE
+#   ALONETONE.keys.each do |key|
+#     class_eval "def #{key}; ALONETONE['#{key}']; end"
+#   end  
+# end
 
 
-  config.cache_store = :mem_cache_store, "localhost:11211"
 
-  config.active_record.observers = :user_observer, :comment_observer, :asset_observer
-  
-  # Required hack to inform rails we have files in models/user and models/asset
-  # Essentially lets us use concerned_with in development mode
-  config.autoload_paths += Dir[
-    File.join(Rails.root,'app','models','**')
-   ].reject{ |f| !File.directory?(f) }
-end
+
+# # load gems from vendor
+# config.load_paths += Dir["#{RAILS_ROOT}/vendor/gems/**"].map do |dir| 
+#   File.directory?(lib = "#{dir}/lib") ? lib : dir
+# end
+
+# config.action_controller.session = {
+#   :session_key => 'alonetone_cookie',
+#   :secret => ALONETONE.secret
+# }
+
+
+# config.cache_store = :mem_cache_store, "localhost:11211"
+
+# config.active_record.observers = :user_observer, :comment_observer, :asset_observer
+# 
+# # Required hack to inform rails we have files in models/user and models/asset
+# # Essentially lets us use concerned_with in development mode
+# config.autoload_paths += Dir[
+#   File.join(Rails.root,'app','models','**')
+#  ].reject{ |f| !File.directory?(f) }
+#nd
