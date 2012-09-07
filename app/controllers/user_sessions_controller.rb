e4c8f0f46c60cc16ece98a57897738f1f1d14c1d
Sudara <sudara@alonetone.com>
2012-09-07 02:50:08 +0200

    Authlogic! (untested, yo)

---
+++ app/controllers/user_sessions_controller.rb
@@ -0,0 +1,34 @@
+# -*- encoding : utf-8 -*-
+class UserSessionsController < ApplicationController
+  def new
+    @page_title = "Login"
+    @user_session = UserSession.new
+  end
+
+  def create
+    @user_session = UserSession.new(params[:user_session].merge({:remember_me => true})) #always stay logged in
+    if @user_session.save
+      if request.xhr?
+        render :nothing => true, :status => 200
+      else
+        redirect_back_or_default default_login_path
+      end
+    else
+      if request.xhr?
+        render :text => "Login Failed", :status => 401
+      else
+        flash.now[:alert] = "There was a problem logging you in! Please re-check your email and password."
+        render :action => :new
+      end
+    end
+  end
+
+  def destroy
+    if logged_in?
+      current_user_session.destroy
+      redirect_to new_user_session_url, :notice => "We've logged you out. Your secrets are safe with us!"
+    else
+      redirect_to login_path
+    end
+  end
+end
---
+++ db/migrate/20120907003353_add_perishable_token_for_authlogic.rb
@@ -0,0 +1,16 @@
+class AddPerishableTokenForAuthlogic < ActiveRecord::Migration
+  def up
+    # activation stuff now handled by authlogic
+    add_column :users, :perishable_token, :string, :default => :null
+    remove_column :users, :activation_code
+    
+    # rename since authlogic handles this now
+    rename_column :users, :last_seen_at, :last_login_at
+    
+    # unused/old shit in user
+    remove_column :users, :facebook_account_id
+  end
+
+  def down
+  end
+end
