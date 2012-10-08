9ec2128ca6009621630106196b2acc200fb5ab65
Sudara <sudara@alonetone.com>
2012-10-08 03:36:26 +0200

    Fix up database.yml to use mysql2. Fix paperclip settings.

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,12 +1,6 @@
-Paperclip::Attachment.default_options[:storage] = :fog
-Paperclip::Attachment.default_options[:fog_credentials] = {:provider => "Local", :local_root => "#{Rails.root}/public"}
-Paperclip::Attachment.default_options[:fog_directory] = ""
-Paperclip::Attachment.default_options[:fog_host] = "http://localhost:3000"
-
-
 # Paperclip config, depends on yml loaded above
-Paperclip::Attachment.default_options[:convert_options => { :all => '-strip -colorspace RGB'} ]
-Paperclip::Attachment.default_options[:storage] => Alonetone.storage
+Paperclip::Attachment.default_options[:convert_options = { :all => '-strip -colorspace RGB'} ]
+Paperclip::Attachment.default_options[:storage] = Alonetone.storage
 
 Paperclip::Attachment.default_options.merge!({ 
   :s3_credentials => {
