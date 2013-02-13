ac87ed9ea4f9b9a9a48251c5cebb948c05395b8f
Sudara <sudara@alonetone.com>
2013-02-13 02:14:14 +0100

    Update alonetone.example.yml

--- app/models/source_file.rb
+++ app/models/source_file.rb
@@ -1,13 +1,6 @@
 # -*- encoding : utf-8 -*-
 class SourceFile < ActiveRecord::Base
   
-  belongs_to      :user
-  #has_attachment  :storage      => Alonetone.storage, 
-  #               :content_type => ['audio/x-aiff','audio/x-wav','application/zip'],
-  #               :max_size     => 400.megabytes,
-  #               :path_prefix  => File.join(Alonetone.path_prefix, "source_files"),
-  #               :s3_access    => :authenticated_read,
-  #               :processor    => Alonetone.image_processor 
-                  
+  belongs_to      :user         
   
 end
