27a897e233c5cdcd474f6c19cd18decc03c25eb6
Sudara <sudara@alonetone.com>
2013-09-17 02:09:47 +0200

    Extra line in db/seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,8 +4,6 @@ require 'active_record/fixtures'
 admin = User.create(:login => 'admin', :password => 'testing123', :password_confirmation => 'testing123')
 admin.update_attribute(:admin, true)
 
-admin = User.find_by_login("admin")
-
 forum = Forum.create
 forum.name = "Making Music"
 forum.description = "Talk about how you do it, how you want to do it, and how you feel about it."
