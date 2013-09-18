256126ed768bc4e2f2289fef58a3f9473f91d870
Sudara <sudara@alonetone.com>
2013-09-19 01:31:02 +0200

    Reduce complexity of forums_controller

--- app/controllers/forums_controller.rb
+++ app/controllers/forums_controller.rb
@@ -3,20 +3,15 @@ class ForumsController < ApplicationController
   
   before_filter :require_login, :except => [:index, :show]
   before_filter :find_user, :only => :index
-  before_filter :set_forum_tab
+  before_filter :set_forum_tab, :set_html_meta
 
   # GET /forums
   # GET /forums.xml
   def index
     # reset the page of each forum we have visited when we go back to index
     session[:forums_page] = nil
-
+    
     @forums = Forum.ordered
-    @page_title = 'alonetone Forums'
-    @description = "alonetone forums. Discuss making music, free music, " << 
-                   "the changing music world, and whatever else comes to you"
-    @keywords = 'alonetone, forums, music, discussion, share'
-    @online =  User.currently_online
     @user_topics = Topic.replied_to_by(@user).collect(&:topic) if logged_in?
     @popular_topics = Topic.popular.collect(&:first)
     @replyless_topics = Topic.replyless
@@ -30,13 +25,7 @@ class ForumsController < ApplicationController
   # GET /forums/1.xml
   def show
     @forum = Forum.find_by_permalink(params[:id])
-    session[:forums] ||= {}
-    session[:forums][@forum.id] = Time.now.utc
-    
-    if current_page > 1
-      session[:forums_page] ||= Hash.new(1)
-      session[:forums_page][@forum.id] = current_page 
-    end
+    handle_forum_session
     
     respond_to do |format|
       format.html do # show.html.erb
@@ -46,15 +35,8 @@ class ForumsController < ApplicationController
     end
   end
 
-  # GET /forums/new
-  # GET /forums/new.xml
   def new
     @forum = Forum.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @forum }
-    end
   end
 
   # GET /forums/1/edit
@@ -62,55 +44,53 @@ class ForumsController < ApplicationController
     @forum = Forum.find_by_permalink(params[:id])
   end
 
-  # POST /forums
-  # POST /forums.xml
   def create
     @forum = Forum.new(params[:forum])
-
-    respond_to do |format|
-      if @forum.save
-        flash[:notice] = 'Forum was successfully created.'
-        format.html { redirect_to(@forum) }
-        format.xml  { render :xml => @forum, :status => :created, :location => @forum }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @forum.errors, :status => :unprocessable_entity }
-      end
+    if @forum.save
+      flash[:notice] = 'Forum was successfully created.'
+      redirect_to(@forum) 
+    else
+      render :action => "new" 
     end
   end
 
-  # PUT /forums/1
-  # PUT /forums/1.xml
   def update
     @forum = Forum.find_by_permalink(params[:id])
-
-    respond_to do |format|
-      if @forum.update_attributes(params[:forum])
-        flash[:notice] = 'Forum was successfully updated.'
-        format.html { redirect_to(@forum) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @forum.errors, :status => :unprocessable_entity }
-      end
+    if @forum.update_attributes(params[:forum])
+      flash[:notice] = 'Forum was successfully updated.'
+      redirect_to(@forum) 
+    else
+      render :action => "edit" 
     end
   end
 
-  # DELETE /forums/1
-  # DELETE /forums/1.xml
   def destroy
     @forum = Forum.find_by_permalink(params[:id])
     @forum.destroy
 
-    respond_to do |format|
-      format.html { redirect_to(forums_path) }
-      format.xml  { head :ok }
-    end
+    redirect_to(forums_path) 
   end
   
   protected
+  
   def authorized?
     admin?
+  end
+  
+  def set_html_meta
+    @page_title = 'alonetone Forums'
+    @description = "alonetone forums. Discuss making music, free music, the changing music world, and whatever else comes to you"
+    @keywords = 'alonetone, forums, music, discussion, share'
+  end
+  
+  def handle_forum_session
+    session[:forums] ||= {}
+    session[:forums][@forum.id] = Time.now.utc
+    
+    if current_page > 1
+      session[:forums_page] ||= Hash.new(1)
+      session[:forums_page][@forum.id] = current_page 
+    end
   end
   
   def set_forum_tab
