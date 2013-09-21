1f4ea705106d4ef1202d2c3b114b83ecee3ae6be
Sudara <sudara@alonetone.com>
2013-09-22 01:45:01 +0200

    Feature controller updates

--- app/controllers/features_controller.rb
+++ app/controllers/features_controller.rb
@@ -3,8 +3,7 @@ class FeaturesController < ApplicationController
   
   before_filter :require_login,  :only => [:new, :create, :edit, :update, :delete]
   before_filter :find_feature,    :only => [:update, :delete, :edit]
-  # GET /features
-  # GET /features.xml
+
   def index
     @features = admin? ? Feature.all: Feature.published
     @page_title = 'Featured Artists'
@@ -13,39 +12,13 @@ class FeaturesController < ApplicationController
       format.xml  { render :xml => @features }
     end
   end
-
-  # GET /features/1
-  # GET /features/1.xml
+  
   def show
-    @feature =  admin? ? 
-                Feature.all.find_by_permalink(params[:id]) : 
-                Feature.published.find_by_permalink(params[:id])
-      
-    @user = @feature.featured_user
-
-    Feature.increment_counter(:views_count, @feature.id) unless admin?
-
-    @featured_tracks = @feature.featured_tracks
-
-    @mostly_listens_to = @user.mostly_listens_to
-
-    @comment = Comment.new(
-      :commentable_type => 'feature',
-      :commentable_id => @feature.id
-    )
-
-    @comments = @feature.comments.include_private
-
+    @feature =  admin? ? find_feature : Feature.published.where(:permalink => params[:id]).first
+    set_related_feature_variables
     @page_title = "Featured Artist: #{@user.name}"
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @feature }
-    end
   end
 
-  # GET /features/new
-  # GET /features/new.xml
   def new
     @feature = Feature.new
     @users = User.alpha
@@ -55,60 +28,33 @@ class FeaturesController < ApplicationController
     end
   end
 
-  # GET /features/1/edit
   def edit
     @users = User.alpha
     @assets = @feature.featured_user.assets
   end
 
-  # POST /features
-  # POST /features.xml
   def create
     @feature = Feature.new(params[:feature])
-
-    respond_to do |format|
-      if @feature.save
-        flash[:notice] = 'Feature was successfully created.'
-        format.html { redirect_to(@feature) }
-        format.xml  { render(
-                        :xml      => @feature, 
-                        :status   => :created, 
-                        :location => @feature 
-                    )}
-      else
-        format.html { render :action => "new" }
-        format.xml  { render(
-                        :xml    => @feature.errors, 
-                        :status => :unprocessable_entity
-                    )}
-      end
+    if @feature.save
+      flash[:notice] = 'Feature was successfully created.'
+      redirect_to(@feature) }
+    else
+      render :action => "new" 
     end
   end
 
-  # PUT /features/1
-  # PUT /features/1.xml
   def update
-
-    respond_to do |format|
-      if @feature.update_attributes(params[:feature])
-        flash[:notice] = 'Feature was successfully updated.'
-        format.html { redirect_to(@feature) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render(
-                        :xml    => @feature.errors, 
-                        :status => :unprocessable_entity 
-                    )}
-      end
+    if @feature.update_attributes(params[:feature])
+      flash[:notice] = 'Feature was successfully updated.'
+      redirect_to(@feature) 
+    else
+      render :action => "edit" 
     end
   end
 
-  # DELETE /features/1
-  # DELETE /features/1.xml
+
   def destroy
     @feature.destroy
-
     respond_to do |format|
       format.html { redirect_to(features_url) }
       format.xml  { head :ok }
@@ -122,6 +68,15 @@ class FeaturesController < ApplicationController
   end
   
   def find_feature
-    @feature = Feature.find_by_permalink(params[:id])
+    @feature = Feature.where(:permalink => params[:id]).first
+  end
+  
+  def set_related_feature_variables
+    @user = @feature.featured_user
+    Feature.increment_counter(:views_count, @feature.id) unless admin?
+    @featured_tracks = @feature.featured_tracks
+    @mostly_listens_to = @user.mostly_listens_to
+    @comment = Comment.new(:commentable_type => 'feature', :commentable_id => @feature.id)
+    @comments = @feature.comments.include_private
   end
 end
