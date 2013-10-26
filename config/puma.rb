54a5775fe864df729910565e7dd98ffcfee88b13
Sudara <sudara@alonetone.com>
2013-10-27 00:50:43 +0200

    Puma log/stdout/socket/state config

--- config/puma.rb
+++ config/puma.rb
@@ -4,7 +4,21 @@ environment 'production'
 workers 4
 preload_app!
 deamonize true
-pidfile
+pidfile '/var/run/puma.pid'
+stdout_redirect '/var/log/puma.log', '/var/log/puma.log', true
+
+
+
+# The default is “tcp://0.0.0.0:9292”
+# bind 'tcp://0.0.0.0:9292'
+
+bind 'unix://var/run/puma.sock'
+state_path '/var/run/puma.state'
+
+
+# on_restart do
+#   puts 'On restart...'
+# end
 
 # config/puma.rb
 on_worker_boot do
