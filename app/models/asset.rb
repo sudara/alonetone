e3a6d449728120b9569d3f1ee73f399af746806d
Sudara <sudara@alonetone.com>
2013-11-03 22:02:47 +0100

    Allow more asset attributes to be updated in bulk

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -29,7 +29,8 @@ class Asset < ActiveRecord::Base
   after_create :notify_followers
   
   validates_presence_of :user_id
-  attr_accessible :user, :mp3, :size, :name, :user_id
+  attr_accessible :user, :mp3, :size, :name, :user_id, :title, :description, 
+    :youtube_embed, :credits
 
   # override has_permalink method to ensure we don't get empty permas
   def generate_permalink!
