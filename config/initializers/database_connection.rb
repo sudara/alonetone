ba692f3c4905b03ab877b126514d5bb2446a82f3
Sudara <sudara@alonetone.com>
2013-11-12 16:18:17 +0100

    Remove reaping freq from database_connection.rb

--- config/initializers/database_connection.rb
+++ config/initializers/database_connection.rb
@@ -3,7 +3,6 @@ Rails.application.config.after_initialize do
 
   ActiveSupport.on_load(:active_record) do
     config = Rails.application.config.database_configuration[Rails.env]
-    config['reaping_frequency'] = ENV['DB_REAP_FREQ'] || 10 # seconds
     config['pool']              = ENV['DB_POOL']      || 8
     ActiveRecord::Base.establish_connection(config)
   end
