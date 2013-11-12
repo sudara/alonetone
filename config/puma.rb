613ad311277eb5087a4dd3d5e051c7207667d0e4
Sudara <sudara@alonetone.com>
2013-11-12 01:42:23 +0100

    Knock puma down to 4 threads max per worker

--- config/puma.rb
+++ config/puma.rb
@@ -2,23 +2,14 @@
 
 environment 'production'
 workers 4
+threads 0,4
 preload_app!
 daemonize true
 pidfile 'tmp/puma.pid'
 stdout_redirect 'log/puma.log', 'log/puma.log', true
 
-
-
-# The default is “tcp://0.0.0.0:9292”
-# bind 'tcp://0.0.0.0:9292'
-
 bind 'unix://tmp/puma.sock'
 state_path 'tmp/puma.state'
-
-
-# on_restart do
-#   puts 'On restart...'
-# end
 
 # config/puma.rb
 on_worker_boot do
