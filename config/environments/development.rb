e625d773d4239d3679bc8466a43c14148d6ed58d
Sudara <sudara@alonetone.com>
2013-11-14 01:59:11 +0100

    Consider requests local in dev

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,7 +8,7 @@ Alonetone::Application.configure do
   config.eager_load = false
 
   # Show full error reports and disable caching
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
