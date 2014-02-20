69fb3b1b5bcc2c49408d7e672681c3b26a703d71
Devin Turner <devin.turner09@gmail.com>
2014-02-20 23:17:55 +0000

    added some css changes to stats page

--- app/assets/javascripts/statscharts.js
+++ app/assets/javascripts/statscharts.js
@@ -1,16 +1,30 @@
-
-$('#expand-charts').on('click', function(event) {
-	event.preventDefault();
-	$('.panel-body').removeClass('hide');
-	$('.panel-body').siblings('.toggle-chart').html('-');
-});
-
+// hide all charts
 $('#collapse-charts').on('click', function(event) {
 	event.preventDefault();
+
+	// hide all the cool shit
 	$('.panel-body').addClass('hide');
-	$('.panel-body').siblings('.toggle-chart').html('+');
+	$('.toggle-chart').html('+');
+
+	// hide this button and show the other
+	$(this).addClass('hide');
+	$('#expand-charts').removeClass('hide');
 });
 
+// show all charts
+$('#expand-charts').on('click', function(event) {
+	event.preventDefault();
+
+	// make the cool shit show up
+	$('.panel-body').removeClass('hide');
+	$('.toggle-chart').html('-');
+
+	// hide this button and show the other
+	$(this).addClass('hide');
+	$('#collapse-charts').removeClass('hide');
+});
+
+// toggle hide/show for individual charts
 $('.toggle-chart').on('click', function(event) {
 	event.preventDefault();
 	$(this).parents('.panel-heading').siblings('.panel-body').toggleClass('hide');
