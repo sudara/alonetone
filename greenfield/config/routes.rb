3c13549efa01d25f21fcb990fddf2b19bf0645d6
Peter Woo <petersnowdonwoo@gmail.com>
2015-01-26 11:42:40 -0800

    Use scope instead of resources for user segment

--- greenfield/config/routes.rb
+++ greenfield/config/routes.rb
@@ -1,5 +1,5 @@
 Greenfield::Engine.routes.draw do
-  resources :users, :path => '/' do
+  scope :path => ':user_id', :as => 'user' do
     resources :posts, :path => '/', :only => [:new, :show, :edit, :create, :update] do
       resources :attached_assets, :only => :show
     end
