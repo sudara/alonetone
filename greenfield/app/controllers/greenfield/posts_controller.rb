c4b74de784432c41f683d3eaef42fe315aa6085a
Peter Woo <petersnowdonwoo@gmail.com>
2015-01-26 16:27:25 -0800

    No more 'new' action, only edit

--- greenfield/app/controllers/greenfield/posts_controller.rb
+++ greenfield/app/controllers/greenfield/posts_controller.rb
@@ -1,31 +1,16 @@
 module Greenfield
   class PostsController < Greenfield::ApplicationController
-    before_filter :render_new_unless_post_exists, :only => :show
-    before_filter :redirect_to_edit_if_post_exists, :only => :new
-    before_filter :extract_attached_asset_waveforms, :only => [:create, :update]
+    before_filter :create_and_edit_unless_post_exists, :only => :show
+    before_filter :extract_attached_asset_waveforms, :only => :update
 
     def show
       @post = find_post
       @page_title = "#{@post.user.display_name} - #{@post.title}"
     end
 
-    def new
-      @post = find_asset.build_greenfield_post
-      @post.attached_assets.build
-    end
-
     def edit
       @post = find_post
-    end
-
-    def create
-      @post = find_asset.build_greenfield_post(params[:post])
-
-      if @post.save
-        redirect_to user_post_path(@post.user, @post)
-      else
-        render :new
-      end
+      @post.attached_assets.build
     end
 
     def update
@@ -39,19 +24,13 @@ module Greenfield
 
     protected
 
-    def render_new_unless_post_exists
-      unless find_asset.greenfield_post
-        new
-        render 'new'
-      end
-    end
-
-    def redirect_to_edit_if_post_exists
-      if post = find_asset.greenfield_post
+    def create_and_edit_unless_post_exists
+      if find_asset.user == current_user && !find_asset.greenfield_post
+        post = find_asset.build_greenfield_post
+        post.save!(:validate => false)
         redirect_to edit_user_post_path(post.user, post)
       end
     end
-
     def extract_attached_asset_waveforms
       params[:post][:attached_assets_attributes].each_with_index do |attrs, i|
         next unless attrs[1][:mp3].present?
--- greenfield/config/routes.rb
+++ greenfield/config/routes.rb
@@ -1,6 +1,6 @@
 Greenfield::Engine.routes.draw do
   scope :path => ':user_id', :as => 'user' do
-    resources :posts, :path => '/', :only => [:new, :show, :edit, :create, :update] do
+    resources :posts, :path => '/', :only => [:show, :edit, :update] do
       resources :attached_assets, :only => :show
     end
   end
