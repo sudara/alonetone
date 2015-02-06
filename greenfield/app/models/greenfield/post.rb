c1ee11bff0d02d17c609edb29e2e1841d63b697a
Sudara <sudara@alonetone.com>
2015-02-06 19:51:50 +0100

    Super basic edit form improvements

--- greenfield/app/models/greenfield/post.rb
+++ greenfield/app/models/greenfield/post.rb
@@ -5,8 +5,10 @@ module Greenfield
     belongs_to :asset # alonetone asset
     has_one :user, :through => :asset
     delegate :to_param, :title, :to => :asset
-
+    
     has_many :attached_assets, :dependent => :destroy # embedded assets
+    
+    accepts_nested_attributes_for :asset # need to update title in greenfield
     accepts_nested_attributes_for :attached_assets
     attr_accessible :attached_assets_attributes
 
