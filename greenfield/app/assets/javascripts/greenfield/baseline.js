3c18ee696f7f20abeb4f4b7d0b43fcad057ac885
Sudara <sudara@alonetone.com>
2015-02-14 05:51:39 +0100

    Baseline work

---
+++ greenfield/app/assets/javascripts/greenfield/baseline.js
@@ -0,0 +1,149 @@
+/*!
+* Baseline.js 1.0
+*
+* Copyright 2013, Daniel Eden http://daneden.me
+* Released under the WTFPL license
+* http://sam.zoy.org/wtfpl/
+*
+* Date: Sat August 04 23:47:00 2013 GMT
+*/
+
+(function (window, $) {
+
+  'use strict';
+
+  var baseline = (function () {
+
+    /**
+     * `_base` will later hold the value for the current baseline that matches
+     * the given breakpoint. `_breakpoints` will hold a reference to all
+     * breakpoints given to the baseline call.
+     */
+
+    var _base = 0,
+        _breakpoints = {};
+
+    /**
+     * @name     _setBase
+     *
+     * Set the correct margin-bottom on the given element to get back to the
+     * baseline.
+     *
+     * @param    {Element}  element
+     *
+     * @private
+     */
+
+    function _setBase (element) {
+      var height = element.offsetHeight,
+          current, old;
+
+      /**
+       * In this step we loop through all the breakpoints, if any were given.
+       * If the baseline call received a number from the beginning, this loop
+       * is simply ignored.
+       */
+
+      for (var key in _breakpoints) {
+        current = key;
+
+        if (document.body.clientWidth > current && current >= old) {
+          _base = _breakpoints[key];
+          old = current;
+        }
+      }
+
+      /**
+       * We set the element's margin-bottom style to a number that pushes the
+       * adjacent element down far enough to get back onto the baseline.
+       */
+
+      element.style.marginBottom = _base - (height % _base) + 'px';
+    }
+
+    /**
+     * @name     _init
+     *
+     * Call `_setBase()` on the given element and add an event listener to the
+     * window to reset the baseline on resize.
+     *
+     * @param    {Element}  element
+     *
+     * @private
+     */
+
+    function _init (element) {
+      _setBase(element);
+
+      if ('addEventListener' in window) {
+        window.addEventListener('resize', function () { _setBase(element); }, false);
+      } else if ('attachEvent' in window) {
+        window.attachEvent('resize', function () { _setBase(element); });
+      }
+    }
+
+    /**
+     * @name     baseline
+     *
+     * Gets the correct elements and attaches the baseline behaviour to them.
+     *
+     * @param    {String/Element/NodeList}  elements
+     * @param    {Number/Object}            options
+     */
+
+    return function baseline (elements, options) {
+
+      /**
+       * Accept a NodeList or a selector string and set `targets` to the
+       * relevant elements.
+       */
+
+      var targets = typeof elements === 'string' ? document.querySelectorAll(elements) : elements,
+          len = targets.length;
+
+      /**
+       * Decide whether to set the `_breakpoints` variable or not. This will be
+       * relevant in the `_setBase()` function.
+       */
+
+      if (typeof options === 'number') {
+        _base = parseInt(options, 10);
+      } else if (typeof options === 'object') {
+        var em = parseInt(getComputedStyle(document.body, null).getPropertyValue('font-size'), 10);
+
+        for (var point in _breakpoints) {
+          var unitless = /\d+em/.test(point) ? parseInt(point, 10) * em : /\d+px/.test(point) ? parseInt(point, 10) : point;
+          _breakpoints[unitless] = parseInt(_breakpoints[point], 10);
+        }
+      }
+
+      /**
+       * If we have multiple elements, loop through them, otherwise just
+       * initialise the functionality on the single element.
+       */
+
+      if (len > 1) {
+        while (len--) { _init(targets[len]); }
+      } else {
+        _init(targets[0]);
+      }
+    };
+
+  }());
+
+  /**
+   * Export baseline as a jQuery or Zepto plugin if any of them are loaded,
+   * otherwise export as a browser global.
+   */
+
+  if (typeof $ !== "undefined") {
+    $.extend($.fn, {
+      baseline: function (options) {
+        return baseline(this, options);
+      }
+    });
+  } else {
+    window.baseline = baseline;
+  }
+
+}(window, window.jQuery || window.Zepto || undefined));
