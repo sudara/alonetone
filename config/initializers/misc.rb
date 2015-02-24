01ee3e047d3a2b1c85be8ff7a89947ea4e60f04e
Peter Woo <petersnowdonwoo@gmail.com>
2015-02-24 14:30:10 -0800

    Fix Geokit initialization

--- config/initializers/misc.rb
+++ config/initializers/misc.rb
@@ -16,9 +16,9 @@ end
 WillPaginate::ViewHelpers.pagination_options[:inner_window] = 2
 WillPaginate::ViewHelpers.pagination_options[:outer_window] = 0
 
-# These defaults are used in GeoKit::Mappable.distance_to and in acts_as_mappable
-GeoKit::default_units = :miles
-GeoKit::default_formula = :sphere
+# These defaults are used in Geokit::Mappable.distance_to and in acts_as_mappable
+Geokit::default_units = :miles
+Geokit::default_formula = :sphere
 
 # This is the timeout value in seconds to be used for calls to the geocoder web
 # services.  For no timeout at all, comment out the setting.  The timeout unit
@@ -28,7 +28,8 @@ Geokit::Geocoders::request_timeout = 2
 # This is your Google Maps geocoder key. 
 # See http://www.google.com/apis/maps/signup.html
 # and http://www.google.com/apis/maps/documentation/#Geocoding_Examples  
-Geokit::Geocoders::google = 'ABQIAAAAb7vQ1d8XHrxuF5AJr8c-oxTbnWe9oX1zcTcDyLcGskh9JPynnBQ3ngxc6atkyQFihW5nLKpXFQ58gQ'
+Geokit::Geocoders::GoogleGeocoder.api_key =
+  'ABQIAAAAb7vQ1d8XHrxuF5AJr8c-oxTbnWe9oX1zcTcDyLcGskh9JPynnBQ3ngxc6atkyQFihW5nLKpXFQ58gQ'
 
 
 # This is the order in which the geocoders are called in a failover scenario
