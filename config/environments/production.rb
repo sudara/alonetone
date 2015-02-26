a02271701608aae00c6efe11042c04bf3fa52115
Peter Woo <petersnowdonwoo@gmail.com>
2015-02-25 22:21:53 -0800

    config for mailing via smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,12 +64,8 @@ Alonetone::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.smtp_settings = {
-  # :address        => "localhost",
-  # :port           => 25,
-  # :domain         => "ramenmusic.com"
-  # }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = Alonetone.smtp_settings
   
   # Set to :debug to see everything in the log.
   config.log_level = :info
