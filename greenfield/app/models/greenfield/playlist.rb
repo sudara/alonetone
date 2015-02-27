dbb7b654b5107ec178baad5b114dd12b85d40827
Peter Woo <petersnowdonwoo@gmail.com>
2015-02-26 18:58:28 -0800

    Playlist tracks are dependent: destroy

--- greenfield/app/models/greenfield/playlist.rb
+++ greenfield/app/models/greenfield/playlist.rb
@@ -2,7 +2,7 @@ module Greenfield
   class Playlist < ActiveRecord::Base
     belongs_to :user
 
-    has_many :playlist_tracks
+    has_many :playlist_tracks, :dependent => :destroy
     has_many :posts, :through => :playlist_tracks, :source => :post
 
     attr_accessible :title
