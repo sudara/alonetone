33494ece0cc3ac2e3711626b1bc60b02f0d2c581
Peter Woo <petersnowdonwoo@gmail.com>
2015-03-06 15:00:36 -0800

    Finish removing baseline.js

--- greenfield/app/assets/javascripts/greenfield/posts.js
+++ greenfield/app/assets/javascripts/greenfield/posts.js
@@ -1,7 +1,4 @@
 $(document).ready(function(){
   // resizable textarea for edit page
   $('.post_content textarea').autosize();  
-  
-  // adjust image margins to fit baseline in posts
-  $('.post_content p > img').baseline(27);  
 });
