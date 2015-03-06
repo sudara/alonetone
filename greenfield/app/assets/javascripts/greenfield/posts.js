e14ba2bddb989f88afafe88d1c975c4e0b9ce13f
Peter Woo <petersnowdonwoo@gmail.com>
2015-03-06 02:06:51 -0800

    Concept for sticky player widget on scroll

--- greenfield/app/assets/javascripts/greenfield/posts.js
+++ greenfield/app/assets/javascripts/greenfield/posts.js
@@ -1,3 +1,29 @@
+$(function() {
+  var player = $('.player');
+  var playerOffset = player.offset().top;
+  var placeholder = $('<div>').addClass('player').css({
+    'display': 'none',
+    'border-top': '1px solid rgba(0, 0, 0, 0)',
+    'border-bottom': '1px solid rgba(0, 0, 0, 0)'
+  }).insertBefore(player);
+
+  var stuck = false;
+  $(window).scroll(function() {
+    var wasStuck = stuck;
+    stuck = $(this).scrollTop() > playerOffset+25;
+
+    if (!wasStuck && stuck) {
+      player.addClass('stuck');
+      reconstructWaveform(player.find('.waveform'));
+      placeholder.css('display', 'block');
+    } else if (wasStuck && !stuck) {
+      player.removeClass('stuck');
+      reconstructWaveform(player.find('.waveform'));
+      placeholder.css('display', 'none');
+    }
+  });
+});
+
 $(document).ready(function(){
   // resizable textarea for edit page
   $('.post_content textarea').autosize();  
