a9155a91dc305ebad450f00055c2d719ab287224
Peter Woo <petersnowdonwoo@gmail.com>
2015-05-11 12:05:22 -0700

    Uploads sometimes come in as audio/x-mp3

--- app/models/asset/uploading.rb
+++ app/models/asset/uploading.rb
@@ -15,7 +15,7 @@ class Asset
 
   validates_attachment_size :mp3, :less_than => 60.megabytes
   validates_attachment_presence :mp3, :message => 'must be set. Make sure you chose a file to upload!'
-  validates_attachment_content_type :mp3, :content_type => ['audio/mpeg', 'audio/mp3'], :message => " was wrong. It doesn't look like you uploaded a valid mp3 file. Could you double check?"
+  validates_attachment_content_type :mp3, :content_type => ['audio/mpeg', 'audio/mp3', 'audio/x-mp3'], :message => " was wrong. It doesn't look like you uploaded a valid mp3 file. Could you double check?"
 
 
  # Disable zip uploads for now, make life easier transitioning to paperclip
