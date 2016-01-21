62f376c6b98da097817f8ff47eebc6a7f4717413
Peter Woo <petersnowdonwoo@gmail.com>
2016-01-21 12:04:10 -0800

    Try a different strategy for calculating waveform

--- greenfield/app/models/greenfield/waveform.rb
+++ greenfield/app/models/greenfield/waveform.rb
@@ -8,28 +8,35 @@ module Greenfield
                                '--decode', Shellwords.shellescape(file),
                                Shellwords.shellescape(tmp.path)])
 
-      # read in the data and make it mono
+      # lame can only downsample to 8KHz, but that's still
+      # way too high so we do a second resampling here
+      # computing a RMS-like quantity over 500 slices
       waveform = []
       input = RubyAudio::Sound.open(tmp.path)
       begin
-        until (signal = input.read(:int, 300).to_a).size.zero?
-          signal.map!{ |s| Array(s).sum.to_f / s.size }
-          waveform.concat(signal)
+        rms = 0.0
+        rms_size = islice = 0
+        slice_size = input.info.frames / 500
+        until (signal = input.read(:int, 300)).real_size.zero?
+          signal.each do |frame|
+            mono = frame.sum
+            rms += mono * mono
+          end
+
+          rms_size += signal.real_size
+          if rms_size > slice_size
+            waveform << Math.sqrt(rms)
+            rms = 0.0
+            rms_size = 0
+          end
         end
+        waveform << Math.sqrt(rms)
       ensure
         input.close
         tmp.close!
       end
 
-      # lame can only downsample to 8KHz, but that's still
-      # way too high so we do a second resampling here
-      average(waveform.each_slice([waveform.size / 500, 1].max))
-    end
-
-    private
-
-    def self.average(signal)
-      signal.map{ |slice| Math.sqrt(slice.map{ |x| x*x }.sum) }
+      waveform
     end
   end
 end
