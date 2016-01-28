c93036f387953e9d345afaf5af5ae7336e6b42f7
Peter Woo <petersnowdonwoo@gmail.com>
2016-01-28 15:01:06 -0800

    Scope to published assets in SearchController

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -14,7 +14,7 @@ class SearchController < ApplicationController
     if params[:query]
       @query = session[:last_search] = params[:query]
       @users = User.conditions_by_like(@query).includes(:pic).paginate(:per_page => 15, :page => params[:page])
-      @assets = Asset.conditions_by_like(@query).includes(:user => :pic).paginate(:per_page => 15, :page => params[:page])
+      @assets = Asset.published.conditions_by_like(@query).includes(:user => :pic).paginate(:per_page => 15, :page => params[:page])
       @page_title = "#{(@query)} songs and #{@query} artists"
     else
       @page_title = "Search artists and uploads"
