f35a6e32577d9d4839b8bcca3da49cb74c915d38
bells <ben@modernthings.net>
2016-02-14 20:56:53 -0700

    Refactor CSS for the 2 different locations of download/external links

--- greenfield/app/helpers/greenfield/playlists_helper.rb
+++ greenfield/app/helpers/greenfield/playlists_helper.rb
@@ -3,11 +3,11 @@ module Greenfield
     def external_link_for(link)
       text = case link
       when /spotify/i
-        '<i class="fa fa-spotify"></i>Listen on Spotify'
+        '<i class="fa fa-spotify"></i>Spotify'
       when /app\=music/i
-        '<i class="fa fa-apple"></i>Listen on Apple Music'
+        '<i class="fa fa-apple"></i>Apple Music'
       when /app\=itunes/i
-        '<i class="fa fa-apple"></i>Buy on iTunes'
+        '<i class="fa fa-apple"></i>iTunes'
       else
         '<i class="fa fa-link"></i>Website'
       end.html_safe
