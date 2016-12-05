950866a1263f1c8aa9bed8f69e0127f4043cb506
Sudara <sudara@alonetone.com>
2016-12-05 04:13:38 +0100

    Fix login

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -7,7 +7,7 @@ class UserSessionsController < ApplicationController
   end
 
   def create
-    @user_session = UserSession.new(params[:user_session].merge({:remember_me => true})) #always stay logged in
+    @user_session = UserSession.new(user_session_params.merge({:remember_me => true})) #always stay logged in
     if @user_session.save
       redirect_back_or_default(user_home_path(@user_session.user))
     else
@@ -30,5 +30,10 @@ class UserSessionsController < ApplicationController
     else
       redirect_to login_path, :error => "You weren't logged in to begin with, old chap/dame!"
     end
+  end
+  
+  private
+  def user_session_params
+    params.require(:user_session).permit(:password, :login)
   end
 end
