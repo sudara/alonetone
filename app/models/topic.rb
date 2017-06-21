2cbfb6af48393bd99f5cd4a7565c0522e4936e12
Sudara <sudara@alonetone.com>
2017-06-21 22:09:21 +0200

    Fix forum index sql  app/models/topic.rb

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -14,51 +14,51 @@ class Topic < ActiveRecord::Base
   scope :for_footer, -> { recent.not_spam.includes(:recent_post => :user).includes(:forum).limit(3) }
   # creator of forum topic
   belongs_to :user
-  
+
   # creator of recent post
   belongs_to :last_user, :class_name => "User"
   belongs_to :forum, :counter_cache => true
 
   has_many :posts, :dependent => :delete_all
-   
+
   has_one  :recent_post,
     -> { not_spam.order('posts.created_at DESC') },
     :class_name => "Post"
-  
+
   has_many :voices,
     -> { distinct },
     :through => :posts, :source => :user
-  
+
   validates_presence_of :user_id, :forum_id, :title
-  
+
   validates_presence_of :body, :on => :create
 
   attr_accessor :body
   attr_readonly :posts_count, :hits
-  
+
   has_permalink :title, true
-                               
+
   # hacks for defensio
   def article
     self
   end
-  
-  def author_name 
+
+  def author_name
     user.login
   end
-  
+
   def editable_by?(user)
     user && (user.id == user_id || user.moderator? || user.admin?)
   end
-  
+
   def full_permalink
     "http://#{Alonetone.url}/forums/#{permalink}"
   end
-  
+
   def sticky?
     sticky == 1
   end
-  
+
   def hit!
     self.class.increment_counter :hits, id
   end
@@ -66,7 +66,7 @@ class Topic < ActiveRecord::Base
   def paged?
     posts_count > Post.per_page
   end
-  
+
   def last_page
     [(posts_count.to_f / Post.per_page.to_f).ceil.to_i, 1].max
   end
@@ -74,7 +74,7 @@ class Topic < ActiveRecord::Base
   def update_cached_post_fields(post)
     # these fields are not accessible to mass assignment
     if remaining_post = post.frozen? ? recent_post : post
-      update_columns(:last_updated_at => remaining_post.created_at, 
+      update_columns(:last_updated_at => remaining_post.created_at,
         :last_user_id => remaining_post.user_id,
         :last_post_id => remaining_post.id)
       Topic.reset_counters id, :posts
@@ -82,13 +82,13 @@ class Topic < ActiveRecord::Base
       self.destroy
     end
   end
-  
+
   def to_param
     permalink
   end
 
   def self.replied_to_by(user)
-    user.posts.not_spam.select('distinct posts.topic_id, topics.*').order('topics.last_updated_at DESC').limit(5).joins(:topic)
+    user.posts.not_spam.select('distinct posts.topic_id,posts.created_at,topics.*').order('topics.last_updated_at DESC').limit(5).joins(:topic)
   end
 
   def self.popular
@@ -100,7 +100,7 @@ class Topic < ActiveRecord::Base
   end
 
 protected
-  
+
   def set_default_attributes
     self.sticky          ||= 0
     self.last_updated_at ||= Time.now.utc
@@ -118,11 +118,11 @@ protected
     Forum.decrement_counter(:topics_count, @old_forum_id)
     Forum.increment_counter(:topics_count, forum_id, touch: true)
   end
-  
+
   def count_user_posts_for_counter_cache
     @user_posts = posts.group_by { |p| p.user_id }
   end
-  
+
   def update_cached_forum_and_user_counts
     Forum.where(:id => forum_id).update_all "posts_count = posts_count - #{posts_count}"
     @user_posts.each do |user_id, posts|
