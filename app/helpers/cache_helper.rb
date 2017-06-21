16117b68424288baa7003b879aa50d14deccf274
Sudara <sudara@alonetone.com>
2017-06-21 21:43:21 +0200

    fix cache_digest helper's invalid sql on mysql 5.7

--- app/helpers/cache_helper.rb
+++ app/helpers/cache_helper.rb
@@ -1,7 +1,7 @@
-module CacheHelper 
+module CacheHelper
   # A humble attempt to create better lazy expiration keys
-  
-  # creates a hash of the count + last updated_at for the 
+
+  # creates a hash of the count + last updated_at for the
   def cache_digest(collection)
     if collection.is_a?(WillPaginate::Collection) || collection.is_a?(Array)
       collection_key =  Digest::MD5.hexdigest(collection.collect{|a| a.updated_at}.join)
@@ -10,7 +10,7 @@ module CacheHelper
       collection_key = collection.maximum(:updated_at).try(:utc).try(:to_s, :number)
       name = collection.name
     end
-    "#{name}/all-#{collection.count}-#{collection_key}"
+    "#{name}/all-#{collection.reorder('').count}-#{collection_key}"
   end
 
 end
