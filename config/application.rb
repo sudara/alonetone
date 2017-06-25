306effa4c39d228d7bf69b13cd072bf5e2908967
Sudara <sudara@alonetone.com>
2017-06-25 18:25:53 +0200

    Will greenfield css compile now?

--- config/application.rb
+++ config/application.rb
@@ -12,11 +12,11 @@ module Alonetone
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.exceptions_app = self.routes
-    
+
     config.assets.paths.concat(
       Compass::Frameworks::ALL.map { |f| f.stylesheets_directory }
     )
-    
+
     config.assets.quiet = true
   end
 end
