181ec62e1df58ee85102cef6f67c05ad7b253c23
Sudara <sudara@alonetone.com>
2017-06-27 01:49:08 +0200

    White Theme toggle

---
+++ db/migrate/20170626233640_add_white_theme_toggle_to_users.rb
@@ -0,0 +1,16 @@
+class AddWhiteThemeToggleToUsers < ActiveRecord::Migration[5.1]
+  def change
+    remove_column :users, :last_session_at
+    remove_column :users, :remember_token
+    remove_column :users, :remember_created_at
+    remove_column :users, :identidy_url
+    remove_column :users, :token_expires_at
+    remove_column :users, :token
+    remove_column :users, :pic_id
+    remove_column :users, :myspace
+    remove_column :users, :extended_bio
+    remove_column :users, :deleted_at
+
+    add_column :users, :white_theme_enabled, :boolean, default: false
+  end
+end
