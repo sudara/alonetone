8f7989d82e90f3eb6bcfd6b0ab81f4b60548f7aa
Sudara <sudara@alonetone.com>
2017-07-03 23:48:17 +0200

    Add versions to migrations

--- db/migrate/20161014210316_add_has_details_to_playlist.rb
+++ db/migrate/20161014210316_add_has_details_to_playlist.rb
@@ -1,5 +1,5 @@
-class AddHasDetailsToPlaylist < ActiveRecord::Migration
-  def change  
+class AddHasDetailsToPlaylist < ActiveRecord::Migration[5.0]
+  def change
     add_column :playlists, :has_details, :boolean, default: false
   end
 end
