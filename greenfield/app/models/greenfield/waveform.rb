f0602b4d513d6b3c2bae9abb3b3d07d27173ca24
Sudara <sudara@alonetone.com>
2017-08-04 01:20:43 +0200

    Fix call to Paperclip.run

--- greenfield/app/models/greenfield/waveform.rb
+++ greenfield/app/models/greenfield/waveform.rb
@@ -4,9 +4,9 @@ module Greenfield
       tmp = Tempfile.new(['resampled-upload', '.wav'])
 
       # resample the mp3 down to 8KHz to make it more manageable
-      command = Paperclip.run(['lame', '--mp3input', '--resample', '8',
+      command = Paperclip.run('lame', ['--mp3input', '--resample', '8',
                                '--decode', Shellwords.shellescape(file),
-                               Shellwords.shellescape(tmp.path)])
+                               Shellwords.shellescape(tmp.path)].join(' '))
 
       # lame can only downsample to 8KHz, but that's still
       # way too high so we do a second resampling here
