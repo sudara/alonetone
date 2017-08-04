826b7d5e082ee4616a8daeb8ade302b0da220b25
Sudara <sudara@alonetone.com>
2017-08-04 20:47:29 +0200

    Make production multithreaded. Fix puma server locally.

--- config/puma.rb
+++ config/puma.rb
@@ -1,16 +1,15 @@
 #!/usr/bin/env puma
-
-environment 'production'
-workers 4
-threads 1,1
-queue_requests false
-prune_bundler
+environment ENV.fetch("RAILS_ENV") { "production" }
+if ENV.fetch("RAILS_ENV") == "production"
+workers 3
 daemonize true
-pidfile 'tmp/puma.pid'
-stdout_redirect 'log/puma.log', 'log/puma.log', true
-
 bind 'unix://tmp/puma.sock'
 state_path 'tmp/puma.state'
+end
+threads 1,4
+prune_bundler
+pidfile 'tmp/puma.pid'
+stdout_redirect 'log/puma.log', 'log/puma.log', true
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
