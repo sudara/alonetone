275e40f816ae5ba0729ec7027fac19d69e45aa58
Sudara <sudara@alonetone.com>
2017-08-05 02:15:54 +0200

    Don't get greedy; remove social metadata for now

--- config/puma.rb
+++ config/puma.rb
@@ -5,11 +5,11 @@ workers 3
 daemonize true
 bind 'unix://tmp/puma.sock'
 state_path 'tmp/puma.state'
+stdout_redirect 'log/puma.log', 'log/puma.log', true
 end
 threads 1,4
 prune_bundler
 pidfile 'tmp/puma.pid'
-stdout_redirect 'log/puma.log', 'log/puma.log', true
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
