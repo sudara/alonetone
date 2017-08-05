ec4734ebfde17dac3bd6a396b842535e515e1249
Sudara <sudara@alonetone.com>
2017-08-05 02:35:17 +0200

    Initial rakismet integration for assets

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,6 +43,8 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  config.rakismet.test = false # actually send query to akismet
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
