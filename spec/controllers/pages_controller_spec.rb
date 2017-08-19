98a9483b0a407374cad66ac2c97c683612e4edfc
Sudara <sudara@alonetone.com>
2017-08-19 22:59:13 +0200

    Fix pages specs

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,7 +3,17 @@ require "rails_helper"
 RSpec.describe PagesController, type: :controller do
 
   it "should have an about page that renders without errors" do
-    get :index
+    get :about
+    expect(response).to be_success
+  end
+
+  it "should have a stats page that renders without errors" do
+    get :stats
+    expect(response).to be_success
+  end
+
+  it "should have a contact page that renders without errors" do
+    get :press
     expect(response).to be_success
   end
 
