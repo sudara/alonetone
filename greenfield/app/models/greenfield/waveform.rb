5d2bdee1d24fa65002931a874567a65ac1612065
Sudara <sudara@alonetone.com>
2017-08-19 17:34:12 +0200

    We don't need decimal places on waveform data

--- greenfield/app/models/greenfield/waveform.rb
+++ greenfield/app/models/greenfield/waveform.rb
@@ -26,7 +26,7 @@ module Greenfield
 
           rms_size += signal.real_size
           if rms_size > slice_size
-            waveform << Math.sqrt(rms)
+            waveform << Math.sqrt(rms).round
             rms = 0.0
             rms_size = 0
           end
