714a386709e0b46daa2e95c39d36d9d9aeb436a8
Sudara <sudara@alonetone.com>
2017-08-19 18:26:49 +0200

    Adjust waveform migration

--- db/migrate/20170819160514_move_waveform_data.rb
+++ db/migrate/20170819160514_move_waveform_data.rb
@@ -2,7 +2,8 @@ class MoveWaveformData < ActiveRecord::Migration[5.1]
   def up
     change_column :audio_features, :waveform, :text, :limit => 16777215
     Asset.where('waveform is not null').each do |asset|
-      asset.create_audio_feature(waveform: asset.waveform) if asset.waveform.present?
+      if asset.waveform.present? and asset.waveform.is_a? Array
+        asset.create_audio_feature(waveform: asset.waveform)
     end
   end
 
