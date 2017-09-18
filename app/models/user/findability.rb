7c123829537e8eb39b9c190f068a476559761c8f
Sudara <sudara@alonetone.com>
2017-09-18 15:32:55 -0600

    Remove "on twitter" sorting of users

--- app/models/user/findability.rb
+++ app/models/user/findability.rb
@@ -1,57 +1,57 @@
 class User
-    
+
   def self.currently_online
      User.where(["last_request_at > ?", Time.now.utc-15.minutes])
   end
-  
-  def self.conditions_by_like(value) 
+
+  def self.conditions_by_like(value)
     conditions = ['users.display_name','users.login','users.bio','users.city','users.country'].collect do |c|
-      "#{c} LIKE " + ActiveRecord::Base.connection.quote("%#{value}%") 
+      "#{c} LIKE " + ActiveRecord::Base.connection.quote("%#{value}%")
     end
     where(conditions.join(" OR "))
   end
-  
+
   def self.search(query, options = {})
     with_scope :find => { :conditions => build_search_conditions(query) } do
       find :all, options
     end
   end
-  
+
   def self.build_search_conditions(query)
     query && ['LOWER(display_name) LIKE :q OR LOWER(login) LIKE :q', {:q => "%#{query}%"}]
   end
-  
+
   # feeds the users/index subnav
   def self.paginate_by_params(params)
     if params[:sort] == 'dedicated_listeners'
       User.dedicated_listeners(params[:page] || 1, params[:per_page] || 15)
     else
-      subnav = %w(recently_joined monster_uploaders on_twitter last_uploaded)
+      subnav = %w(recently_joined monster_uploaders last_uploaded)
       params[:sort] = 'last_seen' if !params[:sort].present? or !subnav.include?(params[:sort])
       User.send(params[:sort]).paginate(:page => params[:page], :per_page => 15)
     end
   end
 
   protected
-  
+
 
   # needed to map incoming params to scopes
-  def self.last_seen 
+  def self.last_seen
     recently_seen
   end
-  
-  def self.recently_joined 
+
+  def self.recently_joined
     activated
   end
-  
-  def self.monster_uploaders 
+
+  def self.monster_uploaders
     musicians
   end
-  
+
   def self.last_uploaded
     includes(:assets).order('assets.created_at DESC')
   end
-  
+
   def self.dedicated_listeners(page, per_page)
    entries = WillPaginate::Collection.create(page, per_page) do |pager|
       # returns an array, like so: [User, number_of_listens]
@@ -67,7 +67,7 @@ class User
     end
     entries
   end
-  
+
   def geocode_address
     return unless city && country
     geo=GeoKit::Geocoders::MultiGeocoder.geocode([city, country].compact.join(', '))
