690e5ec764f0f468cc87858d946a8fc764d4e402
bells <ben@modernthings.net>
2017-09-25 23:09:58 -0600

    JSTune diving in to seperate animated SVG icons

--- app/assets/javascripts/white_theme.js
+++ app/assets/javascripts/white_theme.js
@@ -31,129 +31,155 @@
 	document.addEventListener("turbolinks:load", function() {
 		
 
-		function PlayListAnim(thisPlaylist) {
+		$(".sprites-play").each(function(){
+			$(this).find("svg").prependTo( $(this).find("a") );
+		})
 
+		function PlayListAnim(thisClicked) {
 
-		  var xmlns = "http://www.w3.org/2000/svg",
-		    xlinkns = "http://www.w3.org/1999/xlink",
-		    select = function(s) {
-		      return document.querySelector(s);
-		    },
-		    selectAll = function(s) {
-		      return document.querySelectorAll(s);
-		    }, 
-		    mainSVG = select('#testSVG'),
-		    
-		    mainTl, dottyRotationTl, spinballTl, pauseTl,
-		      pauseGroup = select('.pauseGroup'),
-		      spinballGroup = select('.spinballGroup'),
-		      outline = select('.outline'),
-		      dotty = selectAll('.dotty'),
-		      icon = select('.icon'),
-		      outlinePath = "M300,545C164.69,545,55,435.31,55,300S164.69,55,300,55,545,164.69,545,300,435.31,545,300,545Z"
+			var xmlns = "http://www.w3.org/2000/svg";
+			
+			var xlinkns = "http://www.w3.org/1999/xlink";
 
-		  this.init = function() {
-		    TweenMax.set(mainSVG, {
-		      visibility: 'visible'
-		    })
+			var select = function(s) {
+			  return document.querySelector(s);
+			};
+
+			var selectAll = function(s) {
+			  return document.querySelectorAll(s);
+			};
+
+			// var mainSVG = select('#testSVG');
+			
+			var mainSVG = $( $(thisClicked).find("svg") )[0];
+
+			console.log ( $( $(thisClicked).find(".spinballGroup") ) ); 
+
+			var mainTl;
+			var dottyRotationTl;
+			var spinballTl;
+			var pauseTl;
+			var pauseGroup = select('.pauseGroup');
+			var spinballGroup = select('.spinballGroup');
+			// var spinballGroup = $( $(thisClicked).find(".spinballGroup") );
+			
+			var outline = select('.outline');
+			
+			var dotty = selectAll('.dotty');
+			var icon = select('.icon');
+			
+			var outlinePath = "M300,545C164.69,545,55,435.31,55,300S164.69,55,300,55,545,164.69,545,300,435.31,545,300,545Z";
+
+			this.init = function() {
+				
+				TweenMax.set(mainSVG, {
+					visibility: 'visible'
+				})
+
+				TweenMax.set(dotty, {
+					transformOrigin:'50% 50%',
+					scale:1.3
+				})
+			
+				TweenMax.set(spinballGroup, {
+					transformOrigin:'50% 50%',
+					scale:0
+				})
+
+				TweenMax.set(pauseGroup, {
+					transformOrigin:'50% 50%',
+					scaleY:0
+				})
+
+				pauseTl = new TimelineMax({}).timeScale(1);
+				dottyRotationTl = new TimelineMax({}).timeScale(1);
+				spinballTl = new TimelineMax({}).timeScale(1);
+				mainTl = new TimelineMax({paused:true}).timeScale(2.2);
+
+				dottyRotationTl.to(dotty, 4, {
+					rotation:-360,
+					repeat:-1,
+					ease:Linear.easeNone
+				})
 		   
-		   TweenMax.set(dotty, {
-		    transformOrigin:'50% 50%',
-		    scale:1.3
-		   })
-		  TweenMax.set(spinballGroup, {
-		    transformOrigin:'50% 50%',
-		    scale:0
-		   })
-		  TweenMax.set(pauseGroup, {
-		    transformOrigin:'50% 50%',
-		    scaleY:0
-		   })
+				mainTl.addLabel('setPlay')
+					.addLabel('showLoading')
+					.to(icon, 1, {
+					morphSVG:{shape:outlinePath, shapeIndex:'auto'},
+					ease:Power1.easeInOut
+				})
+				.to(dotty, 1, {
+					scale:1,
+					ease:Power2.easeOut
+				},'-=1')
+				.to(outline,0.5, {
+					strokeWidth:16,
+					ease:Linear.easeNone
+				},'-=0.5')
+				.to(spinballGroup, 1, {
+					scale:1,
+					ease:Power1.easeInOut
+				},'-=1')
+				.addPause()
+				.addLabel('showPause')
+				.to(spinballGroup, 1, {
+					scale:0,
+					ease:Elastic.easeOut.config(0.3, 0.9)
+				})
+				.to(spinballGroup, 0.2, {
+				autoAlpha:0
+				},'-=1')   
+				.to(pauseGroup, 2, {
+				scaleY:0.7,
+				ease:Elastic.easeOut.config(1, 0.5)
+				},'-=1')   
+				.to(dotty, 1, {
+				scale:1.3,
+				ease:Elastic.easeOut.config(0.3, 0.9)
+				},'-=2') 
+				.addLabel('setPause')
+				spinballTl.to(spinballGroup, 2, {
+				rotation:360,
+				ease:Linear.easeNone,
+				repeat:-1
+				})
 
-		    pauseTl = new TimelineMax({}).timeScale(1);
-		    dottyRotationTl = new TimelineMax({}).timeScale(1);
-		    spinballTl = new TimelineMax({}).timeScale(1);
-		    mainTl = new TimelineMax({paused:true}).timeScale(2.2);
+			}
 
-		    dottyRotationTl.to(dotty, 4, {
-		     rotation:-360,
-		     repeat:-1,
-		     ease:Linear.easeNone
-		    })
-		   
-		   mainTl.addLabel('setPlay')
-		   .addLabel('showLoading')
-		    .to(icon, 1, {
-		    morphSVG:{shape:outlinePath, shapeIndex:'auto'},
-		    ease:Power1.easeInOut
-		   })
-		   .to(dotty, 1, {
-		    scale:1,
-		    ease:Power2.easeOut
-		   },'-=1')
-		   .to(outline,0.5, {
-		    strokeWidth:16,
-		    ease:Linear.easeNone
-		   },'-=0.5')
-		   .to(spinballGroup, 1, {
-		    scale:1,
-		    ease:Power1.easeInOut
-		   },'-=1')
-		   .addPause()
-		   //
-		   .addLabel('showPause')
-		  .to(spinballGroup, 1, {
-		    scale:0,
-		    ease:Elastic.easeOut.config(0.3, 0.9)
-		   })
-		  .to(spinballGroup, 0.2, {
-		    autoAlpha:0
-		   
-		   },'-=1')   
-		   .to(pauseGroup, 2, {
-		    scaleY:0.7,
-		    ease:Elastic.easeOut.config(1, 0.5)
-		   },'-=1')   
-		   .to(dotty, 1, {
-		    scale:1.3,
-		    ease:Elastic.easeOut.config(0.3, 0.9)
-		   },'-=2') 
-		   .addLabel('setPause')
-		   
-		   spinballTl.to(spinballGroup, 2, {
-		    rotation:360,
-		    ease:Linear.easeNone,
-		    repeat:-1
-		   })
+			this.play = function(pos) {
+				if(pos){mainTl.play(pos);} return;    
+				mainTl.play();
+			}
+			
+			this.pause = function(pos) {
+				if(pos){mainTl.pause(pos);} return;    
+				mainTl.pause();
+			}
+			
+			this.timeline = function() {
+				return mainTl;
+			}
+			
+			this.setPlay = function(){
+				mainTl.pause('setPlay')
+			}
+			
+			this.showLoading = function(){
+				mainTl.play('showloading')
+			}
+			
+			this.showPause = function(){
+				mainTl.play('showPause')
+			}
+			
+			this.setPause = function(){
+				mainTl.pause('setPause')
+			}
+			this.svg = function(){
+				return mainSVG
+			}
 
-		  }
 
-		  this.play = function(pos) {
-		   if(pos){mainTl.play(pos);} return;    
-		    mainTl.play();
-		  }
-		  this.pause = function(pos) {
-		   if(pos){mainTl.pause(pos);} return;    
-		    mainTl.pause();
-		  }
-		  this.timeline = function() {
-		    return mainTl;
-		  }
-		  this.setPlay = function(){
-		   mainTl.pause('setPlay')
-		  }
-		  this.showLoading = function(){
-		   mainTl.play('showloading')
-		  }
-		  this.showPause = function(){
-		   mainTl.play('showPause')
-		  }
-		 this.setPause = function(){
-		   mainTl.pause('setPause')
-		  }
-		 this.svg = function(){
-		   return mainSVG
-		  }
 		}
 
 		//you must initialise it
@@ -178,20 +204,24 @@
 
 		document.body.onclick = function(e){
 
-		var myPlayListAnim = new PlayListAnim();
+		  // var myPlayListAnim = new PlayListAnim();
 
-		  myPlayListAnim.init();
-		  oldIcon = currentIcon;
-		  currentIcon = e.target;
+		  // myPlayListAnim.init();
+		  
+		  // myPlayListAnim.setPlay()
+		  
+		  //oldIcon = currentIcon;
+		  //
+		  //currentIcon = e.target;
+//
+		  //if(oldIcon){
+		  //  oldIcon.setAttribute('opacity', 1);
+		  //}
+		  //currentIcon.setAttribute('opacity', 0);
 
-		  if(oldIcon){
-		    oldIcon.setAttribute('opacity', 1);
-		  }
-		  currentIcon.setAttribute('opacity', 0);
+		  // var svg = e.target.parentNode.appendChild(myPlayListAnim.svg());
 
-		  var svg = e.target.parentNode.appendChild(myPlayListAnim.svg());
-
-		  myPlayListAnim.showLoading()
+		  // myPlayListAnim.showLoading()
 
 		/*
 		 if(myPlayListAnim.timeline().time() == 0){
@@ -211,23 +241,25 @@
 
 
 
-		// $("div.sprites-play").click(function(e) {
+		 $("div.sprites-play").click(function(e) {
+			
+			var myPlayListAnim = new PlayListAnim($(this));
+			
+			myPlayListAnim.init();
 
-		// 	var myPlayListAnim = new PlayListAnim($(this));
-
-		// 	  myPlayListAnim.init();
-		// 	  oldIcon = currentIcon;
-		// 	  currentIcon = e.target;
-
-		// 	  if(oldIcon){
-		// 	    oldIcon.setAttribute('opacity', 1);
-		// 	  }
-		// 	  currentIcon.setAttribute('opacity', 0);
-
-		// 	  var svg = e.target.parentNode.appendChild(myPlayListAnim.svg());
-
-		// 	  myPlayListAnim.showLoading()
-		// })
+			myPlayListAnim.setPlay();
+			// myPlayListAnim.showLoading();
+			
+			// oldIcon = currentIcon;
+			// currentIcon = e.target;
+			// if(oldIcon){
+			// oldIcon.setAttribute('opacity', 1);
+			// }
+			// currentIcon.setAttribute('opacity', 0);
+			// var svg = e.target.parentNode.appendChild(myPlayListAnim.svg());
+			
+			// myPlayListAnim.showLoading()
+		 })
 
 
 		////
