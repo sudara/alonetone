38d8493d72069e393bf5de82017b1c923954f3db
Sudara <sudara@alonetone.com>
2017-10-12 00:31:01 +0200

    Remove pagination from and add options to /users

--- app/models/user/findability.rb
+++ app/models/user/findability.rb
@@ -23,13 +23,9 @@ class User
 
   # feeds the users/index subnav
   def self.paginate_by_params(params)
-    if params[:sort] == 'dedicated_listeners'
-      User.dedicated_listeners(params[:page] || 1, params[:per_page] || 15)
-    else
-      subnav = %w(recently_joined monster_uploaders last_uploaded)
-      params[:sort] = 'last_seen' if !params[:sort].present? or !subnav.include?(params[:sort])
-      User.send(params[:sort]).paginate(:page => params[:page], :per_page => 15)
-    end
+    available_sortings = %w(last_uploaded most_listened_to new_artists monster_uploaders dedicated_listeners)
+    params[:sort] = 'last_seen' if !params[:sort].present? or !available_sortings.include?(params[:sort])
+    User.send(params[:sort])
   end
 
   protected
@@ -37,35 +33,33 @@ class User
 
   # needed to map incoming params to scopes
   def self.last_seen
-    recently_seen
+    recently_seen.limit(30)
   end
 
   def self.recently_joined
-    activated
+    activated.limit(30)
+  end
+
+  def self.new_artists
+    musicians.reorder('users.created_at DESC').limit(30)
+  end
+
+  def self.most_listened_to
+    result = Listen.since(1.month.ago).group(:track_owner).order('count_all DESC').limit(30).count
+    result.collect(&:first)
   end
 
   def self.monster_uploaders
-    musicians
+    musicians.reorder('users.assets_count DESC').limit(30)
   end
 
   def self.last_uploaded
-    includes(:assets).order('assets.created_at DESC')
+    includes(:assets).order('assets.created_at DESC').limit(30)
   end
 
-  def self.dedicated_listeners(page, per_page)
-   entries = WillPaginate::Collection.create(page, per_page) do |pager|
-      # returns an array, like so: [User, number_of_listens]
-      result = Listen.since(1.month.ago).where('listener_id is not null').group(:listener).order('count_all DESC').limit(pager.per_page).offset(pager.offset).count
-
-      # inject the result array into the paginated collection:
-      pager.replace(result.collect(&:first))
-
-      unless pager.total_entries
-        # the pager didn't manage to guess the total count, do it manually
-        pager.total_entries = Listen.where('listener_id != ""').count(:listener_id)
-      end
-    end
-    entries
+  def self.dedicated_listeners
+    result = Listen.since(1.month.ago).where('listener_id is not null').group(:listener).order('count_all DESC').limit(30).count
+    result.collect(&:first)
   end
 
   def geocode_address
