5c8f8446f6fed84bbc849a796e2aefb976b84862
bells <ben@modernthings.net>
2017-10-30 16:01:10 -0600

    Handle 1-3 Playlists on a User page, to bybass the usual flex position for many more playlists

--- app/assets/javascripts/white_theme/flexbox.js
+++ app/assets/javascripts/white_theme/flexbox.js
@@ -2,22 +2,21 @@
 
     document.addEventListener("turbolinks:load", function() {
         
-        console.log( $("ul.playlists") );
-
+        // If the playlists on the user page extend to more than one line,
+        // use this technique for aligning the orphan row
         if ( $("#user_playlists_area ul.playlists").height() > 300 ) {
-
 	        for (var i = 0; i < 8; i++) {
-				
 				var $flexInvisibleDiv = $("<li></li>").appendTo( $("#user_playlists_area ul.playlists") );
-
 				$flexInvisibleDiv.width( $flexInvisibleDiv.prev().width() );
-
 	        }
-        	
+        }
+        
+        // If the playlists on the user page are 3 or less,
+        // skip the usual flex-box formatting with this added class
+        if ( ( $("#user_playlists_area ul.playlists").length > 0 ) && ( $("#user_playlists_area ul.playlists li").length < 4 ) ) {
+            $("#user_playlists_area ul.playlists").addClass("three_or_less_playlists");
         }
 
-
-    
     });
 
 })(jQuery);
