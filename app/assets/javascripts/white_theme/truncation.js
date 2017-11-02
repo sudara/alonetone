b19da77014851afc25e4efe765fe98428f4d46d9
bells <ben@modernthings.net>
2017-11-01 23:56:43 -0600

    Fix bug that was hiding the Track names on search result assets

--- app/assets/javascripts/white_theme/truncation.js
+++ app/assets/javascripts/white_theme/truncation.js
@@ -3,11 +3,11 @@
     var playButtonAndTimeWidth = 90; // make this dynamic?
 
     $( window ).resize(function() {
-      $("a.track_link").width( $(".box").width() - playButtonAndTimeWidth );
+      $("a.track_link").width( $(".asset").width() - playButtonAndTimeWidth );
     });
 
     document.addEventListener("turbolinks:load", function() {
-        $("a.track_link").width( $(".box").width() - playButtonAndTimeWidth );
+        $("a.track_link").width( $(".asset").width() - playButtonAndTimeWidth );
     });
 
 })(jQuery);
