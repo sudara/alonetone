5bfc8d49e04c489779df18e381dd92fe3244fb2e
Sudara <sudara@alonetone.com>
2018-03-22 01:20:20 +0100

    Rails 5.2.0.beta2 & Request Specs

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-gem 'rails', '~> 5.2.0.beta2'
+gem 'rails', '~> 5.2.0.rc2'
 gem 'mysql2'
 
 gem 'thredded'
---
+++ spec/request/user_sessions_controller_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+RSpec.describe UserSessionsController, type: :request do
+  fixtures :users
+
+  it "should fail to login with wrong password" do
+    post '/user_sessions', params: { user_session: { login: 'arthur', password: 'incorrect'} }
+    expect(session[:user_credentials]).to_not be_present
+    expect(response).to render_template('user_sessions/new')
+  end  
+
+  it "should successfully login with alonetone login" do
+    post '/user_sessions', params: { user_session: { login: 'arthur', password: 'test'} }
+    expect(session[:user_credentials]).to be_present
+  end
+
+end
