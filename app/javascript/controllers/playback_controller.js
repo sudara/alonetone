d508d4b9ad691519238062a88db7310910ac1994
Sudara <sudara@alonetone.com>
2018-07-04 01:14:32 +0200

    Cache this.sound.seek() for performance

--- app/javascript/controllers/playback_controller.js
+++ app/javascript/controllers/playback_controller.js
@@ -37,6 +37,7 @@ export default class extends Controller {
   }
 
   whilePlaying() {
+    this.position = this.sound.seek()
     if (this.sound.playing()) {
       this.whilePlayingCallback()
       if (!this.nextTrackLoading && this.positionFromEnd(10000)) {
@@ -47,7 +48,7 @@ export default class extends Controller {
         this.playNextTrack()
       } else {
         // we don't want to update this 60 times a second, at most 10
-        setTimeout(requestAnimationFrame(this.whilePlaying.bind(this), 100))
+        requestAnimationFrame(this.whilePlaying.bind(this))
       }
     }
     this.calculateTime()
@@ -100,16 +101,16 @@ export default class extends Controller {
   }
 
   positionFromEnd(ms) {
-    const positionFromEnd = ((this.sound.duration() - this.sound.seek())) * 1000
+    const positionFromEnd = (this.sound.duration() - this.position) * 1000
     return ms > positionFromEnd
   }
 
   percentPlayed() {
-    return this.sound.seek() / this.sound.duration()
+    return this.position / this.sound.duration()
   }
 
   calculateTime() {
-    const time = Math.floor(this.sound.seek())
+    const time = Math.floor(this.position)
     const min = Math.floor(time / 60) 
     const sec = time % 60
     this.time = min + ':' + (sec >= 10 ? sec : '0' + sec)
