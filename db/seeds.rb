a6557344fccd6837618c06426b16ee79a55cb795
Sudara <sudara@alonetone.com>
2018-07-04 22:54:53 +0200

    Fix issues with 5.2 defaults

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,11 @@
 require 'active_record/fixtures'
 include ActionDispatch::TestProcess
 
-admin = User.create(:login => 'admin', :email => "123@123.com", :password => 'testing123', :password_confirmation => 'testing123')
-admin.update_attribute(:admin, true)
+admin = User.create(:login => 'admin', :email => "123@123.com", :password => 'testing123', :password_confirmation => 'testing123', admin: true)
 
-moderator = User.create(:login => 'moderator', :email =>'mod@mod.com', :password => 'mod123', :password_confirmation => 'mod123')
-moderator.update_attribute(:moderator, true)
+moderator = User.create(:login => 'moderator', :email =>'mod@mod.com', :password => 'mod123', :password_confirmation => 'mod123', moderator: true)
 
-musician = User.create(:login => 'musician', :email =>'music@music.com', :password => 'music123', :password_confirmation => 'music123')
-musician.update_column(:greenfield_enabled, true)
+musician = User.create(:login => 'musician', :email =>'music@music.com', :password => 'music123', :password_confirmation => 'music123', greenfield_enabled: true)
 
 mp3 = fixture_file_upload(File.join('spec/fixtures/assets','muppets.mp3'),'audio/mpeg')
 asset = musician.assets.create(:mp3 => mp3, :title => 'muppets!', :description => '*muppets* poking fun', :waveform => Greenfield::Waveform.extract(mp3.path))
