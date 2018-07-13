b94134ce8236c974889981a65dc9664c57850c42
Sudara <sudara@alonetone.com>
2018-07-13 21:01:36 +0200

    Revert "Explicitly opt in to 5.2 defaults"
    This reverts commit f21027dced10e8436a245d4d56e580685659a2e8.

--- config/application.rb
+++ config/application.rb
@@ -11,11 +11,12 @@ module Alonetone
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.load_defaults 5.2
+    config.exceptions_app = self.routes
 
     config.assets.paths.concat(
       Compass::Frameworks::ALL.map { |f| f.stylesheets_directory }
     )
+    config.active_record.sqlite3.represent_boolean_as_integer = true
     config.assets.quiet = true
   end
 end
