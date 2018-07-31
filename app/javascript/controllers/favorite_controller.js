075d8271e24108fa4684199fc42649f5c4460bc8
Sudara <sudara@alonetone.com>
2018-07-31 18:22:44 +0200

    Add favorites_controller

---
+++ app/javascript/controllers/favorite_controller.js
@@ -0,0 +1,27 @@
+import { Controller } from 'stimulus'
+import FaveAnimation from '../animation/fave_animation'
+
+export default class extends Controller {
+  static targets = ['svg']
+
+  initialize() {
+    this.id = parseInt(this.element.href.split('=')[1])
+    this.favorited = window.userFavorites.includes(this.id)
+    this.animation = new FaveAnimation(this.element)
+    this.animation.init()
+    if (this.favorited) {
+      this.animation.setFave()
+    }
+  }
+
+  // ajax request fires immediately afterwards
+  toggle() {
+    if(this.favorited){
+      this.animation.clickUnfave()
+      this.favorited = false
+    } else {
+      this.animation.clickFave()
+      this.favorited = true
+    }
+  }
+}
