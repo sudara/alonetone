f201f33be995f651e89d0d708209258f2f27604c
Sudara <sudara@alonetone.com>
2018-08-05 01:58:04 +0200

    Fix Thredded index pt II

--- spec/request/thredded_spec.rb
+++ spec/request/thredded_spec.rb
@@ -5,4 +5,10 @@ RSpec.describe "Thredded", :type => :request do
     get "/discuss"
     expect(response).to be_success
   end
+
+  it "loads the thredded index when logged in" do
+    post '/user_sessions', params: { user_session: { login: 'arthur', password: 'test'} }
+    get "/discuss"
+    expect(response).to be_success
+  end
 end
