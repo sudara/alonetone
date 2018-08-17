e2d960a0825839aad2bb9049c9587f9cce02d53c
“Jenya <jzueva@gmail.com>
2018-08-16 22:42:50 -0400

    Revert changes to seed.rb file. `authlogic` gem takes care of crypted_password, but the model requires both pwd and pwd_confirmation to generate crypted password. Not sure how it worked previously :shrugs:

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,20 @@
 require 'active_record/fixtures'
 include ActionDispatch::TestProcess
 
-admin = User.create(:login => 'admin', :email => "123@123.com", :crypted_password => 'testing123')
+admin = User.create(:login => 'admin', :email => "123@123.com", :password => 'testing123', :password_confirmation => 'testing123')
 admin.update_attribute(:admin, true)
 
-moderator = User.create(:login => 'moderator', :email =>'mod@mod.com', :crypted_password => 'mod123')
+moderator = User.create(:login => 'moderator', :email =>'mod@mod.com', :password => 'mod123', :password_confirmation => 'mod123')
 moderator.update_attribute(:moderator, true)
 
-musician = User.create(:login => 'musician', :email =>'music@music.com', :crypted_password => 'music123')
+musician = User.create(:login => 'musician', :email =>'music@music.com', :password => 'music123', :password_confirmation => 'music123')
 musician.update_column(:greenfield_enabled, true)
 
 mp3 = fixture_file_upload(File.join('spec/fixtures/assets','muppets.mp3'),'audio/mpeg')
 asset = musician.assets.create(:mp3 => mp3, :title => 'muppets!', :description => '*muppets* poking fun', :waveform => Greenfield::Waveform.extract(mp3.path))
 
 asset.listens.create(
-  :listener     => moderator, 
+  :listener     => moderator,
   :track_owner  => asset.user, 
   :user_agent   => 'db seeds',
   :ip           => '127.0.0.1'
