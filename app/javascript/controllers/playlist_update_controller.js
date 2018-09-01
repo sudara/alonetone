88aa4477d7ded84b948e5a2d07e57bf117541139
Sudara <sudara@alonetone.com>
2018-09-02 00:35:34 +0200

    Improved Playlist editing with feedback

--- app/javascript/controllers/playlist_update_controller.js
+++ app/javascript/controllers/playlist_update_controller.js
@@ -9,29 +9,65 @@ export default class extends Controller {
     this.removeUrl = document.querySelector('.remove_url').getAttribute('href')
   }
 
+  setPlaylistEdit() {
+    this.playlistEdit = this.application.getControllerForElementAndIdentifier(document.querySelector('#columns'), 'playlist-edit')
+  }
+
   add(e) {
     e.preventDefault()
+    this.setPlaylistEdit()
     Rails.ajax({
       url: this.addUrl,
       type: 'POST',
       data: `asset_id=${this.element.id}`,
-      // success (data) {
-      //   this.displaySuccess()
-      // }
+      before: this.spin.bind(this),
+      success: this.added.bind(this),
+      error: this.errored.bind(this),
     })
-    alert('add')
   }
 
   remove(e) {
     e.preventDefault()
+    this.setPlaylistEdit()
     Rails.ajax({
       url: this.removeUrl,
       type: 'GET',
-      data: `track_id=${this.element.id.replace('track_','')}`,
-      success (data) {
-        this.element.remove()
-      }
+      data: `track_id=${this.element.id.replace('track_', '')}`,
+      before: this.spin.bind(this),
+      success: this.removed.bind(this),
+      error: this.errored.bind(this),
     })
-    alert('removed')
+  }
+
+  spin() {
+    this.playlistEdit.spinnerTarget.style.display = 'block'
+  }
+
+  stopSpin() {
+    this.playlistEdit.spinnerTarget.style.display = 'none'
+  }
+
+  errored() {
+    setTimeout(this.stopSpin, 300)
+    this.playlistEdit.feedbackTarget.innerHTML = '<div class="ajax_fail">Dang, something went wrong!</div>'
+  }
+
+  removed() {
+    setTimeout(this.stopSpin, 3000)
+    this.playlistEdit.feedbackTarget.innerHTML = '<div class="ajax_success">Removed</div>'
+    this.updatePlaylistSize()
+    this.element.parentNode.removeChild(this.element)
+  }
+
+  added() {
+    setTimeout(this.stopSpin, 300)
+    this.playlistEdit.feedbackTarget.innerHTML = '<div class="ajax_success">Added!</div>'
+    this.playlistEdit.sortableTarget.appendChild(this.element)
+    this.updatePlaylistSize()
+  }
+
+  updatePlaylistSize() {
+    const size = this.playlistEdit.sortables().length
+    this.playlistEdit.sizeTarget.innerHTML = `${size}`
   }
 }
