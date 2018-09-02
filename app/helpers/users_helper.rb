c2823a69ad19cef006a5c3bfb2e0dd12b7d00032
Sudara <sudara@alonetone.com>
2018-09-02 02:44:07 +0200

    Dark theme should show dummy pics too

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -12,7 +12,7 @@ module UsersHelper
   end
 
   def avatar(user, size = nil)
-    return "default/no-pic_#{size}.png" if Alonetone.try(:default_user_images)
+    return "default/no-pic_#{size}.png" if Alonetone.try(:show_dummy_pics)
     case size
     when 100 then image_tag(user.has_pic? ? user.pic.pic.url(:large) : 'default/no-pic-thumb100.jpg')
     when 50 then image_tag(user.has_pic? ? user.pic.pic.url(:small) : 'default/no-pic-thumb50.jpg')
