d9e8f05c5ff6a37976082e62db9201acd9c35116
Jenya <jzueva@gmail.com>
2018-09-03 14:17:40 -0400

    Add a ActiveJob to handle creation of autio_feature. If an asset does not already have one, it will fire off a job as soon as a queue becomes available.

---
+++ spec/jobs/create_audio_feature_job_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe CreateAudioFeatureJob, type: :job do
+  include ActiveJob::TestHelper
+
+  fixtures :assets
+
+  after do
+    clear_enqueued_jobs
+    clear_performed_jobs
+  end
+
+  subject(:job) { described_class.perform_later(assets(:valid_arthur_mp3).id) }
+
+  it "queues the job" do
+    expect { job }.to have_enqueued_job(described_class)
+      .with(assets(:valid_arthur_mp3).id)
+      .on_queue("default")
+  end
+
+  it "executes perform_later" do
+    expect(asset).to receive(:import_waveform)
+    perform_enqueued_jobs { job }
+  end
+
+end
