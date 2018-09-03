265121bb6bafafbf52f8106d157c80a6261a72a4
“Jenya <jzueva@gmail.com>
2018-09-03 14:23:27 -0400

    Add a placeholder for sidekiq in development config file. Cleanup few things per rubocop. `asset$.import_waveform`??? NICE

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,7 +32,7 @@ Rails.application.configure do
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
-
+  # config.active_job.queue_adapter = :sidekiq
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
 
