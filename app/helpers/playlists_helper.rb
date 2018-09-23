30c8557ee36fcbd28c48e6357668d933e913d8f8
Sudara <sudara@alonetone.com>
2018-09-23 18:29:18 +0200

    Rough in external-links icons

--- app/helpers/playlists_helper.rb
+++ app/helpers/playlists_helper.rb
@@ -49,16 +49,19 @@ module PlaylistsHelper
   def external_link_for(link)
     text = case link
            when /spotify/i
-      '<i class="fa fa-spotify"></i>Spotify<span class="action-text">Stream</span>'
+            service = 'spotify'
+            '<i class="spotify"></i><span class="service-name">Spotify</span><span class="action-text">Stream</span>'
            when /app\=music/i
-      '<i class="fa fa-apple"></i>Apple Music<span class="action-text">Stream</span>'
+            service = 'apple'
+            '<i class="apple_music"></i><span class="service-name">Apple Music</span><span class="action-text">Stream</span>'
            when /app\=itunes/i
-      '<i class="fa fa-apple"></i>iTunes<span class="action-text">Buy</span>'
+            service = 'itunes'
+            '<i class="itunes"></i><span class="service-name">iTunes</span><span class="action-text">Buy</span>'
            when /play\.google/i
       '<i class="fa fa-google"></i>Google<span class="action-text">Stream</span>'
            else
       '<i class="fa fa-link"></i>Website<span class="action-text">Visit</span>'
     end.html_safe
-    link_to(text, link).html_safe
+    link_to(text, link, class: service).html_safe
   end
 end
