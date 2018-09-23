0726fb967287a374931735ff728827706bde92b3
Sudara <sudara@alonetone.com>
2018-09-23 15:06:21 +0200

    Renable the inline adapter by default in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,7 +33,10 @@ Rails.application.configure do
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
+
+  config.active_job.queue_adapter = :inline
   # config.active_job.queue_adapter = :sidekiq
+
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
 
