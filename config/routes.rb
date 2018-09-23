2db3959ce621a92dd7c4c6ef3bcc65bd3fb6e6d5
Sudara <sudara@alonetone.com>
2018-09-23 03:07:52 +0200

    Add "other users with same IP" to user profile

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Alonetone::Application.routes.draw do
     mount Thredded::Engine => '/discuss'
     get '/upload', :to => 'assets#new'
     get '/new_album', :to => 'playlists#new'
+    get '/favorites', :to => 'playlists#favorites'
     get '/login', :to => 'user_sessions#new', :as => 'login'
     get '/logout', :to => 'user_sessions#destroy', as: 'logout', via: [:get, :post]
     resources :user_sessions
