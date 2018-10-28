de56e72985caf1b5e4ba751300dd098ba65741ce
Sudara <sudara@alonetone.com>
2018-10-28 09:19:03 +0100

    Disallow raw html in comments/tracks (markdown works w/nofollow)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -71,7 +71,7 @@ module ApplicationHelper
     return "" unless text
     text = emojify(text)
     @@renderer ||= begin
-                     html = Redcarpet::Render::HTML.new(hard_wrap: true, link_attributes: { rel: "nofollow" })
+                     html = Redcarpet::Render::HTML.new(hard_wrap: true, filter_html: true, link_attributes: { rel: "nofollow" })
                      Redcarpet::Markdown.new(html, autolink: true, no_intraemphasis: true)
                    end
     Redcarpet::Render::SmartyPants.render(@@renderer.render(text)).html_safe
