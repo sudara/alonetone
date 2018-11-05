e19781893bbaca2cfeef76dd8e69d40f0cb9cd16
Jenya <jzueva@gmail.com>
2018-11-04 23:09:09 -0500

    Exclude comments that might already be spam from the scope.

--- app/controllers/admin/comments/mark_as_spam_controller.rb
+++ app/controllers/admin/comments/mark_as_spam_controller.rb
@@ -1,13 +1,17 @@
 class Admin::Comments::MarkAsSpamController < Admin::BaseController
   def create
     if params[:id]
-      comments = Comment.where(id: params[:id])
+      scope = Comment.where(id: params[:id])
     elsif params[:user_id]
-      comments = Comment.where(commenter: params[:user_id])
+      scope = Comment.where(commenter: params[:user_id])
     end
+    # limit scope to non_spam comments
+    # and we should include private comments as well
+    comments = scope.include_private
 
     comments.map(&:spam!)
     comments.update_all(is_spam: true)
+    # placeholder for now before we get Stimulujs setup
     redirect_back(fallback_location: root_path)
   end
 end
