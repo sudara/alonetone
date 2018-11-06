1c0d145efaeb696ad017fe587eae10c2f52cd512
Jenya <jzueva@gmail.com>
2018-11-05 19:35:02 -0500

    Create blocklist controller rather than AddToBlocklist. Awaiting better requirements definition.

---
+++ app/controllers/admin/blocklists_controller.rb
@@ -0,0 +1,17 @@
+module Admin
+  class BlocklistsController < Admin::BaseController
+    def create
+    end
+    # will probably be smth more specific
+    # like add_to_blocklist
+    def add
+    end
+    # will probably be smth more specific
+    # like remove_from_blocklist
+    def remove_from_blocklist
+    end
+
+    def index
+    end
+  end
+end
