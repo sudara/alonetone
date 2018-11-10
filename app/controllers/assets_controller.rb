db5d6230e5582b473b43015305daedca8c593b52
Jenya <jzueva@gmail.com>
2018-11-09 22:49:03 -0500

    Commenting out not to forget to delete and move views to new/correct routes

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -184,19 +184,19 @@ class AssetsController < ApplicationController
     end
   end
 
-  def unspam
-    @asset.ham!
-    @asset.update_column :is_spam, false
-    flash.notice = "Track was made public"
-    redirect_back(fallback_location: root_path)
-  end
+  # def unspam
+  #   @asset.ham!
+  #   @asset.update_column :is_spam, false
+  #   flash.notice = "Track was made public"
+  #   redirect_back(fallback_location: root_path)
+  # end
 
-  def spam
-    @asset.spam!
-    @asset.update_column :is_spam, true
-    flash.notice = "Track was marked as spam"
-    redirect_back(fallback_location: root_path)
-  end
+  # def spam
+  #   @asset.spam!
+  #   @asset.update_column :is_spam, true
+  #   flash.notice = "Track was marked as spam"
+  #   redirect_back(fallback_location: root_path)
+  # end
 
   def stats
     respond_to do |format|
