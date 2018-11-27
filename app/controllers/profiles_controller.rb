165f2edd50ea5d06514cd0185cf4a8b66d683290
Sudara <sudara@alonetone.com>
2018-11-27 22:56:57 +0100

    Fix indentation on ProfilesController

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,14 +1,14 @@
 class ProfilesController < ApplicationController
-    before_action :require_login, :find_user
+  before_action :require_login, :find_user
 
-    def update
-      @user.profile.update_attributes(profile_params)
-      redirect_back(fallback_location: edit_user_path(@user))
-    end
+  def update
+    @user.profile.update_attributes(profile_params)
+    redirect_back(fallback_location: edit_user_path(@user))
+  end
 
-    protected
+  protected
 
-    def profile_params
-      params[:profile].permit(:bio, :website, :twitter, :instagram, :bandcamp, :spotify, :city, :country)
-    end
+  def profile_params
+    params[:profile].permit(:bio, :website, :twitter, :instagram, :bandcamp, :spotify, :city, :country)
+  end
 end
