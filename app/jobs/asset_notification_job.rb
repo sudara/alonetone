a4e7706314419c75bd3641d7c0e31e646ccb29e6
Jenya <jzueva@gmail.com>
2018-12-03 23:34:58 -0500

    Refactor AssetNotificationJob. Instead of accepting single id, accept an array of ids and iterate over each title to support mass upload functionality NOT TESTED end to end :)

--- app/jobs/asset_notification_job.rb
+++ app/jobs/asset_notification_job.rb
@@ -1,9 +1,9 @@
 class AssetNotificationJob < ActiveJob::Base
   queue_as :mailers
 
-  def perform(asset_id, follower_id)
-    asset = Asset.find_by(id: asset_id)
-    email = User.find_by(id: follower_id).try(:email)
-    AssetNotification.upload_notification(asset, email).deliver_now if asset && email
+  def perform(asset_ids: asset_ids, user_id: user_id)
+    assets = Asset.find_by(id: asset_ids)
+    user = User.find_by(id: user_id)
+    AssetNotification.upload_notification(assets, user).deliver_now if assets && user&.email
   end
 end
