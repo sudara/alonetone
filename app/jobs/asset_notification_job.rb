10efe5b5456451ec57c5faf854925297b721c70c
Jenya <jzueva@gmail.com>
2018-12-05 17:50:40 -0500

    I202 cleanup default attributes

--- app/jobs/asset_notification_job.rb
+++ app/jobs/asset_notification_job.rb
@@ -1,7 +1,7 @@
 class AssetNotificationJob < ActiveJob::Base
   queue_as :mailers
 
-  def perform(asset_ids: asset_ids, user_id: user_id)
+  def perform(asset_ids: , user_id: )
     assets = Asset.find_by(id: asset_ids)
     user = User.find_by(id: user_id)
     AssetNotification.upload_notification(assets, user).deliver_now if assets && user&.email
