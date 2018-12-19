974fc16e444bf590a371b2827cfe4a4cbc40610b
Sudara <sudara@alonetone.com>
2018-12-19 21:02:28 +0100

    Ignore some 404s for bugsnag

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -2,4 +2,6 @@ Bugsnag.configure do |config|
   config.release_stage = "production"
   config.api_key = Alonetone.try(:bugsnag_key)
   config.auto_capture_sessions = true
+  config.ignore_classes << ActionController::UnknownFormat
+  config.ignore_classes << ActiveRecord::RecordNotFound
 end if Rails.env.production?
