f18f92639a28de6671fad7d6c4fdb6903569bc03
Jenya <jzueva@gmail.com>
2019-01-02 20:14:15 -0500

    Add shoulda-matchers gem configs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -92,3 +92,11 @@ RSpec.configure do |config|
     Percy::Capybara.finalize_build
   end
 end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
+FactoryBot.reload
