4ebe390c5fa3af00265f15060d4e04c7ffc73941
Manfred Stienstra <manfred@fngtps.com>
2019-01-05 18:30:43 +0100

    rails_helper.rb: Remove config set in spec_helper.rb.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -76,15 +76,6 @@ RSpec.configure do |config|
   config.before(:example, type: :controller) do
     activate_authlogic
   end
-
-  # Setting this config option `false` removes rspec-core's monkey patching of the
-  # top level methods like `describe`, `shared_examples_for` and `shared_context`
-  # on `main` and `Module`. The methods are always available through the `RSpec`
-  # module like `RSpec.describe` regardless of this setting.
-  # For backwards compatibility this defaults to `true`.
-  #
-  # https://relishapp.com/rspec/rspec-core/v/3-0/docs/configuration/global-namespace-dsl
-  config.expose_dsl_globally = false
 end
 
 FactoryBot.reload
