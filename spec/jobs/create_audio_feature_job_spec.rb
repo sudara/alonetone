ae2d6850e7515feba3d3fa54bf2e9d0ac3323255
Manfred Stienstra <manfred@fngtps.com>
2019-01-05 19:27:53 +0100

    Remove AJob setup from create_audio_feature_job_spec.rb

--- spec/jobs/create_audio_feature_job_spec.rb
+++ spec/jobs/create_audio_feature_job_spec.rb
@@ -1,13 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe CreateAudioFeatureJob, type: :job do
-  include ActiveJob::TestHelper
-
-  after do
-    clear_enqueued_jobs
-    clear_performed_jobs
-  end
-
   subject(:job) { described_class.perform_later(assets(:valid_arthur_mp3).id) }
 
   it "queues the job" do
