c53c0a1916d5e6d4ffe1b935c4ee02c2d7cb86db
Jenya <jzueva@gmail.com>
2019-01-09 16:11:45 -0500

    Remove lingering binding pry

--- app/helpers/cache_helper.rb
+++ app/helpers/cache_helper.rb
@@ -3,7 +3,6 @@ module CacheHelper
 
   # creates a hash of the count + last updated_at for the
   def cache_digest(collection)
-    binding.pry
     if collection.is_a?(Array)
       collection_key = Digest::MD5.hexdigest(collection.collect(&:updated_at).join)
       name = collection.first.class
