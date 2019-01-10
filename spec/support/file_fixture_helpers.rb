bb082d9da78f092d30356d9552bb05588ce93f01
Manfred Stienstra <manfred@fngtps.com>
2019-01-10 14:31:28 +0100

    Add RSpec helper to create Asset from file fixture.

--- spec/support/file_fixture_helpers.rb
+++ spec/support/file_fixture_helpers.rb
@@ -16,6 +16,22 @@ module RSpec
         FileUtils.cp(file_fixture(path), tempfile.path)
         tempfile
       end
+
+      def file_fixture_uploaded_file(path, filename: nil, content_type: nil)
+        filename ||= File.basename(path)
+        ActionDispatch::Http::UploadedFile.new(
+          tempfile: file_fixture_tempfile(path),
+          filename: filename,
+          type: content_type || 'application/octet-stream'
+        )
+      end
+
+      def file_fixture_asset(path, filename: nil, content_type: nil, user: nil)
+        Asset.create(
+          user: user || users(:sudara),
+          mp3: file_fixture_uploaded_file(path, filename: filename, content_type: content_type)
+        )
+      end
     end
   end
 end
