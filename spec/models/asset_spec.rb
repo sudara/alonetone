ed8082388f8b759de77457f8fc915905a8a880e6
Manfred Stienstra <manfred@fngtps.com>
2019-01-10 14:32:00 +0100

    Use asset file fixture helper to rewrite asset specs.

--- spec/models/asset_spec.rb
+++ spec/models/asset_spec.rb
@@ -1,13 +1,6 @@
 require "rails_helper"
 
 RSpec.describe Asset, type: :model do
-  def new_track(path)
-    Asset.create(
-      user: users(:sudara),
-      mp3: fixture_file_upload(file_fixture(path), 'audio/mpeg')
-    )
-  end
-
   context "validation" do
     it 'can be an mp3 file' do
       expect(assets(:valid_mp3)).to be_valid
@@ -32,23 +25,27 @@ RSpec.describe Asset, type: :model do
 
   context "uploading" do
     it "should only require a name and mp3 " do
-      asset = new_track('muppets.mp3')
+      asset = file_fixture_asset('muppets.mp3', content_type: 'audio/mpeg')
       expect(asset).not_to be_new_record
       expect(asset.errors).not_to be_present
     end
 
     it "should catch empty or bogus files" do
-      asset = new_track('empty.mp3')
+      asset = file_fixture_asset('empty.mp3', content_type: 'audio/mpeg')
       expect(asset).to be_new_record
       expect(asset.errors).to be_present
     end
 
     it "should increase the user's count" do
-      expect { new_track('muppets.mp3').save }.to change(Asset, :count).by(1)
+      user = users(:sudara)
+      expect do
+        file_fixture_asset('muppets.mp3', content_type: 'audio/mpeg', user: user)
+      end.to change { user.reload.assets_count }.by(+1)
     end
 
     it "should not allow zipfiles" do
-      expect { new_track('1valid-1invalid.zip').save }.not_to change(Asset, :count)
+      asset = file_fixture_asset('1valid-1invalid.zip', content_type: 'application/zip')
+      expect { asset.save }.not_to change(Asset, :count)
     end
   end
 
@@ -82,24 +79,24 @@ RSpec.describe Asset, type: :model do
 
   context "mp3 tags" do
     it "should use tag2 TT2 as name if present" do
-      asset = new_track('muppets.mp3')
+      asset = file_fixture_asset('muppets.mp3', content_type: 'audio/mpeg')
       expect(asset.name).to eq('Old Muppet Men Booing')
     end
 
     it 'should still work even when tags are empty and the name is weird' do
-      asset = new_track('_ .mp3')
+      asset = file_fixture_asset('_ .mp3', content_type: 'audio/mpeg')
       expect(asset.permalink).to eq('untitled')
       expect(asset.name).to eq('untitled')
     end
 
     it 'should handle strange charsets / characters in title tags' do
-      asset = new_track('japanese-characters.mp3')
+      asset = file_fixture_asset('japanese-characters.mp3', content_type: 'audio/mpeg')
       expect(asset.name).to eq('01-¶ÔµÄÈË') # name is still 01-\266Ե??\313"
       expect(asset.mp3_file_name).to eq('japanese-characters.mp3')
     end
 
     it 'should handle empty name in mp3 tag' do
-      asset = new_track('japanese-characters.mp3')
+      asset = file_fixture_asset('japanese-characters.mp3', content_type: 'audio/mpeg')
       expect(asset.permalink).to eq("01-oaee") # name is 01-\266Ե??\313"
       asset.title = 'bee'
       asset.save
@@ -107,53 +104,53 @@ RSpec.describe Asset, type: :model do
     end
 
     it 'should cope with non-english filenames' do
-      asset = new_track('中文測試.mp3')
+      asset = file_fixture_asset('中文測試.mp3', content_type: 'audio/mpeg')
       expect(asset.save).to eq(true)
       asset.mp3_file_name == '中文測試.mp3'
     end
 
     it 'should handle umlauts and non english characters in the filename' do
-      asset = new_track('müppets.mp3')
-      expect(asset.mp3_file_name).to eq('müppets.mp3')
+      filename = 'müppets.mp3'.mb_chars.normalize
+      asset = file_fixture_asset(
+        'muppets.mp3',
+        filename: filename,
+        content_type: 'audio/mpeg'
+      )
+      expect(asset.mp3_file_name).to eq(filename)
     end
 
     it 'should handle permalink with ???? as tags, default to untitled' do
-      asset = new_track('中文測試.mp3')
+      asset = file_fixture_asset('中文測試.mp3', content_type: 'audio/mpeg')
       expect(asset.name).to eq("中文測試")
       expect(asset.permalink).not_to be_blank
       expect(asset.permalink).to eq("untitled")
     end
 
     it 'should use the mp3 tag1 title as name if present' do
-      asset = new_track('tag1.mp3')
+      asset = file_fixture_asset('tag1.mp3', content_type: 'audio/mpeg')
       expect(asset.name).to eq("Mark S Williams")
     end
 
     it 'should use the filename as name if no tags are present' do
-      asset = new_track('titleless.mp3')
+      asset = file_fixture_asset('titleless.mp3', content_type: 'audio/mpeg')
       expect(asset.name).to eq('Titleless')
     end
 
     it 'should generate a permalink from tags' do
-      asset = new_track('tag2.mp3')
+      asset = file_fixture_asset('tag2.mp3', content_type: 'audio/mpeg')
       expect(asset.permalink).to eq('put-a-nickel-on-my-door')
     end
 
     it 'should generate unique permalinks' do
-      asset = new_track('tag2.mp3')
-      asset2 = new_track('tag2.mp3')
+      asset = file_fixture_asset('tag2.mp3', content_type: 'audio/mpeg')
+      asset2 = file_fixture_asset('tag2.mp3', content_type: 'audio/mpeg')
       expect(asset2.permalink).to eq('put-a-nickel-on-my-door-1')
     end
 
     it 'should make sure to grab bitrate and length in seconds' do
-      asset = new_track('muppets.mp3')
+      asset = file_fixture_asset('muppets.mp3', content_type: 'audio/mpeg')
       expect(asset.bitrate).to eq(192)
       expect(asset.length).to eq('0:13')
-    end
-
-    it 'should open up a zip and dig out valid mp3 files' do
-      # asset = new_track('1valid-1invalid.zip', 'application/zip')
-      # lambda{asset.save}.should change(Asset, :count).by(1)
     end
 
     describe "#publish" do
@@ -165,11 +162,10 @@ RSpec.describe Asset, type: :model do
       end
     end
   end
-  
+
   context 'on update' do
     it 'should regenerate a permalink after the title is changed' do
-      asset = new_track('muppets.mp3')
-      asset.save
+      asset = file_fixture_asset('muppets.mp3', content_type: 'audio/mpeg')
       asset.title = 'New Muppets 123'
       asset.save
       expect(asset.permalink).to eq('new-muppets-123')
