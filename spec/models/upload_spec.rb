a72611c466a4b4b8c478c6ea501dcc016bcdba18
Manfred Stienstra <manfred@fngtps.com>
2019-01-11 15:04:14 +0100

    Attempt to save as many records after upload as possible.

--- spec/models/upload_spec.rb
+++ spec/models/upload_spec.rb
@@ -3,6 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe Upload, type: :model do
+  let(:user) { users(:will_studd) }
   let(:uploaded_filename) { 'smallest.zip' }
   let(:uploaded_file) do
     ActionDispatch::Http::UploadedFile.new(
@@ -13,8 +14,8 @@ RSpec.describe Upload, type: :model do
   end
   let(:upload) do
     Upload.new(
-      files: [uploaded_file],
-      user: users(:will_studd)
+      user: user,
+      files: [uploaded_file]
     )
   end
 
@@ -39,22 +40,69 @@ RSpec.describe Upload, type: :model do
     end
   end
 
-  context 'with blank uploaded ZIP file' do
-    it 'processes' do
-      expect(upload.process).to eq(true)
+  context 'with empty uploaded ZIP file' do
+    it 'processes but does not create any assets not playlists' do
+      expect do
+        expect(upload.process).to eq(true)
+      end.to_not change { Asset.count + Playlist.count }
+
+      expect(upload.playlists).to be_empty
+      expect(upload.assets).to be_empty
     end
   end
 
-  context 'with uploaded ZIP file' do
+  context 'with uploaded ZIP file with separate tracks' do
     let(:uploaded_filename) { 'tracks.zip' }
 
-    it 'processes' do
-      expect(upload.process).to eq(true)
+    it 'processes and creates an asset for each MP3 and no playlist' do
+      expect do
+        expect do
+          expect(upload.process).to eq(true)
+        end.to change { user.assets.count }.by(+3)
+      end.to_not change { Playlist.count }
+
+      expect(upload.playlists.length).to eq(0)
+      expect(upload.assets.length).to eq(3)
+    end
+  end
+
+  context 'with ZIP' do
+    let(:uploaded_filename) { '1valid-1invalid.zip' }
+
+    it 'processes and creates an asset for each MP3 and no playlist' do
+      expect do
+        expect do
+          expect(upload.process).to eq(true)
+        end.to change { user.assets.count }.by(+1)
+      end.to_not change { Playlist.count }
+
+      expect(upload.playlists.length).to eq(0)
+      expect(upload.assets.length).to eq(2)
+      expect(upload.assets.reject(&:valid?).length).to eq(1)
+    end
+  end
+
+  # Disabled because the ZIP contains tracks with UTF-8 sequences not supported by the database
+  # encoding.
+  context 'with uploaded ZIP file with tracks in same album' do
+    let(:uploaded_filename) { 'Le Duc Vacherin.zip' }
+
+    it 'processes and creates an asset for each MP3 and no playlist' do
+      expect do
+        expect do
+          expect(upload.process).to eq(true)
+        end.to change { user.assets.count }.by(+3)
+      end.to change { Playlist.count }.by(+1)
+
+      expect(upload.playlists.length).to eq(1)
+      expect(upload.assets.length).to eq(3)
     end
   end
 
   context 'with multiple uploaded files' do
-    let(:uploaded_filenames) { %w[tracks.zip smallest.mp3] }
+    let(:uploaded_filenames) do
+      ['1valid-1invalid.zip', 'piano.mp3', 'smallest.mp3', 'broken.zip']
+    end
     let(:uploaded_files) do
       uploaded_filenames.map do |uploaded_filename|
         ActionDispatch::Http::UploadedFile.new(
@@ -66,13 +114,21 @@ RSpec.describe Upload, type: :model do
     end
     let(:upload) do
       Upload.new(
-        files: uploaded_files,
-        user: users(:will_studd)
+        user: user,
+        files: uploaded_files
       )
     end
 
-    it 'processes' do
-      expect(upload.process).to eq(true)
+    it 'processes and creates an asset for each MP3 and no playlist' do
+      expect do
+        expect do
+          expect(upload.process).to eq(true)
+        end.to change { user.assets.count }.by(+4)
+      end.to_not change { Playlist.count }
+
+      expect(upload.playlists.length).to eq(0)
+      expect(upload.assets.length).to eq(5)
+      expect(upload.assets.reject(&:valid?).length).to eq(1)
     end
   end
 end
