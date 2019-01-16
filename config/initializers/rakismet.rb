d6aa69c3621ee32b8a62dafd1c37085c0253ecbd
Manfred Stienstra <manfred@fngtps.com>
2019-01-16 19:11:06 +0100

    Change check if Rakismet is configured slightly.
    Also remove the constant which sets whether Rakismet is
    enabled because it's not used.

--- config/initializers/rakismet.rb
+++ config/initializers/rakismet.rb
@@ -1,17 +1,18 @@
 require 'rakismet'
-if Alonetone.rakismet_key
+
+if Alonetone.rakismet_key.present?
   RAKISMET_ENABLED = true
   Alonetone::Application.config.rakismet.key = Alonetone.rakismet_key
   Alonetone::Application.config.rakismet.url = Alonetone.url
+
+  module RakismetLog
+    def akismet_call(function, args={})
+      Rails.logger.warn("RAKISMET #{function}: #{args}")
+      super
+    end
+  end
+
+  Rakismet.singleton_class.prepend RakismetLog
 else
   RAKISMET_ENABLED = false
 end
-
-module RakismetLog
-  def akismet_call(function, args={})
-    Rails.logger.warn("RAKISMET #{function}: #{args}")
-    super
-  end
-end
-
-Rakismet.singleton_class.prepend RakismetLog
