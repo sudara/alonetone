0d889487982013924d502fc3ce57b0d19f4a6a37
Manfred Stienstra <manfred@fngtps.com>
2019-01-16 14:11:58 +0100

    Use CloudFront configuration names in cloudfront_signer.rb.

--- config/initializers/cloudfront_signer.rb
+++ config/initializers/cloudfront_signer.rb
@@ -1,5 +1,5 @@
 Aws::CF::Signer.configure do |config|
   config.key_path = File.join(Rails.root,'config','certs','cloudfront.pem')
-  config.key_pair_id = Alonetone.cloudfront_key_id
+  config.key_pair_id = Alonetone.amazon_cloud_front_key_pair_id
   config.default_expires = 3600
-end if Alonetone.cloudfront_enabled
+end if Alonetone.cloudfront_enabled?
