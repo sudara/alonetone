447f09a8e24955e693e187436a789d308eb3acfd
Manfred Stienstra <manfred@fngtps.com>
2019-01-17 11:40:48 +0100

    Change superclass of Topic to ApplicationRecord.

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,4 +1,4 @@
-class Topic < ActiveRecord::Base
+class Topic < ApplicationRecord
   before_validation :set_default_attributes, on: :create
   before_update  :check_for_moved_forum
   after_update   :set_post_forum_id
