7a0c62692c51625b38371be36fb6a957ad211cf5
Manfred Stienstra <manfred@fngtps.com>
2019-01-17 14:28:29 +0100

    Remove Paperclip code from Pic.

--- app/models/pic.rb
+++ app/models/pic.rb
@@ -1,48 +1,5 @@
 class Pic < ActiveRecord::Base
   belongs_to :picable, polymorphic: true, touch: true
-
-  # Default config for has_attached_file can be found in config/initializers
-  attachment_options = {
-    styles: {
-      tiny: "25x25#",
-      small: "50x50#",
-      large: "125x125#",
-      album: "200x200#",
-      original: "800x800#",
-      greenfield: "1500x1500#",
-      hq: "3000x3000#"
-    }
-  }
-  if Rails.application.remote_storage?
-    # required for production alonetone, to be compatible with rails 2/attachment_fu paths
-    attachment_options[:path] = "/pics/:id/:name_with_style.:extension"
-  end
-  has_attached_file :pic, attachment_options
-
-  validates_attachment_presence :pic, message: 'must be set. Make sure you chose a file to upload!'
-  validates_attachment_size :pic, less_than: 5.megabytes, greater_than: 100.bytes
-  validates_attachment_content_type :pic, content_type: /image/
-
-  # ids after 69601 have :original style stored as filename_original.jpg instead of filename.jpg
-  Paperclip.interpolates 'name_with_style' do |attachment, style|
-    if attachment.instance.id.to_i > 69601
-      "#{basename(attachment, style)}_#{style || 'original'}"
-    elsif style.nil? # original
-      basename(attachment, style)
-    else
-      "#{basename(attachment, style)}_#{style}"
-    end
-  end
-
-  # Returns true when this represents a usable picture.
-  def image_present?
-    pic_file_name.present? && pic_content_type.start_with?('image/')
-  end
-
-  # Generates a URL to the requested variant.
-  def url(variant:)
-    image_present? ? pic.url(variant) : nil
-  end
 end
 
 # == Schema Information
--- spec/models/pic_spec.rb
+++ spec/models/pic_spec.rb
@@ -3,41 +3,4 @@
 require "rails_helper"
 
 RSpec.describe Pic, type: :model do
-  context "valid and pointing to a file" do
-    let(:pic) { pics(:sudara_avatar) }
-
-    it "knows there is an image present" do
-      expect(pic.image_present?).to eq(true)
-    end
-
-    it "returns a URL to a variant" do
-      url = pic.url(variant: :album)
-      expect(url).to start_with('/system/pics')
-      expect(url).to end_with('.jpg')
-    end
-  end
-
-  context "with missing information" do
-    let(:pic) { pics(:orphan) }
-
-    it "knows there is no image present" do
-      expect(pic.image_present?).to eq(false)
-    end
-
-    it "does not return a URL" do 
-      expect(pic.url(variant: :album)).to be_nil
-    end
-  end
-
-  context "new record" do
-    let(:pic) { Pic.new }
-
-    it "knows there is no image present" do
-      expect(pic.image_present?).to eq(false)
-    end
-
-    it "does not return a URL" do
-      expect(pic.url(variant: :album)).to be_nil
-    end
-  end
 end
