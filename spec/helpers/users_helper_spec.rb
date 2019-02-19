094e51fbafeaf888de6cd842d273587fb832af00
Manfred Stienstra <manfred@fngtps.com>
2019-02-19 16:03:40 +0100

    Deal with missing locality data slightly more elegant.

--- spec/helpers/users_helper_spec.rb
+++ spec/helpers/users_helper_spec.rb
@@ -7,6 +7,17 @@ RSpec.describe UsersHelper, type: :helper do
     expect(UsersHelper.no_avatar_path).to eq('default/no-pic_white.svg')
   end
 
+  it "formats location for a user without profile" do
+    expect(user_location(nil)).to be_blank
+  end
+
+  it "formats location for a user with a profile" do
+    expect(user_location(profiles(:jamie_kiesl))).to eq('from Wilwaukee, US')
+    expect(user_location(profiles(:will_studd))).to eq('from AU')
+    expect(user_location(profiles(:henri_willig))).to eq('from Edam')
+    expect(user_location(profiles(:william_shatner))).to eq('')
+  end
+
   context "user with an avatar" do
     let(:user) { users(:sudara) }
 
