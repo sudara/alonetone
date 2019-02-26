1d839d48392aa58a2128752b3bdd1ebcf51ec98d
Jenya <jzueva@gmail.com>
2019-02-26 13:18:54 -0500

    Update regex for permitted characters for login attr

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ActiveRecord::Base
 
   validates :login,
     format: {
-      with: /\A[\w\.+-@]+\z/,
+      with: /[\A\w\+\z]/,
       message: "should use only letters and numbers."
     },
     length: { within: 3..100 },
