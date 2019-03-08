04532de94147cc87dd52179f1a110f208a36172d
Sudara <sudara@alonetone.com>
2019-03-08 18:15:56 +0100

    Add a feature spec helper to login and logged in feature spec

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -4,7 +4,21 @@ RSpec.describe 'home page', type: :feature, js: true do
   it 'renders' do
     visit '/'
     expect(page).to have_selector('#home_latest_area', visible: true)
-
+    expect(page).not_to have_selector('.profile_link')
+    track_chunk = find(".asset", match: :first)
+    track_chunk.click
+    expect(track_chunk).not_to have_selector('.add_to_favorites')
     Percy::Capybara.snapshot(page, name: 'as guest')
+  end
+
+  it 'renders logged in' do
+    logged_in do
+      visit '/'
+      expect(page).to have_selector('.profile_link')
+      track_chunk = find(".asset", match: :first)
+      track_chunk.click
+      expect(track_chunk).to have_selector('.add_to_favorites')
+      Percy::Capybara.snapshot(page, name: 'as user')
+    end
   end
 end
