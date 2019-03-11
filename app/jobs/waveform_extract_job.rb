f8eb7ccb34e31e425ef6f9b17931ba6e89092aa9
Manfred Stienstra <manfred@fngtps.com>
2019-03-11 14:35:59 +0100

    Move WaveformExtractJob out of Greenfield module.

--- app/jobs/waveform_extract_job.rb
+++ app/jobs/waveform_extract_job.rb
@@ -1,9 +1,7 @@
-module Greenfield
-  class WaveformExtractJob < ActiveJob::Base
-    queue_as :default
+class WaveformExtractJob < ActiveJob::Base
+  queue_as :default
 
-    def perform(alonetone_asset_id)
-      Asset.find(alonetone_asset_id).import_waveform
-    end
+  def perform(alonetone_asset_id)
+    Asset.find(alonetone_asset_id).import_waveform
   end
 end
