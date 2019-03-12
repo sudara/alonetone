950e85a60ae44feb83ec8359ab34826e101d13ed
Manfred Stienstra <manfred@fngtps.com>
2019-03-12 14:15:38 +0100

    Generate no waveform for audio files we can't process.

--- app/models/waveform.rb
+++ app/models/waveform.rb
@@ -1,46 +1,50 @@
+# Generates a list of RMS-like amplitudes which can be used to render a
+# waveform for an MP3.
 module Waveform
   def self.extract(file)
-    tmp = Tempfile.new(['resampled-upload', '.wav'])
+    Tempfile.open(['resampled-upload', '.wav']) do |tempfile|
+      reduce_file(tempfile) if resample(file, tempfile)
+    end
+  end
 
-    # resample the mp3 down to 8KHz to make it more manageable
+  # Resample the source MP3 down to 8KHz to make it more manageable.
+  def self.resample(file, tempfile)
     system(
       'lame',
       '--quiet',
       '--mp3input',
       '--resample', '8',
       '--decode', Shellwords.shellescape(file),
-      Shellwords.shellescape(tmp.path)
+      Shellwords.shellescape(tempfile.path),
+      out: File::NULL, err: File::NULL
     )
+  end
 
-    # lame can only downsample to 8KHz, but that's still
-    # way too high so we do a second resampling here
-    # computing a RMS-like quantity over 500 slices
+  def self.reduce_file(tempfile)
+    RubyAudio::Sound.open(tempfile.path) { |sound| reduce(sound) }
+  rescue RubyAudio::Error
+  end
+
+  # Compute RMS-like values to reduce the number of slices to less than 500.
+  def self.reduce(sound)
     waveform = []
-    input = RubyAudio::Sound.open(tmp.path)
-    begin
-      rms = 0.0
-      rms_size = islice = 0
-      slice_size = input.info.frames / 500
-      is_mono = input.info.channels == 1
-      until (signal = input.read(:int, 300)).real_size.zero?
-        signal.each do |frame|
-          mono = is_mono ? frame : frame.sum
-          rms += mono * mono
-        end
-
-        rms_size += signal.real_size
-        next unless rms_size > slice_size
-
-        waveform << Math.sqrt(rms).round
-        rms = 0.0
-        rms_size = 0
+    rms = 0.0
+    rms_size = islice = 0
+    slice_size = sound.info.frames / 500
+    is_mono = sound.info.channels == 1
+    until (signal = sound.read(:int, 300)).real_size.zero?
+      signal.each do |frame|
+        mono = is_mono ? frame : frame.sum
+        rms += mono * mono
       end
-      waveform << Math.sqrt(rms)
-    ensure
-      input.close
-      tmp.close!
-    end
 
-    waveform
+      rms_size += signal.real_size
+      next unless rms_size > slice_size
+
+      waveform << Math.sqrt(rms).round
+      rms = 0.0
+      rms_size = 0
+    end
+    waveform << Math.sqrt(rms)
   end
 end
