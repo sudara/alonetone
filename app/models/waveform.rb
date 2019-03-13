25c34b52705818bf50f36e86ae3464b8b4d81e3b
Manfred Stienstra <manfred@fngtps.com>
2019-03-13 11:24:29 +0100

    Read number of frames to reduce to do less.

--- app/models/waveform.rb
+++ app/models/waveform.rb
@@ -1,14 +1,54 @@
 # Generates a list of RMS-like amplitudes which can be used to render a
 # waveform for an MP3.
 module Waveform
+  class Reduction
+    attr_reader :sound
+    attr_reader :slice_size
+
+    def initialize(sound)
+      @sound = sound
+      self.info = sound.info
+    end
+
+    def mono?
+      @mono
+    end
+
+    def samples
+      samples = []
+      while signal = read
+        rms = 0.0
+        for frame in signal
+          rms += (mono? ? frame : frame.sum) ** 2
+        end
+        samples << Math.sqrt(rms).round
+      end
+      samples
+    end
+
+    private
+
+    attr_reader :signal
+
+    def info=(info)
+      @mono = info.channels == 1
+      @slice_size = (info.frames / 500.0).ceil
+    end
+
+    def read
+      signal = sound.read(:int, slice_size)
+      signal.real_size == 0 ? nil : signal
+    end
+  end
+
   def self.extract(file)
     Tempfile.open(['resampled-upload', '.wav']) do |tempfile|
-      reduce_file(tempfile) if resample(file, tempfile)
+      reduce_file(tempfile) if decode_resample(file, tempfile)
     end
   end
 
   # Resample the source MP3 down to 8KHz to make it more manageable.
-  def self.resample(file, tempfile)
+  def self.decode_resample(file, tempfile)
     system(
       'lame',
       '--quiet',
@@ -25,26 +65,8 @@ module Waveform
   rescue RubyAudio::Error
   end
 
-  # Compute RMS-like values to reduce the number of slices to less than 500.
+  # Compute RMS-like values to reduce the number of aplitudes to 500.
   def self.reduce(sound)
-    waveform = []
-    rms = 0.0
-    rms_size = islice = 0
-    slice_size = sound.info.frames / 500
-    is_mono = sound.info.channels == 1
-    until (signal = sound.read(:int, 300)).real_size.zero?
-      signal.each do |frame|
-        mono = is_mono ? frame : frame.sum
-        rms += mono * mono
-      end
-
-      rms_size += signal.real_size
-      next unless rms_size > slice_size
-
-      waveform << Math.sqrt(rms).round
-      rms = 0.0
-      rms_size = 0
-    end
-    waveform << Math.sqrt(rms)
+    Waveform::Reduction.new(sound).samples
   end
 end
