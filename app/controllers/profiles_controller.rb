bd6554b306176402eacafd38c6761910a6860a22
Jenya <jzueva@gmail.com>
2019-03-15 10:32:03 -0400

    Add Akismet check during profile#update action. Mark user as spam if detected, notify user via flash message. User won't show up in /users

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,6 +3,11 @@ class ProfilesController < ApplicationController
 
   def update
     @user.profile.update(profile_params)
+    if @user.spam?
+      @user.update(is_spam: @user.spam?)
+      flash[:error] = "Hrm, robots marked you as spam. Your info will not appear on Alonetone. If this was done in error, please email support@alonetone.com and magic fairies will fix it right up."
+    end
+
     redirect_back(fallback_location: edit_user_path(@user))
   end
 
