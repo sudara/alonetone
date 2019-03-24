31223ac31b1e1442944fc3d6803cb0fd21099a21
“Jenya <jzueva@gmail.com>
2019-03-24 12:02:37 -0400

    Add an Akismet check during user creation. If user fails Akismet check, perform the following: 1. mark user as spam 2. soft delete user 3. display a flash message for user 4. logout user from current session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,7 +31,15 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params_with_ip)
-    if is_a_bot? && @user.valid?
+
+    if @user.spam?
+      @user.is_spam = @user.spam?
+      # since we don't have any relations at this point yet,
+      # only perform soft-deletion
+      @user.soft_delete
+      flash[:error] = "Hrm, robots marked you as spam. If this was done in error, please email support@alonetone.com and magic fairies will fix it right up."
+      redirect_to logout_path
+    elsif is_a_bot? && @user.valid?
       flash[:ok] = "We just sent you an email to '#{CGI.escapeHTML @user.email}'.<br/><br/>Just click the link in the email, and the hard work is over! <br/> Note: check your junk/spam inbox if you don't see a new email right away.".html_safe
       redirect_to login_url(already_joined: true)
     elsif @user.valid? && @user.save_without_session_maintenance
