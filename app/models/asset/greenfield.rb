10b3d6e23283b5485fe15b0aa619f215c4dc0568
Manfred Stienstra <manfred@fngtps.com>
2019-04-03 13:25:49 +0200

    Use Active Storage to open file for waveform processing.

--- app/models/asset/greenfield.rb
+++ app/models/asset/greenfield.rb
@@ -4,12 +4,9 @@ class Asset
   has_one :greenfield_post, class_name: '::Greenfield::Post'
 
   def import_waveform
-    Tempfile.open(%w[mp3-download .mp3], encoding: 'binary') do |tempfile|
-      audio_file.download { |chunk| tempfile.write(chunk) }
+    audio_file.open do |file|
       update!(
-        audio_feature_attributes: {
-          waveform: Waveform.extract(tempfile.path)
-        }
+        audio_feature_attributes: { waveform: Waveform.extract(file.path) }
       )
     end
   end
