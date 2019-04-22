f3216de64c305d45d7dfd5494b75fbd16cd2389c
Jenya <jzueva@gmail.com>
2019-04-22 14:15:58 -0400

    Add js handling of various buttons on admin/assets page

--- app/javascript/controllers/admin/asset_controller.js
+++ app/javascript/controllers/admin/asset_controller.js
@@ -1,10 +1,33 @@
 import { Controller } from 'stimulus'
 
 export default class extends Controller {
-  initialize() {}
+  static targets = ['spamButton', 'unspamButton', 'deleteUserButton']
 
-  connect() {
-    // console.log("Hello, Stimulus!", this.element)
+  initialize() {
+    this.showDeleteUserButton()
+
+    if (this.data.get('spam') === 'false') {
+      this.showSpamButton()
+    } else {
+      this.showUnspamButton()
+    }
   }
 
+  showSpamButton() {
+    this.data.set('spam', 'false')
+    this.element.classList.remove('spam')
+    this.spamButtonTarget.style.display = 'block'
+    this.unspamButtonTarget.style.display = 'none'
+  }
+
+  showDeleteUserButton() {
+    this.deleteUserButtonTarget.style.display = 'block'
+  }
+
+  showUnspamButton() {
+    this.data.set('spam', 'true')
+    this.element.classList.add('spam')
+    this.unspamButtonTarget.style.display = 'block'
+    this.spamButtonTarget.style.display = 'none'
+  }
 }
