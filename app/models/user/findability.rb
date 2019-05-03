200b4ffe05aa1db6e2d5f2bb5e6f1ec8c5f44f15
Manfred Stienstra <manfred@fngtps.com>
2019-05-03 15:26:03 +0200

    Include User::Findability as module.

--- app/models/user/findability.rb
+++ app/models/user/findability.rb
@@ -1,73 +1,70 @@
-class User
-  def self.currently_online
-     User.where(["last_request_at > ?", Time.now.utc - 15.minutes])
-  end
+# frozen_string_literal: true
 
-  def self.conditions_by_like(value)
-    conditions = ['users.display_name', 'users.login', 'profiles.bio', 'profiles.city', 'profiles.country'].collect do |c|
-      "#{c} LIKE " + ActiveRecord::Base.connection.quote("%#{value}%")
-    end
-    where(conditions.join(" OR "))
-  end
+class User < ApplicationRecord
+  module Findability
+    extend ActiveSupport::Concern
 
-  def self.search(query, options = {})
-    with_scope find: { conditions: build_search_conditions(query) } do
-      find :all, options
-    end
-  end
+    module ClassMethods
+      def currently_online
+         User.where(["last_request_at > ?", Time.now.utc - 15.minutes])
+      end
 
-  def self.build_search_conditions(query)
-    query && ['LOWER(display_name) LIKE :q OR LOWER(login) LIKE :q', { q: "%#{query}%" }]
-  end
+      def conditions_by_like(value)
+        conditions = ['users.display_name', 'users.login', 'profiles.bio', 'profiles.city', 'profiles.country'].collect do |c|
+          "#{c} LIKE " + ActiveRecord::Base.connection.quote("%#{value}%")
+        end
+        where(conditions.join(" OR "))
+      end
 
-  # feeds the users/index subnav
-  def self.paginate_by_params(params)
-    available_sortings = %w[last_uploaded most_listened_to new_artists monster_uploaders dedicated_listeners]
-    params[:sort] = 'last_seen' if !params[:sort].present? || !available_sortings.include?(params[:sort])
-    User.send(params[:sort])
-  end
+      def search(query, options = {})
+        with_scope find: { conditions: build_search_conditions(query) } do
+          find :all, options
+        end
+      end
 
-  protected
+      def build_search_conditions(query)
+        query && ['LOWER(display_name) LIKE :q OR LOWER(login) LIKE :q', { q: "%#{query}%" }]
+      end
 
-  # needed to map incoming params to scopes
-  def self.last_seen
-    recently_seen.limit(30)
-  end
+      # feeds the users/index subnav
+      def paginate_by_params(params)
+        available_sortings = %w[last_uploaded most_listened_to new_artists monster_uploaders dedicated_listeners]
+        params[:sort] = 'last_seen' if !params[:sort].present? || !available_sortings.include?(params[:sort])
+        User.send(params[:sort])
+      end
 
-  def self.recently_joined
-    activated.limit(30)
-  end
+      protected
 
-  def self.new_artists
-    musicians.reorder('users.created_at DESC').limit(30)
-  end
+      # needed to map incoming params to scopes
+      def last_seen
+        recently_seen.limit(30)
+      end
 
-  def self.most_listened_to
-    result = Listen.since(1.month.ago).group(:track_owner).order('count_all DESC').limit(30).count
-    result.collect(&:first)
-  end
+      def recently_joined
+        activated.limit(30)
+      end
 
-  def self.monster_uploaders
-    musicians.reorder('users.assets_count DESC').limit(30)
-  end
+      def new_artists
+        musicians.reorder('users.created_at DESC').limit(30)
+      end
 
-  def self.last_uploaded
-    includes(:assets).order('assets.created_at DESC').limit(30)
-  end
+      def most_listened_to
+        result = Listen.since(1.month.ago).group(:track_owner).order('count_all DESC').limit(30).count
+        result.collect(&:first)
+      end
 
-  def self.dedicated_listeners
-    result = Listen.since(1.month.ago).where('listener_id is not null').group(:listener).order('count_all DESC').limit(30).count
-    result.collect(&:first)
-  end
+      def monster_uploaders
+        musicians.reorder('users.assets_count DESC').limit(30)
+      end
 
-  def geocode_address
-    return unless city && country
+      def last_uploaded
+        includes(:assets).order('assets.created_at DESC').limit(30)
+      end
 
-    geo = GeoKit::Geocoders::MultiGeocoder.geocode([city, country].compact.join(', '))
-
-    if geo.success
-      self.lat = geo.lat
-      self.lng = geo.lng
+      def dedicated_listeners
+        result = Listen.since(1.month.ago).where('listener_id is not null').group(:listener).order('count_all DESC').limit(30).count
+        result.collect(&:first)
+      end
     end
   end
 end
