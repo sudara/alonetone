c9c051e741960aea33debf5c6306f787bd4084fe
Manfred Stienstra <manfred@fngtps.com>
2019-05-06 09:03:37 +0200

    Ignore AbstractController::ActionNotFound.
    Controller does not implement the action that was requested; happens when
    someone hits an action because overmatching routes.
    
    This already renders a not-found page and doesn't have to be reported to
    Bugsnag.

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -3,7 +3,15 @@ if api_key = Rails.configuration.alonetone.bugsnag_api_key
     config.release_stage = "production"
     config.api_key = api_key
     config.auto_capture_sessions = true
+    # Controller does not implement the action that was requested; happens when
+    # someone hits an action because overmatching routes.
+    config.ignore_classes << AbstractController::ActionNotFound
+    # Action does not support for a certain format (e.g. xml); happens when
+    # someone hits an action with a weird accept header or adds the format
+    # param with an unsupported value.
     config.ignore_classes << ActionController::UnknownFormat
+    # When a model doesn't find a record; this already renders a 404 and is not
+    # really interesting.
     config.ignore_classes << ActiveRecord::RecordNotFound
   end
 end
