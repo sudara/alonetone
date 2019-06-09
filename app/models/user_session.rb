ebd7c284340f14f42cfb9f586fdb75ba6292b668
Sudara <sudara@alonetone.com>
2019-06-09 23:07:19 +0200

    Clean up user_session and user ip/timestamp attribute management

--- app/models/user_session.rb
+++ app/models/user_session.rb
@@ -3,15 +3,12 @@ class UserSession < Authlogic::Session::Base
 
   # override authlogic's version of this to take into account @sudo
   def update_info
-    record.profile.user_agent = controller.request.user_agent
-    if record.respond_to?(:current_login_at) && !controller.session[:sudo]
-      record.last_login_at = record.current_login_at if record.respond_to?(:last_login_at)
+    if !controller.session[:sudo]
+      record.last_login_at = record.current_login_at
       record.current_login_at = klass.default_timezone == :utc ? Time.now.utc : Time.now
-    end
-
-    if record.respond_to?(:current_login_ip) && !controller.session[:sudo]
-      record.last_login_ip = record.current_login_ip if record.respond_to?(:last_login_ip)
+      record.last_login_ip = record.current_login_ip
       record.current_login_ip = controller.request.ip
+      record.profile.user_agent = controller.request.user_agent
     end
   end
 end
