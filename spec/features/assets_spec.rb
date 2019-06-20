f81ab97522f1df6a2a5408a9cd0f5c3221b1954d
Sudara <sudara@alonetone.com>
2019-06-20 21:42:27 +0200

    Add mass edit percy test

--- spec/features/assets_spec.rb
+++ spec/features/assets_spec.rb
@@ -1,12 +1,23 @@
 require "rails_helper"
 
-RSpec.describe 'single track playing', type: :feature, js: true do
-  it 'renders' do
+RSpec.describe 'tracks', type: :feature, js: true do
+  it 'renders assets#show' do
     visit '/sudara/tracks/song1'
     play_button = find(".play-button a")
 
     # TODO: find a way to accomplish this without creating jank
     # play_button.click
     Percy.snapshot(page, name: 'Single Track Play')
+  end
+
+  it 'renders assets#edit' do
+    logged_in do
+      visit 'arthur/tracks/mass_edit'
+      first("input[type='text']").set("New Title")
+      akismet_stub_response_ham
+      first('input[name="commit"]').click
+      first('.ajax_success')
+      Percy.snapshot(page, name: 'Single Track Edit')
+    end
   end
 end
