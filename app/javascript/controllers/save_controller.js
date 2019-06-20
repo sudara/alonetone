9f9065d07c0ae91cdcd265cbf6a0ac0fb323c880
Sudara <sudara@alonetone.com>
2019-06-21 00:26:46 +0200

    Be a bit more hygienic with classList in save_controller

--- app/javascript/controllers/save_controller.js
+++ app/javascript/controllers/save_controller.js
@@ -4,18 +4,20 @@ export default class extends Controller {
   static targets = ['response', 'spinner']
 
   spin() {
+    this.responseTarget.classList.remove('ajax_success', 'ajax_fail')
+    this.responseTarget.innerHTML = ''
     this.spinnerTarget.style.display = 'block'
   }
 
   success() {
     this.responseTarget.innerHTML = '<div><span>Saved!</span></div>'
-    this.responseTarget.classList.toggle('ajax_success')
+    this.responseTarget.classList.add('ajax_success')
     this.spinnerTarget.style.display = 'none'
   }
 
   error() {
     this.responseTarget.innerHTML = '<div><span>Sorry, that didn\'t work</span></div>'
-    this.responseTarget.classList.toggle('ajax_fail')
+    this.responseTarget.classList.add('ajax_fail')
     this.spinnerTarget.style.display = 'none'
   }
 
