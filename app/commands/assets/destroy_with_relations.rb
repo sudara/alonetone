733492ae98fb2348b1cb19cd0964d0c37baedc9e
“Jenya <jzueva@gmail.com>
2019-07-14 14:56:24 -0400

    Instead of handling true deletion through `dependent: destroy`, move that work into its own command.

---
+++ app/commands/assets/destroy_with_relations.rb
@@ -0,0 +1,17 @@
+module Assets
+  class DestroyWithRelations
+    attr_reader :asset
+
+    def initialize(asset:)
+      @asset = asset
+    end
+
+    def call
+      asset.comments&.with_deleted&.delete_all
+      asset.tracks&.with_deleted&.delete_all
+      asset.listens&.with_deleted&.delete_all
+      asset.audio_feature.delete
+      asset.destroy
+    end
+  end
+end
