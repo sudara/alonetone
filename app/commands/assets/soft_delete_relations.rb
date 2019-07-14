ec9dbffb15a16c3861eb2f3923905634ffe9339f
Jenya <jzueva@gmail.com>
2019-07-14 14:55:19 -0400

    Since we can't really prove that the listen wasn't a spam (do we care really?), do not attempt to reduce its count

--- app/commands/assets/soft_delete_relations.rb
+++ app/commands/assets/soft_delete_relations.rb
@@ -13,11 +13,6 @@ module Assets
         asset.playlists.update_all(['tracks_count = tracks_count - 1, playlists.updated_at = ?', Time.now])
       end
       asset.tracks&.map(&:soft_delete)
-
-      # remove asset's listens_count from user record
-      # listens_count on user is marked_as readonly
-      # new_count = asset.user.listens_count - asset.listens_count
-      # asset.user.update_attribute(:listens_count, new_count)
       asset.listens&.map(&:soft_delete)
     end
   end
