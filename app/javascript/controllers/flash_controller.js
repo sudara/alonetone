6e50fde58f7aabce2b1666f5448ee79474064546
Sudara <sudara@alonetone.com>
2019-07-31 17:55:45 +0200

    ESlint flash_controller.js

--- app/javascript/controllers/flash_controller.js
+++ app/javascript/controllers/flash_controller.js
@@ -14,20 +14,19 @@ export default class extends Controller {
     this.element.innerHTML = message
     new TimelineMax({})
       .to(this.element, 0, { autoAlpha: 0, y: 50 })
-      .to(this.element, .5, { autoAlpha: 1, y: 0 })
+      .to(this.element, 0.5, { autoAlpha: 1, y: 0 })
       .to(this.element, 1, { autoAlpha: 0, y: 0 }, 3)
   }
 
   alertSaved(message = 'Saved!') {
-    this.element.classList.add("ajax_success")
-    this.element.classList.remove("ajax_fail")
+    this.element.classList.add('ajax_success')
+    this.element.classList.remove('ajax_fail')
     this.alert(`<div><span>${message}</span></div>`)
   }
 
   alertFailed(message = "Sorry that didn't work") {
-    this.element.classList.add("ajax_fail")
-    this.element.classList.remove("ajax_success")
+    this.element.classList.add('ajax_fail')
+    this.element.classList.remove('ajax_success')
     this.alert(`<div><span>${message}</span></div>`)
   }
-
 }
