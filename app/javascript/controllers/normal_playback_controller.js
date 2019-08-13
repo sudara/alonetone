e62dd4220ea07793aa0bbc593050d06ef3ccdd46
Sudara <sudara@alonetone.com>
2019-08-13 16:00:56 +0200

    Once play is pressed, each track gets its own play button svg for animating

--- app/javascript/controllers/normal_playback_controller.js
+++ app/javascript/controllers/normal_playback_controller.js
@@ -2,21 +2,19 @@ import PlaybackController from './playback_controller'
 import PlayAnimation from '../animation/play_animation'
 
 let currentlyOpen
-let animation
 
 export default class extends PlaybackController {
   // these are added to the targets defined in PlaybackController
   static targets = ['playButton', 'details', 'time', 'seekBarPlayed', 'title']
 
   preInitialize() {
-    // animation = new PlayAnimation()
     this.preload = false
     this.url = this.playTarget.querySelector('a').getAttribute('href')
   }
 
   whilePlayingCallback() {
     if (!this.loaded) {
-      animation.showPause()
+      this.animation.showPause()
       this.loaded = true
     }
     this.updateSeekBarPlayed()
@@ -25,21 +23,13 @@ export default class extends PlaybackController {
 
   playCallback() {
     this.showAnimation()
-    if (!this.loaded) this.animateLoading()
-    else animation.setPause()
     this.openDetails()
     this.updateSeekBarLoaded()
     this.registeredListen = true
   }
 
   pauseCallback() {
-    animation.setPlay()
-
-    if (document.contains(document.getElementById("animating"))) {
-      document.getElementById('animating').remove()
-    }
-    
-    this.playButtonTarget.style.display = 'block'
+    this.animation.setPlay()
   }
 
   toggleDetails(e) {
@@ -75,20 +65,29 @@ export default class extends PlaybackController {
   }
 
   showAnimation() {
-    this.playButtonTarget.style.display = 'none'
-    
-    const clone = document.getElementById('playAnimationSVG').cloneNode(true);
-    clone.id = "animating";
-    this.playTarget.firstElementChild.append( clone )
+    this.cloneAnimationIfNeeded()
+    if (!this.loaded) {
+      this.playButtonTarget.style.display = 'none' // hide the dummy play button
+      this.animateLoading()
+    } else this.animation.setPause()
+  }
 
-    animation = new PlayAnimation()
-
+  // Print our own copy of #playAnimationSVG to animate freely as we like
+  // Until this point, the play button has been a placeholder icon SVG
+  // After this point, the play button is an animatable SVG
+  cloneAnimationIfNeeded() {
+    if (this.animation === undefined) {
+      const animationElement = document.getElementById('playAnimationSVG').cloneNode(true);
+      animationElement.id = this.data.get('id')
+      this.playTarget.firstElementChild.append(animationElement)
+      this.animation = new PlayAnimation(animationElement)
+    }
   }
 
   animateLoading() {
-    animation.init()
-    animation.setPlay()
-    animation.showLoading()
+    this.animation.init()
+    this.animation.setPlay()
+    this.animation.showLoading()
   }
 
   // With SoundManager we used to animate this width to display
