f8b51d02c859e9a86dc45dcec2f6ad2f6f76aa94
Jenya <jzueva@gmail.com>
2019-08-14 18:39:30 -0400

    Add restore button and clean up javascript controller.

--- app/javascript/controllers/admin/asset_controller.js
+++ app/javascript/controllers/admin/asset_controller.js
@@ -1,39 +1,80 @@
 import { Controller } from 'stimulus'
 
 export default class extends Controller {
-  static targets = ['spamButton', 'deleteButton']
+  static targets = ['spamButton', 'deleteButton', 'restoreButton']
 
   initialize() {
     if (this.data.get('deleted') == "true" ) {
-      this.hideDeleteButtonAndSpamButton()
-    } else if (this.data.get('spam') == "true") {
+      this.hideDeleteButton()
+      this.showRestoreButton()
       this.hideSpamButton()
+    } else if (this.data.get('spam') == "true") {
+      this.markTrackAsSpam()
+      this.hideRestoreButton()
       this.showDeleteButton()
     } else {
       this.showSpamButton()
+      this.hideRestoreButton()
       this.showDeleteButton()
     }
   }
 
-  showSpamButton() {
-    this.spamButtonTarget.style.display = 'block'
+  // SPAM TRACK
+  markTrackAsSpam() {
+    this.setSpamRecord()
+    this.hideSpamButton()
+  }
+
+  setSpamRecord() {
+    this.data.set('spam', 'true')
+    this.element.classList.add('spam')
   }
 
   hideSpamButton(){
-    this.element.classList.add('spam')
     this.spamButtonTarget.style.display = 'none'
+  }
+
+  // RESTORE TRACK
+  markTrackAsRestored() {
+    this.hideRestoreButton()
+    this.showDeleteButton()
+    this.showSpamButton()
+  }
+
+  hideRestoreButton() {
+    this.restoreButtonTarget.style.display = 'none'
+  }
+
+  showSpamButton() {
+    setUnspamRecord()
+    this.spamButtonTarget.style.display = 'block'
+  }
+
+  setUnspamRecord() {
+    this.data.set('spam', 'false')
+    this.element.classList.remove('spam')
   }
 
   showDeleteButton() {
     this.data.set('deleted', 'false')
+    this.element.classList.remove('deleted')
     this.deleteButtonTarget.style.display = 'block'
   }
 
-  hideDeleteButtonAndSpamButton() {
+  // DELETE TRACK
+  markTrackAsDeleted() {
+    this.hideDeleteButton()
+    this.hideSpamButton()
+    this.showRestoreButton()
+  }
+
+  hideDeleteButton() {
     this.data.set('deleted', 'true')
-    this.element.classList.remove('spam')
     this.element.classList.add('deleted')
     this.deleteButtonTarget.style.display = 'none'
-    this.spamButtonTarget.style.display = 'none'
+  }
+
+  showRestoreButton() {
+    this.restoreButtonTarget.style.display = 'block'
   }
 }
