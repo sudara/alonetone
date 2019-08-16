20ef6d54d407a7cdec25126f8c039f350e57548f
Jenya <jzueva@gmail.com>
2019-08-15 23:41:17 -0400

    Soft Delete any spam asset

--- spec/request/admin/assets_controller_spec.rb
+++ spec/request/admin/assets_controller_spec.rb
@@ -6,19 +6,87 @@ RSpec.describe Admin::AssetsController, type: :request do
   end
 
   describe "spam for individual assets" do
-    let(:track) { assets(:valid_mp3) }
+    let(:asset) { assets(:asset_with_relations_for_soft_delete) }
+
+    before :each do
+      akismet_stub_submit_spam
+    end
 
     it "should mark asset as spam" do
-      akismet_stub_submit_spam
-      expect(track.is_spam).to eq(false)
-      put "/admin/assets/#{track.id}/spam"
-      track.reload
-      expect(track.is_spam).to eq(true)
+      expect(asset.is_spam).to eq(false)
+      put spam_admin_asset_path(asset.id)
+      asset.reload
+      expect(asset.is_spam).to eq(true)
     end
 
     it "should update RAKISMET" do
       expect(Rakismet).to receive(:akismet_call)
-      put spam_admin_asset_path(track.id)
+      put spam_admin_asset_path(asset.id)
+    end
+
+    it "should soft_delete asset" do
+      akismet_stub_submit_spam
+      expect {
+        put spam_admin_asset_path(asset.id)
+      }.to change(Asset, :count).by(-1)
+    end
+
+    it "should soft_delete asset's comments" do
+      comment_count = asset.comments.count
+      expect(comment_count).to be > 0
+      expect {
+        put spam_admin_asset_path(asset.id)
+      }.to change(Comment, :count).by(-comment_count)
+    end
+
+    it "should not touch audio_feature" do
+      expect(asset.audio_feature).not_to be_nil
+      expect {
+        put spam_admin_asset_path(asset.id)
+      }.not_to change(AudioFeature, :count)
+    end
+
+    it "should soft_delete tracks" do
+      tracks_count = asset.tracks.count
+      expect(tracks_count).to be > 0
+      expect {
+        put spam_admin_asset_path(asset.id)
+      }.to change(Track, :count).by(-tracks_count)
+    end
+
+    it "should cleanup any existing playlists" do
+      playlist = asset.tracks.first.playlist
+      playlist_tracks_count = playlist.tracks_count
+
+      # make sure there are more than 0 tracks in that playlist
+      expect(playlist_tracks_count).to be >= 1
+
+      put spam_admin_asset_path(asset.id)
+      # confirm it no longer shows soft_deleted tracks
+      expect(playlist.reload.tracks.count).to eq(playlist_tracks_count - 1)
+      # confirm it recalculated tracks_count
+      expect(playlist.reload.tracks_count).to eq(playlist_tracks_count - 1)
+    end
+
+    it "should soft_delete listens" do
+      listens_count = asset.listens.count
+      # make sure there are more than 0 listens for that asset
+      expect(listens_count).to be > 0
+
+      expect {
+        put spam_admin_asset_path(asset.id)
+      }.to change(Listen, :count).by(-listens_count)
+    end
+
+    it "should NOT touch users' listens_count unless we can prove that the listen was spammy" do
+      user = asset.user
+      user_listens_count = user.listens_count
+
+      expect(user_listens_count).to be > 0
+
+      put spam_admin_asset_path(asset.id)
+      # 2 listens in listens.yml
+      expect(user.reload.listens_count).to eq(user_listens_count)
     end
   end
 
@@ -105,7 +173,6 @@ RSpec.describe Admin::AssetsController, type: :request do
       # confirm it recalculated tracks_count
       expect(playlist.reload.tracks_count).to eq(playlist_tracks_count - 1)
     end
-
 
     it "should soft_delete listens" do
       listens_count = asset.listens.count
