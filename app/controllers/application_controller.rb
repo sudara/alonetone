2f44824d7d56504b7903828b8b73b790df80b90b
Sudara <sudara@alonetone.com>
2019-09-03 17:08:47 +0200

    Revert "Temporarily no-op the /forums => /discuss redirect to crawl forums"
    This reverts commit 0b80da47741b59e1b6fcdfee8af4ecee8c863977.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,7 +30,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def redirect_to_new_forums_if_white_theme
-    #redirect_to('/discuss') if white_theme_enabled?
+    redirect_to('/discuss') if white_theme_enabled?
   end
 
   def lazily_create_waveform_if_needed
