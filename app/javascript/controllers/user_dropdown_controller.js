fc2fa1ba6ba3b0de20732a573e06f75d2444d418
Sudara <sudara@alonetone.com>
2019-09-25 16:52:27 +0200

    Clean up js event logic

--- app/javascript/controllers/user_dropdown_controller.js
+++ app/javascript/controllers/user_dropdown_controller.js
@@ -1,26 +1,39 @@
 import { Controller } from 'stimulus'
+import { TweenMax } from 'gsap/all'
 
 export default class extends Controller {
   static targets = ['menu']
 
   initialize() {
-    this.menuTarget.style.top = '-310px'
+    this.currentlyOpen = false
   }
 
-  open(event) {
-    event.preventDefault()
-    this.menuTarget.style.top = '0px'
+  open(e) {
+    console.log('open is called')
+    e.preventDefault()
+    this.menuTarget.style.display = 'block'
+    this.currentlyOpen = true
+
+    // https://greensock.com/ease-visualizer/
+    TweenMax.from('.user_dropdown_menu_header img', 1.2,
+      { width: '32px', height: '32px', ease: Elastic.easeOut })
+
+    // a bit more manual than using click@window->user-dropdown#close
+    // but this ensures the click handler only gets added when
+    // the menu has been opened, which makes my performance brain happy
+    e.stopImmediatePropagation()
+    window.addEventListener('click', this.close.bind(this), { once: true })
   }
 
-  close() {
-    this.menuTarget.style.top = '-310px'
-  }
-
-  touch(event) {
-    if (this.menuTarget.style.top !== '0px') {
-      event.preventDefault()
-      this.open(event)
+  close(e) {
+    console.log('close is called')
+    if (this.open && (e.keyCode === 27 || this.clickOutsideMenu(e))) {
+      this.menuTarget.style.display = 'none'
+      this.currentlyOpen = false
     }
   }
 
+  clickOutsideMenu(e) {
+    return (e.type === 'click') && !this.menuTarget.contains(e.target)
+  }
 }
