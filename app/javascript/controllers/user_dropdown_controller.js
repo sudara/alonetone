899ff027ae1ddee5406704e81aaf0b544ccfe403
Sudara <sudara@alonetone.com>
2019-09-25 17:34:22 +0200

    Improve dropdown event handling with implicit "handleEvent"

--- app/javascript/controllers/user_dropdown_controller.js
+++ app/javascript/controllers/user_dropdown_controller.js
@@ -2,14 +2,13 @@ import { Controller } from 'stimulus'
 import { TweenMax } from 'gsap/all'
 
 export default class extends Controller {
-  static targets = ['menu']
+  static targets = ['menu', 'header']
 
   initialize() {
     this.currentlyOpen = false
   }
 
   open(e) {
-    console.log('open is called')
     e.preventDefault()
     this.menuTarget.style.display = 'block'
     this.currentlyOpen = true
@@ -22,18 +21,24 @@ export default class extends Controller {
     // but this ensures the click handler only gets added when
     // the menu has been opened, which makes my performance brain happy
     e.stopImmediatePropagation()
-    window.addEventListener('click', this.close.bind(this), { once: true })
+    window.addEventListener('click', this)
   }
 
-  close(e) {
-    console.log('close is called')
-    if (this.open && (e.keyCode === 27 || this.clickOutsideMenu(e))) {
+  // This is a "special" method with secret implicit powers.
+  // We are passing an object (this) into add/remove event listener
+  // This is the  method that will be implicitly called
+  // https://medium.com/@photokandy/til-you-can-pass-an-object-instead-of-a-function-to-addeventlistener-7838a3c4ec62
+  handleEvent(e) {
+    if (this.open && ((e.keyCode === 27) || this.clickOutsideMenu(e))) {
       this.menuTarget.style.display = 'none'
       this.currentlyOpen = false
+      window.removeEventListener('click', this)
     }
   }
 
   clickOutsideMenu(e) {
-    return (e.type === 'click') && !this.menuTarget.contains(e.target)
+    return (e.type === 'click') &&
+      (!this.menuTarget.contains(e.target) || // outside whole menu
+      this.headerTarget.contains(e.target))   // inside header
   }
 }
