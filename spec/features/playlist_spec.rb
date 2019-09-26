f80c52b6bc42417c9596e749c3a9cf555349fdef
Sudara <sudara@alonetone.com>
2019-09-26 23:50:21 +0200

    Add attempt at Sortable Selenium test w/ custom hack

--- spec/features/playlist_spec.rb
+++ spec/features/playlist_spec.rb
@@ -1,6 +1,7 @@
 require "rails_helper"
 
 RSpec.describe 'playlists', type: :feature, js: true do
+
   it 'renders track and cover pages' do
     logged_in do
       visit 'henriwillig/playlists/polderkaas'
@@ -55,12 +56,14 @@ RSpec.describe 'playlists', type: :feature, js: true do
       first_upload.click
       expect(page).to have_selector('.sortable .asset', count: 3)
 
+      # Testing html5 drag-n-drop with selenium is non-trivial
+      # https://github.com/SortableJS/Sortable/issues/563
+      #
       # Move "Manfacturer of the Finest Cheese" to be the last song
-      first_track = find('.sortable .asset:first-child')
-      last_track = find('.sortable .asset:last-child')
-      first_track.drag_to(last_track)
-      #expect(find('.sortable .asset:last-child .track_link').text).to eql('Manufacturer of the Finest Cheese')
+      first_track_handle = find('.sortable .asset:first-child .drag_handle')
+      first_track_handle.drag_by(0, -100)
 
+      expect(find('.sortable .asset:last-child .track_link').text).to eql('Manufacturer of the Finest Cheese')
       Percy.snapshot(page, name: 'Playlist Edit')
     end
   end
