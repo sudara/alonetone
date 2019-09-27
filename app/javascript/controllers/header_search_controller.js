f77a938d814a32077c9027606c69910935811214
bells <ben@modernthings.net>
2019-09-27 17:50:35 -0600

    Try to add another mobile only button to the header search, flail about in stimulus

--- app/javascript/controllers/header_search_controller.js
+++ app/javascript/controllers/header_search_controller.js
@@ -1,10 +1,20 @@
 import { Controller } from 'stimulus'
 
 export default class extends Controller {
-  static targets = ['form']
+  static targets = ['form, query, button, reveal']
   
   submitForm() {
     this.formTarget.submit()
+  }
+
+  revealQuery() {
+    this.queryTarget.style.display = 'block'
+    this.buttonTarget.style.display = 'block'
+    this.revealTarget.style.display = 'none'
+  }
+
+  hideQuery() {
+
   }
 
 }
