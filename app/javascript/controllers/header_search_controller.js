49d12e0296a59c91748036262ec52a24cdde78a0
bells <ben@modernthings.net>
2019-09-27 18:04:09 -0600

    Fix target list, update css to allow first mobile click on search to reveal search field

--- app/javascript/controllers/header_search_controller.js
+++ app/javascript/controllers/header_search_controller.js
@@ -1,20 +1,18 @@
 import { Controller } from 'stimulus'
 
 export default class extends Controller {
-  static targets = ['form, query, button, reveal']
+  static targets = ['form', 'query', 'button', 'reveal']
   
   submitForm() {
     this.formTarget.submit()
   }
 
-  revealQuery() {
+  revealQuery(event) {
+
+    event.preventDefault()
     this.queryTarget.style.display = 'block'
     this.buttonTarget.style.display = 'block'
     this.revealTarget.style.display = 'none'
-  }
-
-  hideQuery() {
-
   }
 
 }
