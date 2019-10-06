ce4c6fecb5d94b7071dfc66128dc66dba37d8560
Sudara <sudara@alonetone.com>
2019-10-06 18:07:50 +0200

    Force capybara to manually wait

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -9,9 +9,11 @@ RSpec.describe 'home page', type: :feature, js: true do
     track_chunk = find(".asset", match: :first)
     track_chunk.click
 
-    expect(page).to have_selector('.private_check_box')
+    expect(page).to have_selector('.private_check_box label')
     expect(track_chunk).not_to have_selector('.add_to_favorites')
 
+    # capybara should be waiting here but isn't
+    sleep 0.2
     track_chunk.find('.private_check_box label').click
     expect(page).to have_selector('span.only_user_name')
 
