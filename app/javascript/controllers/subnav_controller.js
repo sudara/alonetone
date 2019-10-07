ed75b179ed9ddf456d6239e35d458fcab0e1e53b
bells <ben@modernthings.net>
2019-10-07 16:42:01 -0600

    Remove logs, set margin to 12, remove debounce method!

--- app/javascript/controllers/subnav_controller.js
+++ app/javascript/controllers/subnav_controller.js
@@ -5,29 +5,18 @@ export default class extends Controller {
 
   connect() {
     this.timer = null
-    this.marginWidth = 6
+    this.marginWidth = 12
     this.headerPadding = 72
     this.twoColumnCutoff = 520
     this.subNavItemsTotalWidth = this.itemTargets.reduce((total, item) => {
-      console.log(item.offsetWidth)
       return total + item.offsetWidth + this.marginWidth
     }, -this.marginWidth) // we want n-1 margins
-    console.log(`subNavItemsTotalWidth is ${this.subNavItemsTotalWidth}`)
     this.actuallyResize()
   }
 
-  // debounce the window.onresize events
   resize() {
-    clearTimeout(this.timer)
-    this.timer = setTimeout(() => {
-      this.actuallyResize()
-    }, 50)
-  }
-
-  actuallyResize() {
     const availableWidth = window.innerWidth - this.headerPadding
-    console.log(`resizing, padding: ${this.headerPadding} available width: ${availableWidth} subNavItemsTotalWidth: ${this.subNavItemsTotalWidth}`)
-
+    
     if (availableWidth < this.twoColumnCutoff) { // 2 col
       this.element.classList.add('two-column')
       this.element.classList.remove('three-column')
