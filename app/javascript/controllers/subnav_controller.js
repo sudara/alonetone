685b5c29626931cbb9d5c777c9dbd8529164a840
bells <ben@modernthings.net>
2019-10-07 14:12:32 -0600

    Work on subnav resizing logic

--- app/javascript/controllers/subnav_controller.js
+++ app/javascript/controllers/subnav_controller.js
@@ -1,9 +1,21 @@
 import { Controller } from 'stimulus'
 
+var navItemsTotalWidth = 0;
+
 export default class extends Controller {
 
   initialize() {
+    
     this.timer = null
+
+    const allItems = this.element.getElementsByTagName("li")
+    Array.prototype.forEach.call(allItems, a => {
+      
+      const thisMargin = window.getComputedStyle(a).getPropertyValue('margin-right')
+
+      navItemsTotalWidth += a.offsetWidth + parseInt( thisMargin );
+    });
+
     this.actuallyResize()
   }
 
@@ -16,7 +28,23 @@ export default class extends Controller {
   }
 
   actuallyResize() {
-    console.log(this.element)
-    console.log('resizing')
+
+    const maxWidthElement = document.getElementsByClassName("header_inner")[0]
+
+    const maxWidth = parseInt(  window.getComputedStyle(maxWidthElement).getPropertyValue('width'))  - parseInt(window.getComputedStyle(maxWidthElement).getPropertyValue('padding-right') ) - parseInt (window.getComputedStyle(maxWidthElement).getPropertyValue('padding-left') )
+
+    console.log ( maxWidth )
+    
+    if ( navItemsTotalWidth > maxWidth ) {
+      this.element.classList.add("three-column")
+    } else {
+      this.element.classList.remove("three-column")
+    }
+    if ( 520 > maxWidth ) {
+      this.element.classList.add("two-column")
+      this.element.classList.remove("three-column")
+    } else {
+      this.element.classList.remove("two-column")
+    }
   }
 }
