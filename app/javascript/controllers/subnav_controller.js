ae36afe79571b229d946b578cc731142b221e276
Sudara <sudara@alonetone.com>
2019-10-07 20:09:01 +0200

    Add subnav_controller that fires on window resize

---
+++ app/javascript/controllers/subnav_controller.js
@@ -0,0 +1,22 @@
+import { Controller } from 'stimulus'
+
+export default class extends Controller {
+
+  initialize() {
+    this.timer = null
+    this.actuallyResize()
+  }
+
+  // debounce the window.onresize events
+  resize() {
+    clearTimeout(this.timer)
+    this.timer = setTimeout(() => {
+      this.actuallyResize()
+    }, 50)
+  }
+
+  actuallyResize() {
+    console.log(this.element)
+    console.log('resizing')
+  }
+}
