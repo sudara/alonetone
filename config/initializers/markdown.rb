5365c11814c427822680c4c733af2c4158a2ab1a
Sudara <sudara@alonetone.com>
2019-10-22 18:54:10 +0200

    Use CommonMarker for about pages markdown rendering

--- config/initializers/markdown.rb
+++ config/initializers/markdown.rb
@@ -1,13 +1,13 @@
-  # https://www.lugolabs.com/articles/render-markdown-views-with-redcarpet-and-pygment-in-rails
+# https://www.lugolabs.com/articles/render-markdown-views-with-redcarpet-and-pygment-in-rails
 class MarkdownHandler
   def call(template, source)
-    "#{render(template.source).inspect}.html_safe;"
+    "#{render(source).inspect}.html_safe;"
   end
 
   def render(text)
     key = cache_key(text)
     Rails.cache.fetch key do
-      markdown.render(text).html_safe
+      markdown(text).html_safe
     end
   end
 
@@ -16,9 +16,8 @@ class MarkdownHandler
     Digest::MD5.hexdigest(text)
   end
 
-  def markdown
-    #     smarty = Redcarpet::Render::SmartyPants.render(markdown.render(template.source)).html_safe
-    @markdown ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML, fenced_code_blocks: true, autolink: true, space_after_headers: true)
+  def markdown(text)
+    CommonMarker.render_doc(text, :SMART).to_html(:HARDBREAKS, [:autolink, :strikethrough])
   end
 end
 
