f06408489935327fd70aea6abe51c1b5b5e47fd8
“Jenya <jzueva@gmail.com>
2019-10-28 22:19:09 -0400

    Add migration to set published date where due

---
+++ db/migrate/20191029020417_populate_published_at_to_playlists.rb
@@ -0,0 +1,11 @@
+class PopulatePublishedAtToPlaylists < ActiveRecord::Migration[6.0]
+  def change
+    ar = []
+    Playlist.find_each do |playlist|
+      next if playlist.published_at
+      next unless playlist.private == false
+      next unless playlist.can_be_public?
+      playlist.published_at = playlist.updated_at
+    end
+  end
+end
