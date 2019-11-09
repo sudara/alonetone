197ba82dccd5fe43655d08e12aaed09c24e37003
“Jenya <jzueva@gmail.com>
2019-11-08 22:09:10 -0500

    Update favorites count on all assets

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_17_201831) do
+ActiveRecord::Schema.define(version: 2019_11_09_022931) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "name", null: false
