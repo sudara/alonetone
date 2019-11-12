1182ce7c71cd25ceac3818bec18b3b68210fa087
Manfred Stienstra <manfred@fngtps.com>
2019-11-12 10:08:45 +0100

    Document authentication concern.

--- app/controllers/concerns/authentication.rb
+++ app/controllers/concerns/authentication.rb
@@ -1,13 +1,16 @@
 # frozen_string_literal: true
 
+# Implements controller helper methods to deal with authentication.
 module Authentication
   def current_user_session
     return @current_user_session if defined?(@current_user_session)
+
     @current_user_session = UserSession.find
   end
 
   def current_user
     return @current_user if defined?(@current_user)
+
     @current_user = current_user_session&.user
   end
 
