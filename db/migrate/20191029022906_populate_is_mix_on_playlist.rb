a855a333caac58386d8f91134edc5f8b3a7970c5
“Jenya <jzueva@gmail.com>
2019-11-12 19:31:52 -0500

    Check if `playlist` qualifies as a mix and update `is_mix` value.

--- db/migrate/20191029022906_populate_is_mix_on_playlist.rb
+++ db/migrate/20191029022906_populate_is_mix_on_playlist.rb
@@ -2,7 +2,7 @@ class PopulateIsMixOnPlaylist < ActiveRecord::Migration[6.0]
   def change
     counter = 0
     Playlist.all.map do |playlist|
-      playlist.set_mix_or_album
+      playlist.is_mix = true if playlist.consider_a_mix?
 
       counter += 1 if playlist.is_mix_changed?
     end
