fc96dcfaea2136e72412aa805c812648abbeef5d
“Jenya <jzueva@gmail.com>
2019-11-12 20:54:45 -0500

    Remove a check to ensure tracks_count > 1 since we check for when making playlist public AND publishing it.

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -8,7 +8,7 @@ class Playlist < ActiveRecord::Base
   scope :for_home,         -> { select('distinct playlists.user_id, playlists.*').recently_published.only_public.with_preloads }
   scope :include_private,  -> { where(is_favorite: false) }
   scope :mixes,            -> { where(is_mix: true) }
-  scope :only_public,      -> { where(private: false).where(is_favorite: false).where("tracks_count > 1") }
+  scope :only_public,      -> { where(private: false).where(is_favorite: false) }
   scope :with_preloads,    -> { preload(:cover_image_blob, user: { avatar_image_attachment: :blob }) }
   scope :recently_published, -> { reorder('playlists.published_at DESC') }
 
