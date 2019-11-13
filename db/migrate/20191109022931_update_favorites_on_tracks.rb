c5d38a098d21625b1e5f09dd4b706c76e20d5513
“Jenya <jzueva@gmail.com>
2019-11-12 21:53:53 -0500

    Use [update_counters[(https://api.rubyonrails.org/classes/ActiveRecord/CounterCache/ClassMethods.html#method-i-update_counters) method to update the favorites_value.

--- db/migrate/20191109022931_update_favorites_on_tracks.rb
+++ db/migrate/20191109022931_update_favorites_on_tracks.rb
@@ -1,11 +1,11 @@
 class UpdateFavoritesOnTracks < ActiveRecord::Migration[6.0]
   def change
-    asset_ids = Track.favorites.map(&:asset_id).uniq
+    asset_ids = Track.favorites.pluck(:asset_id).uniq
     puts "Found #{asset_ids.length} favorite assets."
 
     Asset.where(id: asset_ids).find_each do |asset|
       if asset.favorites_count != asset.tracks.favorites.count
-        asset.favorites_count = asset.tracks.favorites.count
+         Asset.update_counters asset.id, favorites_count: asset.tracks.favorites.count
       end
     end
   end
