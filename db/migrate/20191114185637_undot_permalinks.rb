6ff8a67478fbdc692ba1441bbfad1596efd40bce
Manfred Stienstra <manfred@fngtps.com>
2019-11-14 20:15:29 +0100

    Use tr in migration and put empty method on single line.

--- db/migrate/20191114185637_undot_permalinks.rb
+++ db/migrate/20191114185637_undot_permalinks.rb
@@ -4,11 +4,10 @@ class UndotPermalinks < ActiveRecord::Migration[6.0]
   def up
     Asset.select(:id, :permalink).where("permalink LIKE '%.%'").each do |asset|
       # Replace a dot with a dash and multiple dashes with one dash.
-      permalink = asset.permalink.gsub('.', '-').gsub(%r{-+}, '-')
+      permalink = asset.permalink.tr('.', '-').gsub(%r{-+}, '-')
       asset.update_columns(permalink: permalink, updated_at: Time.zone.now)
     end
   end
 
-  def down
-  end
+  def down; end
 end
