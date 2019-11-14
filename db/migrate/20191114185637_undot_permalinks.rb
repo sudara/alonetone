2848f11ba9663b543ef35d1e3fb1492e589099c7
Manfred Stienstra <manfred@fngtps.com>
2019-11-14 20:04:27 +0100

    Add migration to undot all asset permalinks.

---
+++ db/migrate/20191114185637_undot_permalinks.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class UndotPermalinks < ActiveRecord::Migration[6.0]
+  def up
+    Asset.select(:id, :permalink).where("permalink LIKE '%.%'").each do |asset|
+      # Replace a dot with a dash and multiple dashes with one dash.
+      permalink = asset.permalink.gsub('.', '-').gsub(%r{-+}, '-')
+      asset.update_columns(permalink: permalink, updated_at: Time.zone.now)
+    end
+  end
+
+  def down
+  end
+end
