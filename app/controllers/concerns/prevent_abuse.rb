a75ca670061e6e0233349b0d4c33e0292725bef7
Manfred Stienstra <manfred@fngtps.com>
2019-11-15 08:45:37 +0100

    Some PreventAbuse formatting and syntax changes.

--- app/controllers/concerns/prevent_abuse.rb
+++ app/controllers/concerns/prevent_abuse.rb
@@ -1,14 +1,55 @@
+# frozen_string_literal: true
+
 module PreventAbuse
-  @@bad_ip_ranges = ['195.239', '220.181', '61.135', '60.28.232', '121.14', '221.194', '117.41.183',
-                     '117.41.184', '60.169.78', '222.186', '61.160.232', '60.169.75', '60.169.78',
-                     '120.35.102', '110.84.8', '37.115.145', '195.154.181.60', '31.184.238.9',
-                     '62.210.202.176', '195.154.187.229','5.188.210.13','103.43.33','103.54.103']
+  @@bad_ip_ranges = %w[
+    195.239
+    220.181
+    61.135
+    60.28.232
+    121.14
+    221.194
+    117.41.183
+    117.41.184
+    60.169.78
+    222.186
+    61.160.232
+    60.169.75
+    60.169.78
+    120.35.102
+    110.84.8
+    37.115.145
+    195.154.181.60
+    31.184.238.9
+    62.210.202.176
+    195.154.187.229
+    5.188.210.13
+    103.43.33
+    103.54.103
+  ].freeze
 
-  # user agent whitelist
-  # cfnetwork = Safari on osx 10.4 *only* when it tries to download
-  @@valid_listeners = %w[msie webkit quicktime gecko firefox netscape itunes chrome opera safari cfnetwork facebookexternalhit ipad iphone apple facebook stagefright]
+  # Whitelist of all allowed user-agents. Safari uses `cfnetwork' as user-agent when downloading
+  # a file on macOS 10.4+.
+  @@valid_listeners = %w[
+    msie
+    webkit
+    quicktime
+    gecko
+    firefox
+    netscape
+    itunes
+    chrome
+    opera
+    safari
+    cfnetwork
+    facebookexternalhit
+    ipad
+    iphone
+    apple
+    facebook
+    stagefright
+  ].freeze
 
-  # user agent black list
+  # Blacklist of all disallowed user-agents.
   @@bots = %w[bot spider baidu mp3bot]
 
   def is_from_a_bad_ip?
