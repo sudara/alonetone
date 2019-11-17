7abf01989b943f9f69d2eafd6eaea6b7b660afb4
Sudara <sudara@alonetone.com>
2019-11-17 15:45:15 +0100

    Link to css files without path in manifest

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,7 +1,6 @@
 //= link_tree ../images
-//= link ../stylesheets/application.css
-//= link ../stylesheets/dark_theme.css
-//= link ../stylesheets/white_theme_thredded.css
-//= link white_theme_thredded.css
-//= link ../stylesheets/rpm_challenge.css
-//= link ../stylesheets/24houralbum.css
+//= link 'application.css'
+//= link 'dark_theme.css'
+//= link 'white_theme_thredded.css'
+//= link 'rpm_challenge.css'
+//= link '24houralbum.css'
