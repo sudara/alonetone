120e328662de6b570b9ac8e5914e5fdee0e11fcc
Sudara <sudara@alonetone.com>
2019-11-17 15:34:37 +0100

    List exact css files used in manifest

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,7 @@
 //= link_tree ../images
-//= link_directory ../stylesheets .css
+//= link ../stylesheets/application.css
+//= link ../stylesheets/dark_theme.css
+//= link ../stylesheets/white_theme_thredded.css
+//= link white_theme_thredded.css
+//= link ../stylesheets/rpm_challenge.css
+//= link ../stylesheets/24houralbum.css
