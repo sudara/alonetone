094c9e9ae5d673ad8c3a52fbcdf80cca09cd1b45
Sudara <sudara@alonetone.com>
2019-12-05 23:58:55 +0100

    Add successful feature test for submitting account request form

--- spec/features/account_requests_spec.rb
+++ spec/features/account_requests_spec.rb
@@ -7,6 +7,17 @@ RSpec.describe 'Get An Account', type: :feature, js: true do
     submit.click
     expect(page).to have_selector('#error_explanation', visible: true)
 
-    Percy.snapshot(page, name: 'Single Track Page')
-
+    Percy.snapshot(page, name: 'Sign up')
   end
+
+  it 'submits the form and succeeds' do
+    visit '/get_an_account'
+    page.fill_in 'Email', with: "someband@hotmail.com"
+    page.fill_in 'account_request_login', with: 'someband456'
+    page.fill_in 'account_request_details', with: "This is a story of a very cool band"
+    page.choose 'Musician'
+    page.click_on 'Get On The List'
+
+    expect(page).to have_content('Thank you')
+  end
+end
