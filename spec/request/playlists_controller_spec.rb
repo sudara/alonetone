079cd828481037f7a1d0f4fdf060269db0e99a7b
Manfred Stienstra <manfred@fngtps.com>
2019-12-06 16:17:09 +0100

    Add request spec for playlist with unusable cover.

--- spec/request/playlists_controller_spec.rb
+++ spec/request/playlists_controller_spec.rb
@@ -7,13 +7,19 @@ RSpec.describe PlaylistsController, type: :request do
   context "a visitor" do
     it "sees a playlist without a cover image" do
       playlist = playlists(:william_shatners_favorites)
-      get "/williamshatner/playlists/bills-favorites"
+      get "/#{playlist.user.login}/playlists/#{playlist.permalink}"
+      expect(response).to be_successful
+    end
+
+    it "sees a playlist with an unusable cover image" do
+      playlist = playlists(:jamie_kiesl_hates)
+      get "/#{playlist.user.login}/playlists/#{playlist.permalink}"
       expect(response).to be_successful
     end
 
     it "sees a playlist with a cover image" do
-      get "/willstudd/playlists/rockfort"
       playlist = playlists(:will_studd_rockfort)
+      get "/#{playlist.user.login}/playlists/#{playlist.permalink}"
       expect(response).to be_successful
       expect(response.body).to match_css('link[href]')
     end
