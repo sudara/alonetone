55e455830518c59f91c484a497a8c906e78ca8e5
Sudara <sudara@alonetone.com>
2019-12-06 20:38:06 +0100

    Don't write model expectation in request spec

--- spec/request/admin/account_requests_controller_spec.rb
+++ spec/request/admin/account_requests_controller_spec.rb
@@ -8,7 +8,6 @@ RSpec.describe Admin::AssetsController, type: :request do
   it "should allow moderators to approve a request" do
     put approve_admin_account_request_path(account_requests(:waiting))
     expect(response).to be_successful
-    expect(account_requests(:waiting)).to be_approved
   end
 
   it "should send an email when a request is approved" do
