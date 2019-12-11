61d474eac5f327bd9479c990fb24fa56ebfd1d1e
Jenya <jzueva@gmail.com>
2019-12-10 21:40:28 -0500

    Correctly match asset id to favorites list.

--- app/javascript/controllers/favorite_controller.js
+++ app/javascript/controllers/favorite_controller.js
@@ -2,7 +2,7 @@ import HeartController from './heart_controller'
 
 export default class extends HeartController {
   isFavorited() {
-    this.id = parseInt(this.element.href.split('=')[1])
+    this.id = parseInt(this.data.get('asset-id'))
     return window.userFavorites.includes(this.id)
   }
 }
