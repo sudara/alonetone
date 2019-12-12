3ab4489cabc0b104cda89a2cbee0c1df4b29b123
Jenya <jzueva@gmail.com>
2019-12-11 20:01:23 -0500

    Update migration to reset counters.

--- db/migrate/20191109022931_update_favorites_on_tracks.rb
+++ db/migrate/20191109022931_update_favorites_on_tracks.rb
@@ -2,10 +2,9 @@ class UpdateFavoritesOnTracks < ActiveRecord::Migration[6.0]
   def change
     asset_ids = Track.favorites.pluck(:asset_id).uniq
     puts "Found #{asset_ids.length} favorite assets."
-
     Asset.where(id: asset_ids).find_each do |asset|
       if asset.favorites_count != asset.tracks.favorites.count
-         Asset.update_counters asset.id, favorites_count: asset.tracks.favorites.count
+        Asset.reset_counters asset.id, :favorites
       end
     end
   end
