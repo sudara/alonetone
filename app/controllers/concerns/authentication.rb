b75ff5130d8c37e3808f87f0fed3ca73f3e6dc68
Manfred Stienstra <manfred@fngtps.com>
2019-12-13 16:53:11 +0100

    Add authentication_session method to Authentication.
    Returns an AuthenticatedSession based on the controller
    session so the session can be checked for a valid
    authenticated session.

--- app/controllers/concerns/authentication.rb
+++ app/controllers/concerns/authentication.rb
@@ -2,16 +2,24 @@
 
 # Implements controller helper methods to deal with authentication.
 module Authentication
+  # Returns the current user session from Authlogic
   def current_user_session
     return @current_user_session if defined?(@current_user_session)
 
     @current_user_session = UserSession.find
   end
 
+  # Returns the current authenticated session from Alonetone's own implementation.
+  def authenticated_session
+    return @authenticated_session if defined?(@authenticated_session)
+
+    @authenticated_session = AuthenticatedSession.new(session: session)
+  end
+
   def current_user
     return @current_user if defined?(@current_user)
 
-    @current_user = current_user_session&.user
+    @current_user = current_user_session&.user || authenticated_session&.user
   end
 
   def logged_in?
--- spec/controllers/concerns/authentication_spec.rb
+++ spec/controllers/concerns/authentication_spec.rb
@@ -19,6 +19,10 @@ RSpec.describe Authentication, type: :controller do
     expect(controller).to_not be_logged_in
   end
 
+  it 'returns an authenticated session' do
+    expect(controller.authenticated_session).to_not be_nil
+  end
+
   context 'authenticated' do
     let(:user) { users(:henri_willig) }
 
@@ -28,6 +32,29 @@ RSpec.describe Authentication, type: :controller do
 
     it 'finds the current user session' do
       expect(controller.current_user_session).to_not be_nil
+    end
+
+    it 'finds the current user' do
+      expect(controller.current_user).to_not be_nil
+    end
+
+    it 'is logged in' do
+      expect(controller).to be_logged_in
+    end
+  end
+
+  context 'with authenticated session' do
+    let(:user) { users(:henri_willig) }
+    let(:authenticated_session) { AuthenticatedSession.new(session: session) }
+
+    before do
+      AuthenticatedSession.new(
+        session: session, login: user.login, password: 'test'
+      ).save
+    end
+
+    it 'returns an authenticated session' do
+      expect(controller.authenticated_session).to_not be_nil
     end
 
     it 'finds the current user' do
