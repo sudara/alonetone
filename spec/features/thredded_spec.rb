5f2f737862e0eed412126f0712b1735e8420fe3c
Sudara <sudara@alonetone.com>
2019-12-17 17:27:45 +0100

    Log in as sudara for Percy forum snap

--- spec/features/thredded_spec.rb
+++ spec/features/thredded_spec.rb
@@ -2,11 +2,13 @@ require "rails_helper"
 
 RSpec.describe 'thredded', type: :feature, js: true do
   it 'renders' do
-    visit '/forums'
-    switch_themes
+    logged_in(:sudara) do
+      visit '/forums'
+      switch_themes
 
-    # Wait for js to append a column class
-    expect(page).to have_text "Gear Talk"
-    Percy.snapshot(page, name: 'Forums Index', enable_javascript: true)
+      # Wait for js to append a column class
+      expect(page).to have_text "Gear Talk"
+      Percy.snapshot(page, name: 'Forums Index', enable_javascript: true)
+    end
   end
 end
