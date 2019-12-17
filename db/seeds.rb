60e91d03518d65fb2af0fcdd21b76c9a2b3f340a
Sudara <sudara@alonetone.com>
2019-12-17 21:31:35 +0100

    Add forum seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -279,3 +279,25 @@ end
   asset = Asset.where.not(user_id: user.id).random_order.first
   user.toggle_favorite(asset)
 end
+
+group1 = Thredded::MessageboardGroup.create(name: "Alonetone", position: 1)
+group2 = Thredded::MessageboardGroup.create(name: "General", position: 2)
+boards = []
+boards << Thredded::Messageboard.create(name: 'Ideas, Features, Praise', position: 1, group: group1)
+boards << Thredded::Messageboard.create(name: 'Need Help? Found a Bug?', position: 2, group: group1)
+boards << Thredded::Messageboard.create(name: 'Making Music', position: 1, group: group2)
+boards << Thredded::Messageboard.create(name: 'Gear Talk', position: 2, group: group2)
+boards.collect do |board|
+  topic = board.topics.new do |t|
+    user = User.all.sample
+    t.user = user
+    t.title = "New thread on #{board.name}"
+    t.posts.new do |p|
+      p.user = user
+      p.content = 'Here is the body of a post on #{board.name}... Good stuff!'
+      p.moderation_state = 'approved'
+      p.messageboard = board
+    end
+    t.save
+  end
+end
