f574f7e03d20bd6bec266af3dd97072920f4684a
Sudara <sudara@alonetone.com>
2020-02-07 01:17:47 +0100

    Correct url for edit profile snapshot

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe 'users', type: :feature, js: true do
   it 'renders users/edit' do
     logged_in(:jamie_kiesl) do
-      visit '/users/jamie_kiesl/edit'
+      visit '/users/jamiek/edit'
       Percy.snapshot(page, name: 'Edit Profile')
     end
   end
