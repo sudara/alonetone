2e4d05e6d6d02431b032cbe58b7411644e88a6a6
Sudara <sudara@alonetone.com>
2020-04-30 14:39:28 +0200

    Add model level validations for playlists

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -34,6 +34,10 @@ class Playlist < ActiveRecord::Base
   # We have to define attachments last to make the Active Record callbacks
   # fire in the right order.
   has_one_attached :cover_image
+  validates :cover_image, attached: {
+    content_type: %w[image/png image/jpeg image/jpg image/gif],
+    byte_size: { less_than: 20.megabytes }
+  }, if: :cover_image_present?
 
   enum(
     cover_quality: {
