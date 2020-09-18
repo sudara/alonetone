6e78e2ca3f5285cb8bdb31ded8dfebb9808e259f
Sudara <sudara@alonetone.com>
2020-09-18 18:08:32 +0200

    Update session_store to use latest SameSite standards

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,5 @@
-Rails.application.config.session_store :cookie_store, key: 'alonetone', expire_after: 14.days
+Rails.application.config.session_store :cookie_store,
+  key: 'alonetone',
+  secure: Rails.env.production?,
+  same_site: :lax,
+  expire_after: 30.days
