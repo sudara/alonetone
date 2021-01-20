c67e7519cfda58928d585877b68486de65b918ed
bells <ben@modernthings.net>
2021-01-20 14:15:10 -0700

    Rewrite followees controller to work with any specified size and margins from css, also ESlint approved this time

--- app/javascript/controllers/followees_controller.js
+++ app/javascript/controllers/followees_controller.js
@@ -1,29 +1,37 @@
 import { Controller } from 'stimulus'
 
+let containerPadding = 0
+let avatarWidth = 0
+let avatarMargin = 0
+
 export default class extends Controller {
-  
   connect() {
+    const containerPaddingLeft = parseInt(window.getComputedStyle(this.element).getPropertyValue('padding-left'), 10)
+    const containerPaddingRight = parseInt(window.getComputedStyle(this.element).getPropertyValue('padding-right'), 10)
+    containerPadding = containerPaddingLeft + containerPaddingRight
+    const firstAvatar = document.getElementsByClassName('user_small_avatar')[0]
+    avatarWidth = parseInt(window.getComputedStyle(firstAvatar).getPropertyValue('width'), 10)
+    avatarMargin = parseInt(window.getComputedStyle(firstAvatar).getPropertyValue('margin-bottom'), 10)
+
     this.resize()
   }
 
   resize() {
+    const containerWidth = this.element.clientWidth - containerPadding
+    const avatarsPerRow = Math.round(this.element.clientWidth / (avatarWidth + avatarMargin))
+    const totalMarginWidth = avatarMargin * (avatarsPerRow)
+    const totalImageWidth = containerWidth - totalMarginWidth
+    const newImageWidth = totalImageWidth / avatarsPerRow
+    const totalFollowees = this.element.childElementCount
 
-    let containerWidth = this.element.clientWidth - 24
-    let itemsPerRow = Math.round ( this.element.clientWidth / 58 )
-    let totalMarginWidth = 8 * (itemsPerRow)
-    let totalImageWidth = containerWidth - totalMarginWidth
-    let newImageWidth = totalImageWidth / itemsPerRow
-    let totalFollowees = this.element.childElementCount
+    if (totalFollowees >= (avatarsPerRow - 1)) {
+      const childrenArray = [...this.element.children]
 
-    if ( totalFollowees >= ( itemsPerRow -1 ) ) {
-
-      let children = this.element.children
-      let childrenArray = [ ...children ]
-
-      childrenArray.forEach(function(item) {
-          item.style.width = newImageWidth + "px"
-          item.style.height = newImageWidth + "px"
-      });
+      childrenArray.forEach((avatar) => {
+        const thisAvatar = avatar
+        thisAvatar.style.width = `${newImageWidth}px`
+        thisAvatar.style.height = `${newImageWidth}px`
+      })
     }
   }
 }
