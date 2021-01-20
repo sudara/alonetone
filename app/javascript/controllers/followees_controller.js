ae7f14a8fd32b5132b3770ba08992717c50349fe
bells <ben@modernthings.net>
2021-01-20 14:24:49 -0700

    Change constants that don't depend on screen width to be class members

--- app/javascript/controllers/followees_controller.js
+++ app/javascript/controllers/followees_controller.js
@@ -1,25 +1,22 @@
 import { Controller } from 'stimulus'
 
-let containerPadding = 0
-let avatarWidth = 0
-let avatarMargin = 0
-
 export default class extends Controller {
   connect() {
     const containerPaddingLeft = parseInt(window.getComputedStyle(this.element).getPropertyValue('padding-left'), 10)
     const containerPaddingRight = parseInt(window.getComputedStyle(this.element).getPropertyValue('padding-right'), 10)
-    containerPadding = containerPaddingLeft + containerPaddingRight
+    this.containerPadding = containerPaddingLeft + containerPaddingRight
     const firstAvatar = document.getElementsByClassName('user_small_avatar')[0]
-    avatarWidth = parseInt(window.getComputedStyle(firstAvatar).getPropertyValue('width'), 10)
-    avatarMargin = parseInt(window.getComputedStyle(firstAvatar).getPropertyValue('margin-bottom'), 10)
+    this.avatarWidth = parseInt(window.getComputedStyle(firstAvatar).getPropertyValue('width'), 10)
+    this.avatarMargin = parseInt(window.getComputedStyle(firstAvatar).getPropertyValue('margin-bottom'), 10)
+    this.avatarWidthPlusMargin = this.avatarWidth + this.avatarMargin
 
     this.resize()
   }
 
   resize() {
-    const containerWidth = this.element.clientWidth - containerPadding
-    const avatarsPerRow = Math.round(this.element.clientWidth / (avatarWidth + avatarMargin))
-    const totalMarginWidth = avatarMargin * (avatarsPerRow)
+    const containerWidth = this.element.clientWidth - this.containerPadding
+    const avatarsPerRow = Math.round(this.element.clientWidth / this.avatarWidthPlusMargin )
+    const totalMarginWidth = this.avatarMargin * (avatarsPerRow)
     const totalImageWidth = containerWidth - totalMarginWidth
     const newImageWidth = totalImageWidth / avatarsPerRow
     const totalFollowees = this.element.childElementCount
