bcb8760d404d515cce36be8d8ca6c7a77b624ae8
bells <ben@modernthings.net>
2021-03-08 20:48:15 -0700

    Fix bug with h px vs hpx

--- app/javascript/controllers/blank_slate_controller.js
+++ app/javascript/controllers/blank_slate_controller.js
@@ -11,11 +11,11 @@ export default class extends Controller {
     this.spacerTarget.style.height = 0;
     const lastContentNode = this.containerTarget.children[this.containerTarget.children.length - 3]
     let h = Math.max(0, this.containerTarget.clientHeight - this.floaterTarget.clientHeight)
-    this.spacerTarget.style.height = `${h} px`
+    this.spacerTarget.style.height = `${h}px`
 
     while (h > 0 && this.floaterTarget.getBoundingClientRect().bottom > lastContentNode.getBoundingClientRect().bottom) {
       h -= 1
-      this.spacerTarget.style.height = `${h} px`
+      this.spacerTarget.style.height = `${h}px`
     }
   }
 }
