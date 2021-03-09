92d5810b755ebf4142eb313c034249e7b0c07b8b
bells <ben@modernthings.net>
2021-03-08 20:53:57 -0700

    It works! But the SCSS needs some serious refactoring...

--- app/javascript/controllers/blank_slate_controller.js
+++ app/javascript/controllers/blank_slate_controller.js
@@ -12,7 +12,6 @@ export default class extends Controller {
     const lastContentNode = this.containerTarget.children[this.containerTarget.children.length - 3]
     let h = Math.max(0, this.containerTarget.clientHeight - this.floaterTarget.clientHeight)
     this.spacerTarget.style.height = `${h}px`
-
     while (h > 0 && this.floaterTarget.getBoundingClientRect().bottom > lastContentNode.getBoundingClientRect().bottom) {
       h -= 1
       this.spacerTarget.style.height = `${h}px`
