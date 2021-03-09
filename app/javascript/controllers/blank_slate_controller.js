35b5663f81db635e8a430f800e02f5bb5eaaecd6
bells <ben@modernthings.net>
2021-03-08 20:41:25 -0700

    make JSLint a bit happier, fiddle with media queries

--- app/javascript/controllers/blank_slate_controller.js
+++ app/javascript/controllers/blank_slate_controller.js
@@ -1,25 +1,21 @@
 import { Controller } from 'stimulus'
 
 export default class extends Controller {
-  static targets = ['credits', 'sidebar', 'content', 'cover']
+  static targets = ['spacer', 'floater', 'container']
 
   initialize() {
     this.resize()
   }
 
   resize() {
-    this.sizeSpacer(document.getElementById("spacer"));
-  }
+    this.spacerTarget.style.height = 0;
+    const lastContentNode = this.containerTarget.children[this.containerTarget.children.length - 3]
+    let h = Math.max(0, this.containerTarget.clientHeight - this.floaterTarget.clientHeight)
+    this.spacerTarget.style.height = `${h} px`
 
-  sizeSpacer(spacer) {
-    spacer.style.height = 0;
-    var container = spacer.parentNode;
-    var img = spacer.nextElementSibling || spacer.nextSibling;
-    var lastContentNode = container.children[container.children.length - 1];
-    var h = Math.max(0, container.clientHeight - img.clientHeight);
-    spacer.style.height = h + "px";
-    while (h > 0 && img.getBoundingClientRect().bottom > lastContentNode.getBoundingClientRect().bottom) {
-        spacer.style.height = --h + "px";
+    while (h > 0 && this.floaterTarget.getBoundingClientRect().bottom > lastContentNode.getBoundingClientRect().bottom) {
+      h -= 1
+      this.spacerTarget.style.height = `${h} px`
     }
   }
 }
