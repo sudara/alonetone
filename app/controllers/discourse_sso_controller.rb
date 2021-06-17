5d181bf4ed01ece037a9daa75b53fae1815d263a
Sudara <sudara@alonetone.com>
2021-06-17 17:54:39 +0200

    Add discourse SSO integration

---
+++ app/controllers/discourse_sso_controller.rb
@@ -0,0 +1,20 @@
+# This is one of the few things in alonetone that is production-only
+
+class DiscourseSsoController < ApplicationController
+  before_action :require_login
+
+  def sso
+    secret = Rails.configuration.alonetone.discourse_secret
+    sso = SingleSignOn.parse(request.query_string, secret)
+    sso.email = current_user.email
+    sso.name = current_user.display_name
+    sso.username = current_user.login
+    sso.external_id = current_user.id
+    sso.sso_secret = secret
+    sso.bio = current_user.profile.bio
+    sso.moderator = current_user.moderator?
+    sso.avatar_url = current_user.user.avatar_image_location(variant: :large_avatar).to_s
+
+    redirect_to sso.to_url(Rails.configuration.alonetone.discourse_url)
+  end
+end
