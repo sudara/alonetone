0a31519f48ea02944cbf3b81de82070bd7dbad53
Manfred Stienstra <manfred@fngtps.com>
2021-10-03 12:41:25 +0200

    Re-implement Waveform using audiowaveform.
    The ruby-audio gem used to read the audio data is no
    longer maintained and started giving compilation
    errors on newer macs, possibly because of the location
    of the libsndfile headers.
    
    Let's move into the future and use a tool that will also
    eventually be deprecated :smile:.

--- app/models/waveform.rb
+++ app/models/waveform.rb
@@ -1,74 +1,48 @@
-# Generates a list of RMS-like amplitudes which can be used to render a
-# waveform for an MP3.
-module Waveform
+# frozen_string_literal: true
+
+# Generates a list of RMS-like amplitudes which can be used to render a waveform for an MP3.
+class Waveform
   LENGTH = 500
 
-  class Reduction
-    attr_reader :sound
-    attr_reader :slice_size
-
-    def initialize(sound)
-      @sound = sound
-      self.info = sound.info
-    end
-
-    def mono?
-      @mono
-    end
-
-    def samples
-      samples = []
-      while signal = read
-        rms = 0.0
-        for frame in signal
-          rms += (mono? ? frame : frame.sum)**2
-        end
-        samples << Math.sqrt(rms).round
-      end
-      samples
-    end
-
-    private
-
-    attr_reader :signal
-
-    def info=(info)
-      @mono = info.channels == 1
-      @slice_size = (info.frames / Waveform::LENGTH.to_f).ceil
-    end
-
-    def read
-      signal = sound.read(:int, slice_size)
-      signal.real_size == 0 ? nil : signal
-    end
+  def initialize(path)
+    @path = path
   end
 
-  def self.extract(file)
-    Tempfile.open(['resampled-upload', '.wav'], encoding: 'binary') do |tempfile|
-      reduce_file(tempfile) if decode_resample(file, tempfile)
+  def extract
+    return unless json
+
+    # The audiowaveform tool doesn't allow us to restrict the number of samples that are created
+    # so we need to resample to generate around 500 samples.
+    rms = []
+    slice_size = json.fetch('data').size.to_f / LENGTH
+    json.fetch('data').each.with_index do |sample, index|
+      bucket = index / slice_size
+      rms[bucket] = rms[bucket].to_f + sample**2
     end
+    return nil if rms.empty?
+
+    rms.map { |sample| Math.sqrt(sample).round }
   end
 
-  # Resample the source MP3 down to 8KHz to make it more manageable.
-  def self.decode_resample(file, tempfile)
-    system(
-      'lame',
-      '--quiet',
-      '--mp3input',
-      '--resample', '8',
-      '--decode', Shellwords.shellescape(file),
-      Shellwords.shellescape(tempfile.path),
-      out: File::NULL, err: File::NULL
+  def self.extract(path)
+    new(path).extract
+  end
+
+  private
+
+  def json
+    @json ||= execute
+  end
+
+  def execute
+    json, status = Open3.capture2(
+      'audiowaveform',
+      '--input-filename', @path,
+      # Uploaded audio is always converted to MP3 first because it needs to play in the browser.
+      '--input-format', 'mp3',
+      '--output-format', 'json',
+      '--quiet'
     )
-  end
-
-  def self.reduce_file(tempfile)
-    RubyAudio::Sound.open(tempfile.path) { |sound| reduce(sound) }
-  rescue RubyAudio::Error
-  end
-
-  # Compute RMS-like values to reduce the number of aplitudes to 500.
-  def self.reduce(sound)
-    Waveform::Reduction.new(sound).samples
+    JSON.load(json) if status.exitstatus == 0
   end
 end
