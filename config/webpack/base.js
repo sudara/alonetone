77d51bac0167e7c224b2c5b64fa41c3fa5cb076e
Sudara <sudara@alonetone.com>
2022-02-21 18:44:18 +0100

    Remove per-env webpacker files

--- config/webpack/base.js
+++
@@ -1,9 +0,0 @@
-const { webpackConfig, merge } = require('@rails/webpacker')
-
-const customConfig = {
-  output: {
-    library: 'Alonetone',
-    libraryTarget: 'var',
-  },
-}
-module.exports = merge(webpackConfig, customConfig)
