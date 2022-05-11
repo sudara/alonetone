229af0c9621fe684106a9e3e6969a10ca7e3c6d9
Ben Montgomery <ben@modernthings.net>
2022-05-10 21:47:06 -0600

    Another round on track_edit

--- app/javascript/controllers/license_controller.js
+++ app/javascript/controllers/license_controller.js
@@ -3,5 +3,6 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   expand() {
     this.element.classList.add('expanded')
+    this.element.classList.remove('collapsed')
   }
 }
