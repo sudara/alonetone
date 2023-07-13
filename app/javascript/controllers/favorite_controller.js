8ac5e468a7ca08068194f21ba18498a2e9541d52
Jberczel <jxberc@gmail.com>
2023-07-13 16:06:59 -0400

    Update animation_heart svg when faving

--- app/javascript/controllers/favorite_controller.js
+++ app/javascript/controllers/favorite_controller.js
@@ -5,7 +5,29 @@ export default class extends HeartController {
     id: Number,
   }
 
+  connect() {
+    // access controller from child element
+    this.element[this.identifier] = this
+  }
+
   isFavorited() {
     return window.userFavorites.includes(this.idValue)
+  }
+
+  toggle({broadcast=true}) {
+    super.toggle()
+
+    if (broadcast)
+      this.broadcast()
+  }
+
+  broadcast() {
+    const matches = document.querySelectorAll(`[data-favorite-id-value="${this.idValue}"]`)
+    matches.forEach((element) => {
+      if (element === this.element)
+        return
+
+      element.favorite.toggle({broadcast: false})
+    })
   }
 }
