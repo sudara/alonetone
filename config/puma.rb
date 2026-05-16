af15d849cd43a6c6f1c30c25cc219d12aea85ae7
Sudara <sudara@alonetone.com>
2026-05-16 14:15:54 +0200

    Use APP_ROOT so socked can be absolute path in production

--- config/puma.rb
+++ config/puma.rb
@@ -1,15 +1,19 @@
 #!/usr/bin/env puma
+APP_ROOT = File.expand_path('..', __dir__)
+
 env = ENV.fetch("RAILS_ENV") { "production" }
 environment env
+
 if env == "production"
-workers 3
-bind 'unix://tmp/puma.sock'
-state_path 'tmp/puma.state'
-stdout_redirect 'log/puma.log', 'log/puma.log', true
+  workers 3
+  bind "unix://#{APP_ROOT}/tmp/puma.sock"
+  state_path "#{APP_ROOT}/tmp/puma.state"
+  stdout_redirect "#{APP_ROOT}/log/puma.log", "#{APP_ROOT}/log/puma.log", true
 end
-threads 1,4
+
+threads 1, 4
 prune_bundler
-pidfile 'tmp/puma.pid'
+pidfile "#{APP_ROOT}/tmp/puma.pid"
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
