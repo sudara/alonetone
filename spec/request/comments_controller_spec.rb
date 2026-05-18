2ceefc65c6350e70ca76364e8abc2804dd6456a1
Sudara <sudara@alonetone.com>
2026-05-18 13:28:12 +0200

    Cover Akismet-flagged comments saving with is_spam=true

--- spec/request/comments_controller_spec.rb
+++ spec/request/comments_controller_spec.rb
@@ -45,6 +45,21 @@ RSpec.describe CommentsController, type: :request do
     end
   end
 
+  context "when Akismet flags a comment" do
+    let!(:asset) { assets(:valid_mp3) }
+
+    it "still saves it but marks is_spam (so it's hidden from public views)" do
+      akismet_stub_response_spam
+      akismet_stub_submit_spam
+      params = { comment: { body: "anything", private: "0", commentable_type: "Asset", commentable_id: asset.id } }
+      expect do
+        post "/comments", params: params, headers: { 'x-forwarded-for' => '8.8.8.8', 'user-agent' => 'webkit' }
+      end.to change(Comment, :count).by(1)
+      expect(response).to have_http_status(201)
+      expect(Comment.last.is_spam).to be true
+    end
+  end
+
   context "guest rate limiting" do
     let!(:asset) { assets(:valid_mp3) }
 
