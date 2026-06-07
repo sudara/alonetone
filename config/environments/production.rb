bdbe5dc55182cbafb7e80ee59a86ecc25df70f81
Sudara <sudara@alonetone.com>
2026-06-07 18:00:54 +0200

    Use Rails 8 reloading config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,7 @@ Rails.application.configure do
   config.hosts += ["alonetone.com", "listenapp.com"]
 
   # Code is not reloaded between requests.
-  config.cache_classes = true
+  config.enable_reloading = false
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
