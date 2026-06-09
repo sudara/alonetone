c1e1d53990a26a23122ba9ff80042978393c390f
Sudara <sudara@alonetone.com>
2026-06-09 02:02:38 +0200

    Fix admin pagination prev link for Pagy v43 (use #previous)

--- spec/request/admin/account_requests_controller_spec.rb
+++ spec/request/admin/account_requests_controller_spec.rb
@@ -56,6 +56,17 @@ RSpec.describe Admin::AccountRequestsController, type: :request do
     expect(response.body).to include(%(id="account_request_#{account_requests(:waiting).id}"))
   end
 
+  it "renders pagination controls when there is more than one page" do
+    25.times do |i|
+      AccountRequest.create!(login: "pager#{i}", email: "pager#{i}@example.com",
+        entity_type: :musician, details: 'a' * 60, status: :waiting)
+    end
+    get admin_account_requests_path
+    expect(response).to have_http_status(:ok)
+    expect(response.body).to include('Page 1 of')
+    expect(response.body).to include('Next')
+  end
+
   it "narrows the rendered cards to the requested filter" do
     get admin_account_requests_path
     expect(response.body).to match_css('[id^="account_request_"]', count: AccountRequest.count)
